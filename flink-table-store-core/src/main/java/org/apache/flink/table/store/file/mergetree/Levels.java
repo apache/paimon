7c8791947249ce51a8480f5c51b147b6dc065821
JingsongLi <lzljs3620320@aliyun.com>
2022-01-13 17:05:57 +0800

    [FLINK-25629] Introduce CompactManager
    This closes #5

---
+++ flink-table-store-core/src/main/java/org/apache/flink/table/store/file/mergetree/Levels.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.store.file.mergetree;
+
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.store.file.mergetree.sst.SstFileMeta;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.emptyList;
+import static org.apache.flink.util.Preconditions.checkArgument;
+
+/** A class which stores all level files of merge tree. */
+public class Levels {
+
+    private final Comparator<RowData> keyComparator;
+
+    private final TreeSet<SstFileMeta> level0;
+
+    private final List<SortedRun> levels;
+
+    public Levels(Comparator<RowData> keyComparator, List<SstFileMeta> inputFiles, int numLevels) {
+        this.keyComparator = keyComparator;
+        checkArgument(numLevels > 1, "levels must be at least 2.");
+        this.level0 =
+                new TreeSet<>(Comparator.comparing(SstFileMeta::maxSequenceNumber).reversed());
+        this.levels = new ArrayList<>();
+        for (int i = 1; i < numLevels; i++) {
+            levels.add(SortedRun.empty());
+        }
+
+        Map<Integer, List<SstFileMeta>> levelMap = new HashMap<>();
+        for (SstFileMeta file : inputFiles) {
+            levelMap.computeIfAbsent(file.level(), level -> new ArrayList<>()).add(file);
+        }
+        levelMap.forEach((level, files) -> updateLevel(level, emptyList(), files));
+    }
+
+    public void addLevel0File(SstFileMeta file) {
+        checkArgument(file.level() == 0);
+        level0.add(file);
+    }
+
+    public SortedRun runOfLevel(int level) {
+        checkArgument(level > 0, "Level0 dose not have one single sorted run.");
+        return levels.get(level - 1);
+    }
+
+    public int numberOfLevels() {
+        return levels.size() + 1;
+    }
+
+    /** @return the highest non-empty level or -1 if all levels empty. */
+    public int nonEmptyHighestLevel() {
+        int i;
+        for (i = levels.size() - 1; i >= 0; i--) {
+            if (levels.get(i).nonEmpty()) {
+                return i + 1;
+            }
+        }
+        return level0.isEmpty() ? -1 : 0;
+    }
+
+    public List<SstFileMeta> allFiles() {
+        List<SstFileMeta> files = new ArrayList<>();
+        List<LevelSortedRun> runs = levelSortedRuns();
+        for (LevelSortedRun run : runs) {
+            files.addAll(run.run().files());
+        }
+        return files;
+    }
+
+    public List<LevelSortedRun> levelSortedRuns() {
+        List<LevelSortedRun> runs = new ArrayList<>();
+        level0.forEach(file -> runs.add(new LevelSortedRun(0, SortedRun.fromSingle(file))));
+        for (int i = 0; i < levels.size(); i++) {
+            SortedRun run = levels.get(i);
+            if (run.nonEmpty()) {
+                runs.add(new LevelSortedRun(i + 1, run));
+            }
+        }
+        return runs;
+    }
+
+    public void update(List<SstFileMeta> before, List<SstFileMeta> after) {
+        Map<Integer, List<SstFileMeta>> groupedBefore = groupByLevel(before);
+        Map<Integer, List<SstFileMeta>> groupedAfter = groupByLevel(after);
+        for (int i = 0; i < numberOfLevels(); i++) {
+            updateLevel(
+                    i,
+                    groupedBefore.getOrDefault(i, emptyList()),
+                    groupedAfter.getOrDefault(i, emptyList()));
+        }
+    }
+
+    private void updateLevel(int level, List<SstFileMeta> before, List<SstFileMeta> after) {
+        if (before.isEmpty() && after.isEmpty()) {
+            return;
+        }
+
+        if (level == 0) {
+            before.forEach(level0::remove);
+            level0.addAll(after);
+        } else {
+            List<SstFileMeta> files = new ArrayList<>(runOfLevel(level).files());
+            files.removeAll(before);
+            files.addAll(after);
+            levels.set(level - 1, SortedRun.fromUnsorted(files, keyComparator));
+        }
+    }
+
+    private Map<Integer, List<SstFileMeta>> groupByLevel(List<SstFileMeta> files) {
+        return files.stream()
+                .collect(Collectors.groupingBy(SstFileMeta::level, Collectors.toList()));
+    }
+}
---
+++ flink-table-store-core/src/main/java/org/apache/flink/table/store/file/mergetree/compact/CompactManager.java
@@ -0,0 +1,214 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.store.file.mergetree.compact;
+
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.store.file.mergetree.Levels;
+import org.apache.flink.table.store.file.mergetree.SortedRun;
+import org.apache.flink.table.store.file.mergetree.sst.SstFileMeta;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import static java.util.Collections.singletonList;
+
+/** Manager to submit compaction task. */
+public class CompactManager {
+
+    private final ExecutorService executor;
+
+    private final CompactStrategy strategy;
+
+    private final Comparator<RowData> keyComparator;
+
+    private final long minFileSize;
+
+    private final Rewriter rewriter;
+
+    private Future<CompactResult> taskFuture;
+
+    public CompactManager(
+            ExecutorService executor,
+            CompactStrategy strategy,
+            Comparator<RowData> keyComparator,
+            long minFileSize,
+            Rewriter rewriter) {
+        this.executor = executor;
+        this.minFileSize = minFileSize;
+        this.keyComparator = keyComparator;
+        this.strategy = strategy;
+        this.rewriter = rewriter;
+    }
+
+    /** Submit a new compaction task. */
+    public void submitCompaction(Levels levels) {
+        if (taskFuture != null) {
+            throw new IllegalStateException(
+                    "Please finish the previous compaction before submitting new one.");
+        }
+        strategy.pick(levels.numberOfLevels(), levels.levelSortedRuns())
+                .ifPresent(
+                        unit -> {
+                            if (unit.files().size() < 2) {
+                                return;
+                            }
+                            boolean dropDelete =
+                                    unit.outputLevel() >= levels.nonEmptyHighestLevel();
+                            CompactTask task = new CompactTask(unit, dropDelete);
+                            taskFuture = executor.submit(task);
+                        });
+    }
+
+    /** Finish current task, and update result files to {@link Levels}. */
+    public Optional<CompactResult> finishCompaction(Levels levels)
+            throws ExecutionException, InterruptedException {
+        if (taskFuture != null) {
+            CompactResult result = taskFuture.get();
+            levels.update(result.before(), result.after());
+            taskFuture = null;
+            return Optional.of(result);
+        }
+        return Optional.empty();
+    }
+
+    /** Rewrite sections to the files. */
+    @FunctionalInterface
+    public interface Rewriter {
+
+        List<SstFileMeta> rewrite(
+                int outputLevel, boolean dropDelete, List<List<SortedRun>> sections)
+                throws Exception;
+    }
+
+    /** Result of compaction. */
+    public interface CompactResult {
+
+        List<SstFileMeta> before();
+
+        List<SstFileMeta> after();
+    }
+
+    // --------------------------------------------------------------------------------------------
+    // Internal classes
+    // --------------------------------------------------------------------------------------------
+
+    /** Compaction task. */
+    private class CompactTask implements Callable<CompactResult> {
+
+        private final int outputLevel;
+
+        private final List<List<SortedRun>> partitioned;
+
+        private final boolean dropDelete;
+
+        private CompactTask(CompactUnit unit, boolean dropDelete) {
+            this.outputLevel = unit.outputLevel();
+            this.partitioned = new IntervalPartition(unit.files(), keyComparator).partition();
+            this.dropDelete = dropDelete;
+        }
+
+        @Override
+        public CompactResult call() throws Exception {
+            return compact();
+        }
+
+        private CompactResult compact() throws Exception {
+            List<List<SortedRun>> candidate = new ArrayList<>();
+            List<SstFileMeta> before = new ArrayList<>();
+            List<SstFileMeta> after = new ArrayList<>();
+
+            // Checking the order and compacting adjacent and contiguous files
+            // Note: can't skip an intermediate file to compact, this will destroy the overall
+            // orderliness
+            for (List<SortedRun> section : partitioned) {
+                if (section.size() > 1) {
+                    candidate.add(section);
+                } else {
+                    SortedRun run = section.get(0);
+                    // No overlapping:
+                    // We can just upgrade the large file and just change the level instead of
+                    // rewriting it
+                    // But for small files, we will try to compact it
+                    for (SstFileMeta file : run.files()) {
+                        if (file.fileSize() < minFileSize) {
+                            // Smaller files are rewritten along with the previous files
+                            candidate.add(singletonList(SortedRun.fromSingle(file)));
+                        } else {
+                            // Large file appear, rewrite previous and upgrade it
+                            rewrite(candidate, before, after);
+                            upgrade(file, before, after);
+                        }
+                    }
+                }
+            }
+            rewrite(candidate, before, after);
+            return result(before, after);
+        }
+
+        private void upgrade(SstFileMeta file, List<SstFileMeta> before, List<SstFileMeta> after) {
+            if (file.level() != outputLevel) {
+                before.add(file);
+                after.add(file.upgrade(outputLevel));
+            }
+        }
+
+        private void rewrite(
+                List<List<SortedRun>> candidate, List<SstFileMeta> before, List<SstFileMeta> after)
+                throws Exception {
+            if (candidate.isEmpty()) {
+                return;
+            }
+            if (candidate.size() == 1) {
+                List<SortedRun> section = candidate.get(0);
+                if (section.size() == 0) {
+                    return;
+                } else if (section.size() == 1) {
+                    for (SstFileMeta file : section.get(0).files()) {
+                        upgrade(file, before, after);
+                    }
+                    candidate.clear();
+                    return;
+                }
+            }
+            candidate.forEach(runs -> runs.forEach(run -> before.addAll(run.files())));
+            after.addAll(rewriter.rewrite(outputLevel, dropDelete, candidate));
+            candidate.clear();
+        }
+
+        private CompactResult result(List<SstFileMeta> before, List<SstFileMeta> after) {
+            return new CompactResult() {
+                @Override
+                public List<SstFileMeta> before() {
+                    return before;
+                }
+
+                @Override
+                public List<SstFileMeta> after() {
+                    return after;
+                }
+            };
+        }
+    }
+}
