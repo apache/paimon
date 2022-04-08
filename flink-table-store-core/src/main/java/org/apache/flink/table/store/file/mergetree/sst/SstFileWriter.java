372115f440c7bde75694f0718c52398bd1ecb827
tsreaper <tsreaper96@gmail.com>
2022-04-08 17:52:00 +0800

    [fix] add comments

--- flink-table-store-core/src/main/java/org/apache/flink/table/store/file/mergetree/sst/SstFileWriter.java
+++ flink-table-store-core/src/main/java/org/apache/flink/table/store/file/mergetree/sst/SstFileWriter.java
@@ -134,6 +134,9 @@ public class SstFileWriter {
         private long maxSequenceNumber;
 
         private SstRollingFile(int level) {
+            // each level 0 sst file is a sorted run,
+            // we must not write rolling files for level 0 ssts
+            // otherwise we cannot reduce the number of sorted runs when compacting
             super(level == 0 ? Long.MAX_VALUE : suggestedFileSize);
             this.level = level;
             this.serializer = new KeyValueSerializer(keyType, valueType);
