b07eb0b6718efa3fafc4b74d6ec1852fe3733030
ajian2002 <ajian020515@163.com>
2022-05-14 22:01:53 +0800

    use org.apache.flink.api.java.aggregation.AggregationFunction to implement the AggregationMergeFunction

--- flink-table-store-core/src/main/java/org/apache/flink/table/store/file/mergetree/compact/AggregationMergeFunction.java
+++ flink-table-store-core/src/main/java/org/apache/flink/table/store/file/mergetree/compact/AggregationMergeFunction.java
@@ -18,12 +18,20 @@
 
 package org.apache.flink.table.store.file.mergetree.compact;
 
+import org.apache.flink.api.java.aggregation.AggregationFunction;
+import org.apache.flink.api.java.aggregation.AggregationFunctionFactory;
+import org.apache.flink.api.java.aggregation.Aggregations;
+import org.apache.flink.api.java.aggregation.UnsupportedAggregationTypeException;
 import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.RowType;
-import org.apache.flink.types.RowKind;
 
 import javax.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A {@link MergeFunction} where key is primary key (unique) and value is the partial record, update
@@ -37,7 +45,10 @@ public class AggregationMergeFunction implements MergeFunction {
     private final RowData.FieldGetter[] getters;
     private final RowType primaryKeyType;
     private final RowType rowType;
+    private final Set<String> primaryKeyNames;
+    private final ArrayList<String> rowNames;
 
+    private final ArrayList<AggregationFunction<Object>> types;
     private transient GenericRowData row;
 
     public AggregationMergeFunction(
@@ -45,6 +56,20 @@ public class AggregationMergeFunction implements MergeFunction {
         this.getters = fieldGetters;
         this.primaryKeyType = primaryKeyType;
         this.rowType = rowType;
+        this.primaryKeyNames = new HashSet<>(primaryKeyType.getFieldNames());
+        this.rowNames = new ArrayList<>(rowType.getFieldNames());
+        this.types = new ArrayList<>(rowType.getFieldCount());
+        AggregationFunctionFactory factory = Aggregations.SUM.getFactory();
+        for (LogicalType type : rowType.getChildren()) {
+            try {
+                AggregationFunction<Object> f =
+                        factory.createAggregationFunction(
+                                (Class<Object>) type.getDefaultConversion());
+                types.add(f);
+            } catch (UnsupportedAggregationTypeException e) {
+                types.add(null);
+            }
+        }
     }
 
     @Override
@@ -56,64 +81,36 @@ public class AggregationMergeFunction implements MergeFunction {
     public void add(RowData value) {
         for (int i = 0; i < getters.length; i++) {
             Object currentField = getters[i].getFieldOrNull(value);
-
-            if (primaryKeyType.getFieldIndex(rowType.getFieldNames().get(i)) != -1) {
+            AggregationFunction<Object> f = types.get(i);
+            if (primaryKeyNames.contains(rowNames.get(i))) {
                 // primary key
                 if (currentField != null) {
                     row.setField(i, currentField);
                 }
             } else {
-                Object oldValue = row.getField(i);
-                Object result = sum(value.getRowKind(), oldValue, currentField);
-                if (result != null) {
-                    row.setField(i, result);
+                if (f != null) {
+                    f.initializeAggregate();
+                    Object oldValue = row.getField(i);
+                    if (oldValue != null) {
+                        f.aggregate(oldValue);
+                    }
+                    switch (row.getRowKind()) {
+                        case INSERT:
+                            f.aggregate(currentField);
+                            break;
+                        case DELETE:
+                        case UPDATE_AFTER:
+                        case UPDATE_BEFORE:
+                        default:
+                            throw new UnsupportedOperationException(
+                                    "Unsupported row kind: " + row.getRowKind());
+                    }
+                    Object result = f.getAggregate();
+                    if (result != null) {
+                        row.setField(i, result);
+                    }
                 }
             }
-        }
-    }
-
-    private Object sum(RowKind kind, Object oldValue, Object currentField) {
-        switch (kind) {
-            case DELETE:
-                if (oldValue == null) {
-                    return null;
-                }
-                if (currentField == null) {
-                    return oldValue;
-                }
-                if (oldValue instanceof Integer && currentField instanceof Integer) {
-                    return Integer.sum((Integer) oldValue, -(Integer) currentField);
-                } else if (oldValue instanceof Long && currentField instanceof Long) {
-                    return Long.sum((Long) oldValue, -(Long) currentField);
-                } else if (oldValue instanceof Double && currentField instanceof Double) {
-                    return Double.sum((Double) oldValue, -(Double) currentField);
-                } else if (oldValue instanceof Float && currentField instanceof Float) {
-                    return Float.sum((Float) oldValue, -(Float) currentField);
-                }
-                throw new UnsupportedOperationException(
-                        "Cannot support " + oldValue + " delete " + currentField);
-            case INSERT:
-                if (currentField == null) {
-                    return null;
-                }
-                if (oldValue == null) {
-                    return currentField;
-                }
-                if (oldValue instanceof Integer && currentField instanceof Integer) {
-                    return Integer.sum((Integer) oldValue, (Integer) currentField);
-                } else if (oldValue instanceof Long && currentField instanceof Long) {
-                    return Long.sum((Long) oldValue, (Long) currentField);
-                } else if (oldValue instanceof Double && currentField instanceof Double) {
-                    return Double.sum((Double) oldValue, (Double) currentField);
-                } else if (oldValue instanceof Float && currentField instanceof Float) {
-                    return Float.sum((Float) oldValue, (Float) currentField);
-                }
-                throw new UnsupportedOperationException(
-                        "Cannot support " + oldValue + " sum " + currentField);
-            case UPDATE_AFTER:
-            case UPDATE_BEFORE:
-            default:
-                throw new UnsupportedOperationException("Unsupported row kind: " + kind);
         }
     }
 
