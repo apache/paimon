1e468da76e8a08c867200029e58cb85324c27641
ajian2002 <ajian020515@163.com>
2022-05-31 09:06:20 +0800

    delete mytest

--- flink-table-store-connector/src/test/java/org/apache/flink/table/store/connector/AggregationITCase.java
+++ flink-table-store-connector/src/test/java/org/apache/flink/table/store/connector/AggregationITCase.java
@@ -166,41 +166,4 @@ public class AggregationITCase extends FileStoreTableITCase {
                         Row.of("pk2", 2, 7.0));
     }
 
-    @Test
-    public void myTest() throws Exception {
-        String ddl3 =
-                "CREATE TABLE IF NOT EXISTS T5 ( dt STRING, hr INT, price INT, PRIMARY KEY (dt, hr) NOT ENFORCED ) WITH ( 'merge-engine' = 'aggregation', 'price.aggregate-function' = 'sum' );";
-        String tmpPath;
-        try {
-            tmpPath = TEMPORARY_FOLDER.newFolder().toURI().toString();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        String ddl4 =
-                "CREATE TABLE IF NOT EXISTS A ( dt STRING, hr INT, price INT ) WITH ( 'connector' = 'filesystem', 'path' = '"
-                        + tmpPath
-                        + "', 'format' = 'avro' );";
-        String ddl5 =
-                "CREATE TABLE IF NOT EXISTS P ( dt STRING, hr INT, price INT ) WITH ( 'connector' = 'print' );";
-        bEnv.executeSql(ddl3).await();
-        bEnv.executeSql(ddl4).await();
-        sEnv.executeSql(ddl3).await();
-        sEnv.executeSql(ddl4).await();
-        sEnv.executeSql(ddl5).await();
-        bEnv.executeSql(
-                        "INSERT INTO A VALUES ('20220101', 8, 100), ('20220101', 8, 300), ('20220101', 8, 200), ('20220101', 8, 400), ('20220101', 9, 100)")
-                .await();
-        sEnv.executeSql(
-                        "INSERT INTO T5 SELECT dt, hr, price FROM ("
-                                + "  SELECT dt, hr, price, ROW_NUMBER() OVER (PARTITION BY dt, hr ORDER BY price desc) AS rn FROM A"
-                                + ") WHERE rn <= 2")
-                .await();
-        sEnv.executeSql(
-                        "INSERT INTO P SELECT dt, hr, price FROM ("
-                                + "  SELECT dt, hr, price, ROW_NUMBER() OVER (PARTITION BY dt, hr ORDER BY price desc) AS rn FROM A"
-                                + ") WHERE rn <= 2")
-                .await();
-        List<Row> result = iteratorToList(bEnv.from("T5").execute().collect());
-        System.out.println(result);
-    }
 }
