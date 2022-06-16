060577defa9f38cb3808a87385a279541880d3a3
windwheel <yuluoxinsheng@gmail.com>
2022-06-17 06:56:35 +0800

    [FLINK-27845](https://issues.apache.org/jira/browse/FLINK-27845) support Column Type Schema Convert

--- flink-table-store-core/src/main/java/org/apache/flink/table/store/file/data/DataFileReader.java
+++ flink-table-store-core/src/main/java/org/apache/flink/table/store/file/data/DataFileReader.java
@@ -22,6 +22,7 @@ import org.apache.flink.connector.file.src.FileSourceSplit;
 import org.apache.flink.connector.file.src.reader.BulkFormat;
 import org.apache.flink.connector.file.src.util.RecordAndPosition;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.connector.Projection;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.data.binary.BinaryRowData;
@@ -31,11 +32,33 @@ import org.apache.flink.table.store.file.format.FileFormat;
 import org.apache.flink.table.store.file.utils.FileStorePathFactory;
 import org.apache.flink.table.store.file.utils.FileUtils;
 import org.apache.flink.table.store.file.utils.RecordReader;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.logical.BinaryType;
+import org.apache.flink.table.types.logical.CharType;
+import org.apache.flink.table.types.logical.DecimalType;
+import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.LogicalTypeRoot;
 import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.VarBinaryType;
+import org.apache.flink.table.types.logical.VarCharType;
+import org.apache.flink.table.types.logical.utils.LogicalTypeCasts;
+import org.apache.flink.table.types.utils.LogicalTypeDataTypeConverter;
 
 import javax.annotation.Nullable;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.apache.flink.table.types.logical.LogicalTypeFamily.APPROXIMATE_NUMERIC;
+import static org.apache.flink.table.types.logical.LogicalTypeFamily.EXACT_NUMERIC;
+import static org.apache.flink.table.types.logical.LogicalTypeRoot.BINARY;
+import static org.apache.flink.table.types.logical.LogicalTypeRoot.CHAR;
+import static org.apache.flink.table.types.logical.LogicalTypeRoot.TIMESTAMP_WITHOUT_TIME_ZONE;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.getLength;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.getPrecision;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.getScale;
 
 /**
  * Reads {@link KeyValue}s from data files.
@@ -139,6 +162,7 @@ public class DataFileReader {
             this.keyProjection = Projection.range(0, keyType.getFieldCount()).toNestedIndexes();
             this.valueProjection = Projection.range(0, valueType.getFieldCount()).toNestedIndexes();
             applyProjection();
+            translateProjection();
         }
 
         public Factory withKeyProjection(int[][] projection) {
@@ -167,6 +191,107 @@ public class DataFileReader {
         private void applyProjection() {
             projectedKeyType = (RowType) Projection.of(keyProjection).project(keyType);
             projectedValueType = (RowType) Projection.of(valueProjection).project(valueType);
+        }
+        private void translateProjection(){
+            projectedKeyType = translateOldToNewByRowType(projectedKeyType);
+            projectedValueType = translateOldToNewByRowType(projectedValueType);
+        }
+    
+        private RowType translateOldToNewByRowType(RowType oldData) {
+            List<RowType.RowField> newData = oldData.getFields().stream().map(x -> {
+                DataType newDataType = Objects.requireNonNull(findDataTypeOfRoot(LogicalTypeDataTypeConverter.toDataType(x.getType()), x.getType().getTypeRoot()));
+                LogicalType newLogicalType =  LogicalTypeDataTypeConverter.toLogicalType(newDataType);
+                return new RowType.RowField(x.getName(), newLogicalType);
+            }).collect(Collectors.toList());
+            return new RowType(newData);
+        }
+    
+        /**
+         * Returns a data type for the given data type and expected root.
+         *
+         * <p>This method is aligned with {@link LogicalTypeCasts#supportsImplicitCast(LogicalType,
+         * LogicalType)}.
+         *
+         * <p>The "fallback" data type for each root represents the default data type for a NULL
+         * literal. NULL literals will receive the smallest precision possible for having little impact
+         * when finding a common type. The output of this method needs to be checked again if an
+         * implicit cast is supported.
+         */
+        private static @Nullable DataType findDataTypeOfRoot(
+            DataType actualDataType, LogicalTypeRoot expectedRoot) {
+            final LogicalType actualType = actualDataType.getLogicalType();
+            if (actualType.is(expectedRoot)) {
+                return actualDataType;
+            }
+            switch (expectedRoot) {
+                case CHAR:
+                    return DataTypes.CHAR(CharType.DEFAULT_LENGTH);
+                case VARCHAR:
+                    if (actualType.is(CHAR)) {
+                        return DataTypes.VARCHAR(getLength(actualType));
+                    }
+                    return DataTypes.VARCHAR(VarCharType.DEFAULT_LENGTH);
+                case BOOLEAN:
+                    return DataTypes.BOOLEAN();
+                case BINARY:
+                    return DataTypes.BINARY(BinaryType.DEFAULT_LENGTH);
+                case VARBINARY:
+                    if (actualType.is(BINARY)) {
+                        return DataTypes.VARBINARY(getLength(actualType));
+                    }
+                    return DataTypes.VARBINARY(VarBinaryType.DEFAULT_LENGTH);
+                case DECIMAL:
+                    if (actualType.is(EXACT_NUMERIC)) {
+                        return DataTypes.DECIMAL(getPrecision(actualType), getScale(actualType));
+                    } else if (actualType.is(APPROXIMATE_NUMERIC)) {
+                        final int precision = getPrecision(actualType);
+                        // we don't know where the precision occurs (before or after the dot)
+                        return DataTypes.DECIMAL(precision * 2, precision);
+                    }
+                    return DataTypes.DECIMAL(DecimalType.MIN_PRECISION, DecimalType.MIN_SCALE);
+                case TINYINT:
+                    return DataTypes.TINYINT();
+                case SMALLINT:
+                    return DataTypes.SMALLINT();
+                case INTEGER:
+                    return DataTypes.INT();
+                case BIGINT:
+                    return DataTypes.BIGINT();
+                case FLOAT:
+                    return DataTypes.FLOAT();
+                case DOUBLE:
+                    return DataTypes.DOUBLE();
+                case DATE:
+                    return DataTypes.DATE();
+                case TIME_WITHOUT_TIME_ZONE:
+                    if (actualType.is(TIMESTAMP_WITHOUT_TIME_ZONE)) {
+                        return DataTypes.TIME(getPrecision(actualType));
+                    }
+                    return DataTypes.TIME();
+                case TIMESTAMP_WITHOUT_TIME_ZONE:
+                    return DataTypes.TIMESTAMP();
+                case TIMESTAMP_WITH_TIME_ZONE:
+                    return DataTypes.TIMESTAMP_WITH_TIME_ZONE();
+                case TIMESTAMP_WITH_LOCAL_TIME_ZONE:
+                    return DataTypes.TIMESTAMP_WITH_LOCAL_TIME_ZONE();
+                case INTERVAL_YEAR_MONTH:
+                    return DataTypes.INTERVAL(DataTypes.MONTH());
+                case INTERVAL_DAY_TIME:
+                    return DataTypes.INTERVAL(DataTypes.SECOND());
+                case NULL:
+                    return DataTypes.NULL();
+                case ARRAY:
+                case MULTISET:
+                case MAP:
+                case ROW:
+                case DISTINCT_TYPE:
+                case STRUCTURED_TYPE:
+                case RAW:
+                case SYMBOL:
+                case UNRESOLVED:
+                default:
+                    return null;
+            }
         }
     }
 }
