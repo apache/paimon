763debb70c54720f6615bc17e9f74b35c0a3f837
SteNicholas <programgeek@163.com>
2022-11-15 14:00:21 +0800

    [FLINK-28552] GenerateUtils#generateCompare supports MULTISET and MAP

--- flink-table-store-format/src/main/java/org/apache/flink/table/store/format/orc/OrcFileFormat.java
+++ flink-table-store-format/src/main/java/org/apache/flink/table/store/format/orc/OrcFileFormat.java
@@ -113,10 +113,10 @@ public class OrcFileFormat extends FileFormat {
      */
     @Override
     public BulkWriter.Factory<RowData> createWriterFactory(RowType type) {
-        LogicalType[] orcTypes = refineLogicalType(type).getChildren().toArray(new LogicalType[0]);
+        LogicalType refinedType = refineLogicalType(type);
+        LogicalType[] orcTypes = refinedType.getChildren().toArray(new LogicalType[0]);
 
-        TypeDescription typeDescription =
-                OrcSplitReaderUtil.logicalTypeToOrcType(refineLogicalType(type));
+        TypeDescription typeDescription = OrcSplitReaderUtil.logicalTypeToOrcType(refinedType);
         Vectorizer<RowData> vectorizer =
                 new RowDataVectorizer(typeDescription.toString(), orcTypes);
 
