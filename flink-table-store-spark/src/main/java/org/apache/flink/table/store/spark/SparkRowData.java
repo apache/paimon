0372a4566680af0581c22f8838851de585c1607a
Jingsong Lee <jingsonglee0@gmail.com>
2022-11-30 15:57:25 +0800

    Update flink-table-store-spark/src/main/java/org/apache/flink/table/store/spark/SparkRowData.java
    Co-authored-by: Nicholas Jiang <programgeek@163.com>

--- flink-table-store-spark/src/main/java/org/apache/flink/table/store/spark/SparkRowData.java
+++ flink-table-store-spark/src/main/java/org/apache/flink/table/store/spark/SparkRowData.java
@@ -282,7 +282,8 @@ public class SparkRowData implements RowData {
 
         @Override
         public <T> RawValueData<T> getRawValue(int i) {
-            throw new UnsupportedOperationException();
+            throw new UnsupportedOperationException(
+                "Raw value is not supported in Spark, please use SQL types.");
         }
 
         @Override
