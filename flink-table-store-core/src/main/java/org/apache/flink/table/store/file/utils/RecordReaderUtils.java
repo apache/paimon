46ad620aac00d8c85e6469ca1e203ead9f934af7
JingsongLi <lzljs3620320@aliyun.com>
2022-12-02 16:19:56 +0800

    [FLINK-30273] Introduce RecordReaderUtils.transform to transform RecordReader

--- flink-table-store-core/src/main/java/org/apache/flink/table/store/file/utils/RecordReaderUtils.java
+++ flink-table-store-core/src/main/java/org/apache/flink/table/store/file/utils/RecordReaderUtils.java
@@ -20,6 +20,7 @@ package org.apache.flink.table.store.file.utils;
 
 import java.io.IOException;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /** Utils for {@link RecordReader}. */
 public class RecordReaderUtils {
@@ -43,5 +44,50 @@ public class RecordReaderUtils {
         } finally {
             reader.close();
         }
+    }
+
+    /**
+     * Returns a {@link RecordReader} that applies {@code function} to each element of {@code
+     * fromReader}.
+     */
+    public static <L, R> RecordReader<R> transform(
+            RecordReader<L> fromReader, Function<L, R> function) {
+        return new RecordReader<R>() {
+            @Override
+            public RecordIterator<R> readBatch() throws IOException {
+                RecordIterator<L> iterator = fromReader.readBatch();
+                if (iterator == null) {
+                    return null;
+                }
+                return transform(iterator, function);
+            }
+
+            @Override
+            public void close() throws IOException {
+                fromReader.close();
+            }
+        };
+    }
+
+    /**
+     * Returns an iterator that applies {@code function} to each element of {@code fromIterator}.
+     */
+    public static <L, R> RecordReader.RecordIterator<R> transform(
+            RecordReader.RecordIterator<L> fromIterator, Function<L, R> function) {
+        return new RecordReader.RecordIterator<R>() {
+            @Override
+            public R next() throws IOException {
+                L next = fromIterator.next();
+                if (next == null) {
+                    return null;
+                }
+                return function.apply(next);
+            }
+
+            @Override
+            public void releaseBatch() {
+                fromIterator.releaseBatch();
+            }
+        };
     }
 }
