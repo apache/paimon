9b77c9e5c7b3c7dc804271c1ebb3ace7b80baf2c
yuzelin <33053040+yuzelin@users.noreply.github.com>
2023-01-04 09:42:59 +0800

    [FLINK-30522] Fix 'SHOW TBLPROPERTIES' can't read properties of table in Spark3
    This closes #451

--- flink-table-store-spark/src/test/java/org/apache/flink/table/store/spark/SparkSchemaEvolutionITCase.java
+++ flink-table-store-spark/src/test/java/org/apache/flink/table/store/spark/SparkSchemaEvolutionITCase.java
@@ -661,4 +661,10 @@ public class SparkSchemaEvolutionITCase extends SparkReadTestBase {
                 "TBLPROPERTIES(\n" + "  'file.format' = 'avro',\n" + "  'path' = '%s/%s')\n",
                 warehousePath, tablePath);
     }
+
+    private String buildTableProperties(String tablePath) {
+        return String.format(
+                "TBLPROPERTIES(\n" + "  'file.format' = 'avro',\n" + "  'path' = '%s/%s')\n",
+                warehousePath, tablePath);
+    }
 }
