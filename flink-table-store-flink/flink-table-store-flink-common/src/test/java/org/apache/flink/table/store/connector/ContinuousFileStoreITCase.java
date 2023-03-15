02807897b252085adb9ce2cc6d937dca628d64a9
Kerwin <37063904+zhuangchong@users.noreply.github.com>
2023-03-15 09:11:57 +0800

    [FLINK-31438] Improved exception assertion for ContinuousFileStoreITCase
    This closes #599

--- flink-table-store-flink/flink-table-store-flink-common/src/test/java/org/apache/flink/table/store/connector/ContinuousFileStoreITCase.java
+++ flink-table-store-flink/flink-table-store-flink-common/src/test/java/org/apache/flink/table/store/connector/ContinuousFileStoreITCase.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.table.store.connector;
 
+import org.apache.flink.table.api.ValidationException;
 import org.apache.flink.table.store.file.Snapshot;
 import org.apache.flink.table.store.file.utils.BlockingIterator;
 import org.apache.flink.table.store.file.utils.SnapshotManager;
@@ -196,7 +197,9 @@ public class ContinuousFileStoreITCase extends CatalogITCaseBase {
                         () ->
                                 streamSqlIter(
                                         "SELECT * FROM T1 /*+ OPTIONS('log.scan'='from-timestamp') */"))
-                .hasMessageContaining("Unable to create a source for reading table");
+                .hasCauseInstanceOf(IllegalArgumentException.class)
+                .hasRootCauseMessage(
+                        "scan.timestamp-millis can not be null when you use from-timestamp for scan.mode");
     }
 
     @TestTemplate
@@ -219,7 +222,9 @@ public class ContinuousFileStoreITCase extends CatalogITCaseBase {
                                 streamSqlIter(
                                         "SELECT * FROM T1 /*+ OPTIONS('log.scan'='latest', 'log.scan.timestamp-millis'='%s') */",
                                         0))
-                .hasMessageContaining("Unable to create a source for reading table");
+                .hasCauseInstanceOf(IllegalArgumentException.class)
+                .hasRootCauseMessage(
+                        "scan.timestamp-millis must be null when you use latest for scan.mode");
     }
 
     @TestTemplate
@@ -250,7 +255,9 @@ public class ContinuousFileStoreITCase extends CatalogITCaseBase {
                                 streamSqlIter(
                                         "SELECT * FROM T1 /*+ OPTIONS('scan.mode'='latest', 'scan.snapshot-id'='%s') */",
                                         0))
-                .hasMessageContaining("Unable to create a source for reading table");
+                .hasCauseInstanceOf(IllegalArgumentException.class)
+                .hasRootCauseMessage(
+                        "scan.snapshot-id must be null when you use latest for scan.mode");
     }
 
     @TestTemplate
@@ -272,18 +279,22 @@ public class ContinuousFileStoreITCase extends CatalogITCaseBase {
     @TestTemplate
     public void testUnsupportedUpsert() {
         assertThatThrownBy(
-                () ->
-                        streamSqlIter(
-                                "SELECT * FROM T1 /*+ OPTIONS('log.changelog-mode'='upsert') */"),
-                "File store continuous reading dose not support upsert changelog mode");
+                        () ->
+                                streamSqlIter(
+                                        "SELECT * FROM T1 /*+ OPTIONS('log.changelog-mode'='upsert') */"))
+                .hasCauseInstanceOf(ValidationException.class)
+                .hasRootCauseMessage(
+                        "File store continuous reading dose not support upsert changelog mode.");
     }
 
     @TestTemplate
     public void testUnsupportedEventual() {
         assertThatThrownBy(
-                () ->
-                        streamSqlIter(
-                                "SELECT * FROM T1 /*+ OPTIONS('log.consistency'='eventual') */"),
-                "File store continuous reading dose not support eventual consistency mode");
+                        () ->
+                                streamSqlIter(
+                                        "SELECT * FROM T1 /*+ OPTIONS('log.consistency'='eventual') */"))
+                .hasCauseInstanceOf(ValidationException.class)
+                .hasRootCauseMessage(
+                        "File store continuous reading dose not support eventual consistency mode.");
     }
 }
