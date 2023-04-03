2b92827d58d2f2aff6174847fd2f6f0a207d1d26
Kerwin <37063904+zhuangchong@users.noreply.github.com>
2023-04-03 10:31:57 +0800

    [hotfix] Add flink1.17 e2e test ci check (#804)

--- paimon-hive/paimon-hive-connector-2.3/src/test/java/org/apache/paimon/hive/Hive23CatalogITCase.java
+++ paimon-hive/paimon-hive-connector-2.3/src/test/java/org/apache/paimon/hive/Hive23CatalogITCase.java
@@ -138,11 +138,9 @@ public class Hive23CatalogITCase extends HiveCatalogITCaseBase {
         assertThatThrownBy(() -> tEnv.executeSql("ALTER TABLE alter_failed_table SET ('aa'='bb')"))
                 .isInstanceOf(TableException.class)
                 .hasMessage(
-                        String.format(
-                                "Could not execute "
-                                        + "ALTER TABLE my_alter_hive.default.alter_failed_table "
-                                        + "SET (aa: [bb], path: [%sdefault.db/alter_failed_table])",
-                                path));
+                        "Could not execute "
+                                + "ALTER TABLE my_alter_hive.default.alter_failed_table\n"
+                                + "  SET 'aa' = 'bb'");
 
         assertTrue(
                 new SchemaManager(
