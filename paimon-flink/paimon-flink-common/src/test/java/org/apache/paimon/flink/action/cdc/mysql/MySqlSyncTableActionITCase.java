f5695bf671499b4334823b4413f106d34e3f2898
tsreaper <tsreaper96@gmail.com>
2023-04-14 14:44:26 +0800

    [flink] Introduce MySqlSyncDatabaseAction (#904)

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/action/cdc/mysql/MySqlSyncTableActionITCase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/action/cdc/mysql/MySqlSyncTableActionITCase.java
@@ -22,11 +22,8 @@ import org.apache.paimon.catalog.Catalog;
 import org.apache.paimon.catalog.CatalogContext;
 import org.apache.paimon.catalog.CatalogFactory;
 import org.apache.paimon.catalog.Identifier;
-import org.apache.paimon.flink.action.ActionITCaseBase;
 import org.apache.paimon.fs.Path;
 import org.apache.paimon.table.FileStoreTable;
-import org.apache.paimon.table.source.TableScan;
-import org.apache.paimon.types.DataField;
 import org.apache.paimon.types.DataType;
 import org.apache.paimon.types.DataTypes;
 import org.apache.paimon.types.RowType;
@@ -35,65 +32,26 @@ import org.apache.flink.api.common.JobStatus;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.core.execution.JobClient;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.lifecycle.Startables;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /** IT cases for {@link MySqlSyncTableAction}. */
-public class MySqlSyncTableActionITCase extends ActionITCaseBase {
+public class MySqlSyncTableActionITCase extends MySqlActionITCaseBase {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MySqlSyncTableActionITCase.class);
-
-    private static final MySqlContainer MYSQL_CONTAINER = createMySqlContainer(MySqlVersion.V5_7);
-    private static final String USER = "paimonuser";
-    private static final String PASSWORD = "paimonpw";
-    private static final String DATABASE_NAME = "paimon_test";
-
-    @BeforeAll
-    public static void startContainers() {
-        LOG.info("Starting containers...");
-        Startables.deepStart(Stream.of(MYSQL_CONTAINER)).join();
-        LOG.info("Containers are started.");
-    }
-
-    @AfterAll
-    public static void stopContainers() {
-        LOG.info("Stopping containers...");
-        MYSQL_CONTAINER.stop();
-        LOG.info("Containers are stopped.");
-    }
-
-    private static MySqlContainer createMySqlContainer(MySqlVersion version) {
-        return (MySqlContainer)
-                new MySqlContainer(version)
-                        .withConfigurationOverride("mysql/my.cnf")
-                        .withSetupSQL("mysql/setup.sql")
-                        .withUsername(USER)
-                        .withPassword(PASSWORD)
-                        .withDatabaseName(DATABASE_NAME)
-                        .withLogConsumer(new Slf4jLogConsumer(LOG));
-    }
+    private static final String DATABASE_NAME = "paimon_sync_table";
 
     @Test
     @Timeout(60)
@@ -133,9 +91,7 @@ public class MySqlSyncTableActionITCase extends ActionITCaseBase {
 
         try (Connection conn =
                 DriverManager.getConnection(
-                        String.format(
-                                "jdbc:mysql://%s:%s/",
-                                MYSQL_CONTAINER.getHost(), MYSQL_CONTAINER.getDatabasePort()),
+                        MYSQL_CONTAINER.getJdbcUrl(DATABASE_NAME),
                         MYSQL_CONTAINER.getUsername(),
                         MYSQL_CONTAINER.getPassword())) {
             try (Statement statement = conn.createStatement()) {
@@ -146,7 +102,7 @@ public class MySqlSyncTableActionITCase extends ActionITCaseBase {
 
     private void testSchemaEvolutionImpl(Statement statement) throws Exception {
         FileStoreTable table = getFileStoreTable();
-        statement.executeUpdate("USE paimon_test");
+        statement.executeUpdate("USE paimon_sync_table");
 
         statement.executeUpdate("INSERT INTO schema_evolution_1 VALUES (1, 1, 'one')");
         statement.executeUpdate(
@@ -571,58 +527,6 @@ public class MySqlSyncTableActionITCase extends ActionITCaseBase {
                         "Primary keys are not specified. "
                                 + "Also, can't infer primary keys from MySQL table schemas because "
                                 + "MySQL tables have no primary keys or have different primary keys.");
-    }
-
-    private void waitForResult(
-            List<String> expected, FileStoreTable table, RowType rowType, List<String> primaryKeys)
-            throws Exception {
-        assertThat(table.schema().primaryKeys()).isEqualTo(primaryKeys);
-
-        // wait for table schema to become our expected schema
-        while (true) {
-            int cnt = 0;
-            for (int i = 0; i < table.schema().fields().size(); i++) {
-                DataField field = table.schema().fields().get(i);
-                boolean sameName = field.name().equals(rowType.getFieldNames().get(i));
-                boolean sameType = field.type().equals(rowType.getFieldTypes().get(i));
-                if (sameName && sameType) {
-                    cnt++;
-                }
-            }
-            if (cnt == rowType.getFieldCount()) {
-                break;
-            }
-            table = table.copyWithLatestSchema();
-            Thread.sleep(1000);
-        }
-
-        // wait for data to become expected
-        List<String> sortedExpected = new ArrayList<>(expected);
-        Collections.sort(sortedExpected);
-        while (true) {
-            TableScan.Plan plan = table.newScan().plan();
-            List<String> result =
-                    getResult(
-                            table.newRead(),
-                            plan == null ? Collections.emptyList() : plan.splits(),
-                            rowType);
-            List<String> sortedActual = new ArrayList<>(result);
-            Collections.sort(sortedActual);
-            if (sortedExpected.equals(sortedActual)) {
-                break;
-            }
-            Thread.sleep(1000);
-        }
-    }
-
-    private Map<String, String> getBasicMySqlConfig() {
-        Map<String, String> config = new HashMap<>();
-        config.put("hostname", MYSQL_CONTAINER.getHost());
-        config.put("port", String.valueOf(MYSQL_CONTAINER.getDatabasePort()));
-        config.put("username", USER);
-        config.put("password", PASSWORD);
-        config.put("server-time-zone", ZoneId.of("+00:00").toString());
-        return config;
     }
 
     private FileStoreTable getFileStoreTable() throws Exception {
