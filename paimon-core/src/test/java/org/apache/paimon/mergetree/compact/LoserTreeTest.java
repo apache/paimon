ffd75ccd5f7ad063367367a5a4241163997fc806
含风 <qidian.qd@alibaba-inc.com>
2023-05-10 21:29:53 +0800

    Fix tests in LoserTreeTest (#1120)

--- paimon-core/src/test/java/org/apache/paimon/mergetree/compact/LoserTreeTest.java
+++ paimon-core/src/test/java/org/apache/paimon/mergetree/compact/LoserTreeTest.java
@@ -38,27 +38,62 @@ import static org.assertj.core.api.Assertions.assertThat;
 /** Test for {@link LoserTree}. */
 public class LoserTreeTest {
     private static final Comparator<KeyValue> KEY_COMPARATOR =
-            Comparator.comparingInt(o -> o.key().getInt(0));
+            (c1, c2) -> Integer.compare(c2.key().getInt(0), c1.key().getInt(0));
     private static final Comparator<KeyValue> SEQUENCE_COMPARATOR =
-            Comparator.comparingLong(KeyValue::sequenceNumber);
+            (c1, c2) -> Long.compare(c2.sequenceNumber(), c1.sequenceNumber());
 
     @RepeatedTest(100)
     public void testLoserTreeIsOrdered() throws IOException {
-        List<ReusingTestData> reusingTestData = ReusingTestData.generateData(1000, false);
+        List<ReusingTestData> initialTestData = ReusingTestData.generateData(1000, false);
+        List<ReusingTestData> finalDedupedTestData = new ArrayList<>();
+
         List<RecordReader<KeyValue>> sortedTestReaders = new ArrayList<>();
         ThreadLocalRandom random = ThreadLocalRandom.current();
         int numberReaders = random.nextInt(20) + 1;
-        int lowerBound = 0, upperBound = reusingTestData.size();
+        int lowerBound = 0, upperBound = initialTestData.size();
         for (int i = 0; i < numberReaders; i++) {
-            int subUpperBound = random.nextInt(lowerBound, upperBound);
+            int subUpperBound;
+            if (i == numberReaders - 1) {
+                subUpperBound = upperBound;
+            } else {
+                subUpperBound = random.nextInt(lowerBound, upperBound);
+            }
             List<ReusingTestData> subReusingTestData =
-                    reusingTestData.subList(lowerBound, subUpperBound);
+                    new ArrayList<>(initialTestData.subList(lowerBound, subUpperBound));
             Collections.sort(subReusingTestData);
+            // for each reader, there should not be two records with duplicate key
+            dedup(subReusingTestData, 1);
             sortedTestReaders.add(new TestReusingRecordReader(subReusingTestData));
+            finalDedupedTestData.addAll(subReusingTestData);
             lowerBound = subUpperBound;
         }
-        Collections.sort(reusingTestData);
-        checkLoserTree(sortedTestReaders, reusingTestData);
+        Collections.sort(finalDedupedTestData);
+        checkLoserTree(sortedTestReaders, finalDedupedTestData);
+    }
+
+    /**
+     * @param list list of ReusingTestData
+     * @param n max number of occurrence of record with the same key
+     */
+    private static List<ReusingTestData> dedup(List<ReusingTestData> list, int n) {
+        if (list.size() == 0) {
+            return list;
+        }
+        int count = 1;
+        int currentKey = list.get(0).key;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).key == currentKey) {
+                count++;
+                if (count > n) {
+                    list.remove(i);
+                    i--;
+                }
+            } else {
+                currentKey = list.get(i).key;
+                count = 1;
+            }
+        }
+        return list;
     }
 
     private void checkLoserTree(
@@ -66,16 +101,26 @@ public class LoserTreeTest {
             throws IOException {
         try (LoserTree<KeyValue> loserTree =
                 new LoserTree<>(sortedTestReaders, KEY_COMPARATOR, SEQUENCE_COMPARATOR)) {
+            List<Object> popped = new ArrayList<>();
             Iterator<ReusingTestData> expectedIterator = expectedData.iterator();
+            loserTree.initializeIfNeeded();
             do {
-                loserTree.adjustForNextLoop();
+                if (loserTree.peekWinner() == null) {
+                    loserTree.adjustForNextLoop();
+                    assertThat(loserTree.peekWinner() != null);
+                }
                 for (KeyValue winner = loserTree.popWinner();
                         winner != null;
                         winner = loserTree.popWinner()) {
+                    popped.add(winner);
                     assertThat(expectedIterator.hasNext());
-                    expectedIterator.next().assertEquals(winner);
+
+                    ReusingTestData expected = expectedIterator.next();
+
+                    expected.assertEquals(winner);
                 }
-            } while (loserTree.peekWinner() != null && expectedIterator.hasNext());
+                loserTree.adjustForNextLoop();
+            } while (expectedIterator.hasNext());
         }
     }
 }
