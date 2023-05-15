c0a14b77465728dbbf03cb9ad4d75b81097727f4
Weijie Guo <reswqa@163.com>
2023-05-16 02:02:13 +0800

    [test] Enable JUnit 5 Automatic Extension Detection

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/util/AbstractTestBase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/util/AbstractTestBase.java
@@ -18,14 +18,12 @@
 
 package org.apache.paimon.flink.util;
 
-import org.apache.paimon.testutils.junit.TestLoggerExtension;
 import org.apache.paimon.utils.FileIOUtils;
 
 import org.apache.flink.client.program.ClusterClient;
 import org.apache.flink.runtime.client.JobStatusMessage;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -35,7 +33,6 @@ import java.nio.file.Path;
 import java.util.UUID;
 
 /** Similar to Flink's AbstractTestBase but using Junit5. */
-@ExtendWith({TestLoggerExtension.class})
 public class AbstractTestBase {
 
     private static final int DEFAULT_PARALLELISM = 8;
