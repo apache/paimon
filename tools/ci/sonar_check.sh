94a1510f55e4bbee2d03c67149df0b774d163155
Jingsong Lee <jingsonglee0@gmail.com>
2023-05-21 20:38:25 +0800

    [license] Remove maven wrapper files and add Path to license (#1178)

--- tools/ci/sonar_check.sh
+++ tools/ci/sonar_check.sh
@@ -18,7 +18,7 @@ if [ ! "$SONAR_TOKEN" ]; then
   echo "SONAR_TOKEN environment is null, skip check"
   exit 0
 fi
-./mvnw --batch-mode verify sonar:sonar \
+mvn --batch-mode verify sonar:sonar \
 -Dmaven.test.skip=true -Dsonar.host.url=https://sonarcloud.io \
 -Dsonar.organization=apache \
 -Dsonar.projectKey=apache_incubator-paimon \
