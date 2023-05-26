cc3f6239f2cc3ab44bdaff27131ae0b6e767d83c
Alibaba-HZY <daowu.hzy@cainiao.com>
2023-05-26 12:36:09 +0800

    [flink] Supports KafkaSyncTableAction

--- docs/content/how-to/cdc-ingestion.md
+++ docs/content/how-to/cdc-ingestion.md
@@ -157,3 +157,120 @@ Example
     --table-conf changelog-producer=input \
     --table-conf sink.parallelism=4
 ```
+
+## Kafka
+
+### Prepare Kafka Bundled Jar
+
+```
+flink-sql-connector-kafka-*.jar
+```
+### Supported Formats
+Flink provides several Kafka CDC formats :canal-json、debezium-json,ogg-json,maxwell-json.
+If a message in a Kafka topic is a change event captured from another database using the Change Data Capture (CDC) tool, then you can use the Paimon Kafka CDC. Write the INSERT, UPDATE, DELETE messages parsed into the paimon table.
+<table class="table table-bordered">
+    <thead>
+      <tr>
+        <th class="text-left">Formats</th>
+        <th class="text-left">Supported</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+         <td><a href="https://nightlies.apache.org/flink/flink-docs-release-1.16/zh/docs/connectors/table/formats/canal/">Canal CDC</a></td>
+          <td>True</td>
+        </tr>
+        <tr>
+         <td><a href="https://nightlies.apache.org/flink/flink-docs-release-1.16/zh/docs/connectors/table/formats/debezium/">Debezium CDC</a></td>
+         <td>False</td>
+        </tr>
+        <tr>
+         <td><a href="https://nightlies.apache.org/flink/flink-docs-release-1.16/zh/docs/connectors/table/formats/maxwell/ >}}">Maxwell CDC</a></td>
+        <td>False</td>
+        </tr>
+        <tr>
+         <td><a href="https://nightlies.apache.org/flink/flink-docs-release-1.16/zh/docs/connectors/table/formats/ogg/">OGG CDC</a></td>
+        <td>False</td>
+        </tr>
+    </tbody>
+</table>
+
+### Synchronizing Tables
+
+By using [KafkaSyncTableAction](/docs/{{< param Branch >}}/api/java/org/apache/paimon/flink/action/cdc/kafka/KafkaSyncTableAction) in a Flink DataStream job or directly through `flink run`, users can synchronize one or multiple tables from Kafka's one topic into one Paimon table.
+
+To use this feature through `flink run`, run the following shell command.
+
+```bash
+<FLINK_HOME>/bin/flink run \
+    -c org.apache.paimon.flink.action.FlinkActions \
+    /path/to/paimon-flink-**-{{< version >}}.jar \
+    kafka-sync-table
+    --warehouse <warehouse-path> \
+    --database <database-name> \
+    --table <table-name> \
+    [--partition-keys <partition-keys>] \
+    [--primary-keys <primary-keys>] \
+    [--computed-column <'column-name=expr-name(args[, ...])'> [--computed-column ...]] \
+    [--kafka-conf <kafka-source-conf> [--kafka-conf <kafka-source-conf> ...]] \
+    [--catalog-conf <paimon-catalog-conf> [--catalog-conf <paimon-catalog-conf> ...]] \
+    [--table-conf <paimon-table-sink-conf> [--table-conf <paimon-table-sink-conf> ...]]
+```
+
+* `--warehouse` is the path to Paimon warehouse.
+* `--database` is the database name in Paimon catalog.
+* `--table` is the Paimon table name.
+* `--partition-keys` are the partition keys for Paimon table. If there are multiple partition keys, connect them with comma, for example `dt,hh,mm`.
+* `--primary-keys` are the primary keys for Paimon table. If there are multiple primary keys, connect them with comma, for example `buyer_id,seller_id`.
+* `--kafka-conf` is the configuration for Flink Kafka sources. Each configuration should be specified in the format `key=value`. `properties.bootstrap.servers`, `topic`, `properties.group.id`,  and `value.format` are required configurations, others are optional. See its [document](https://nightlies.apache.org/flink/flink-docs-release-1.16/zh/docs/connectors/table/kafka/#%e8%bf%9e%e6%8e%a5%e5%99%a8%e5%8f%82%e6%95%b0) for a complete list of configurations.
+* `--catalog-conf` is the configuration for Paimon catalog. Each configuration should be specified in the format `key=value`. See [here]({{< ref "maintenance/configurations" >}}) for a complete list of catalog configurations.
+* `--table-conf` is the configuration for Paimon table sink. Each configuration should be specified in the format `key=value`. See [here]({{< ref "maintenance/configurations" >}}) for a complete list of table configurations.
+
+If the Paimon table you specify does not exist, this action will automatically create the table. Its schema will be derived from all specified Kafka topic's tables,it gets the earliest non-DDL data parsing schema from topic. If the Paimon table already exists, its schema will be compared against the schema of all specified Kafka topic's tables.
+
+This action supports a limited number of schema changes. Currently, the framework can not drop columns, so the behaviors of `DROP` will be ignored, `RENAME` will add a new column. Currently supported schema changes includes:
+
+* Adding columns.
+
+* Altering column types. More specifically,
+
+  * altering from a string type (char, varchar, text) to another string type with longer length,
+  * altering from a binary type (binary, varbinary, blob) to another binary type with longer length,
+  * altering from an integer type (tinyint, smallint, int, bigint) to another integer type with wider range,
+  * altering from a floating-point type (float, double) to another floating-point type with wider range,
+
+  are supported.
+
+Example
+
+```bash
+<FLINK_HOME>/bin/flink run \
+    -c org.apache.paimon.flink.action.FlinkActions \
+    /path/to/paimon-flink-**-{{< version >}}.jar \
+    kafka-sync-table \
+    --warehouse hdfs:///path/to/warehouse \
+    --database test_db \
+    --table test_table \
+    --partition-keys pt \
+    --primary-keys pt,uid \
+    --computed-columns '_year=year(age)' \
+    --kafka-conf properties.bootstrap.servers=127.0.0.1:9020 \
+    --kafka-conf topic=order \
+    --kafka-conf properties.group.id=123456 \
+    --kafka-conf value.format=canal-json \
+    --catalog-conf metastore=hive \
+    --catalog-conf uri=thrift://hive-metastore:9083 \
+    --table-conf bucket=4 \
+    --table-conf changelog-producer=input \
+    --table-conf sink.parallelism=4
+```
+
+## Computed Functions
+* `--computed-column` are the definitions of computed columns. The argument field is from Kafka topic's table field name. Supported expressions are:
+  * year(date-column): Extract year from a DATE, DATETIME or TIMESTAMP. Output is an INT value represent the year.
+  * substring(column,beginInclusive): Get column.substring(beginInclusive). Output is a STRING.
+  * substring(column,beginInclusive,endExclusive): Get column.substring(beginInclusive,endExclusive). Output is a STRING.
+  * truncate(column,width): truncate column by width. Output type is same with column.
+    * If the column is a STRING, truncate(column,width) will truncate the string to width characters, namely `value.substring(0, width)`.
+    * If the column is an INT or LONG, truncate(column,width) will truncate the number with the algorithm `v - (((v % W) + W) % W)`. The `redundant` compute part is to keep the result always positive.
+    * If the column is a DECIMAL, truncate(column,width) will truncate the decimal with the algorithm: let `scaled_W = decimal(W, scale(v))`, then return `v - (v % scaled_W)`.
---
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/action/cdc/kafka/KafkaCanalActionITCaseBase.java
@@ -0,0 +1,366 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.flink.action.cdc.kafka;
+
+import org.apache.paimon.flink.action.ActionITCaseBase;
+import org.apache.paimon.table.FileStoreTable;
+import org.apache.paimon.table.source.ReadBuilder;
+import org.apache.paimon.table.source.TableScan;
+import org.apache.paimon.types.DataField;
+import org.apache.paimon.types.RowType;
+import org.apache.paimon.utils.StringUtils;
+
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.flink.api.common.JobStatus;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.core.execution.JobClient;
+import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.util.DockerImageVersions;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** Base test class for {@link org.apache.paimon.flink.action.Action}s related to MySQL. */
+public class KafkaCanalActionITCaseBase extends ActionITCaseBase {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaCanalActionITCaseBase.class);
+
+    private static final String INTER_CONTAINER_KAFKA_ALIAS = "kafka";
+    private static final Network NETWORK = Network.newNetwork();
+    private static final int zkTimeoutMills = 30000;
+
+    // Timer for scheduling logging task if the test hangs
+    private final Timer loggingTimer = new Timer("Debug Logging Timer");
+
+    @RegisterExtension
+    public static final KafkaContainerExtension KAFKA_CONTAINER =
+            (KafkaContainerExtension)
+                    new KafkaContainerExtension(DockerImageName.parse(DockerImageVersions.KAFKA)) {
+                        @Override
+                        protected void doStart() {
+                            super.doStart();
+                            if (LOG.isInfoEnabled()) {
+                                this.followOutput(new Slf4jLogConsumer(LOG));
+                            }
+                        }
+                    }.withEmbeddedZookeeper()
+                            .withNetwork(NETWORK)
+                            .withNetworkAliases(INTER_CONTAINER_KAFKA_ALIAS)
+                            .withEnv(
+                                    "KAFKA_TRANSACTION_MAX_TIMEOUT_MS",
+                                    String.valueOf(Duration.ofHours(2).toMillis()))
+                            // Disable log deletion to prevent records from being deleted during
+                            // test run
+                            .withEnv("KAFKA_LOG_RETENTION_MS", "-1");
+
+    protected StreamExecutionEnvironment env;
+    protected StreamTableEnvironment tEnv;
+
+    @BeforeEach
+    public void setup() {
+        env = StreamExecutionEnvironment.getExecutionEnvironment();
+        tEnv = StreamTableEnvironment.create(env);
+        env.getConfig().setRestartStrategy(RestartStrategies.noRestart());
+        tEnv.getConfig()
+                .getConfiguration()
+                .set(ExecutionCheckpointingOptions.ENABLE_UNALIGNED, false);
+
+        // Probe Kafka broker status per 30 seconds
+        scheduleTimeoutLogger(
+                Duration.ofSeconds(30),
+                () -> {
+                    // List all non-internal topics
+                    final Map<String, TopicDescription> topicDescriptions =
+                            describeExternalTopics();
+                    LOG.info("Current existing topics: {}", topicDescriptions.keySet());
+
+                    // Log status of topics
+                    logTopicPartitionStatus(topicDescriptions);
+                });
+    }
+
+    @AfterEach
+    public void after() throws ExecutionException, InterruptedException {
+        // Cancel timer for debug logging
+        cancelTimeoutLogger();
+        // Delete topics for avoid reusing topics of Kafka cluster
+        deleteTopics();
+    }
+
+    private void deleteTopics() throws ExecutionException, InterruptedException {
+        final AdminClient adminClient = AdminClient.create(getStandardProps());
+        adminClient.deleteTopics(adminClient.listTopics().names().get()).all().get();
+    }
+
+    // ------------------------ For Debug Logging Purpose ----------------------------------
+
+    private void scheduleTimeoutLogger(Duration period, Runnable loggingAction) {
+        TimerTask timeoutLoggerTask =
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            loggingAction.run();
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to execute logging action", e);
+                        }
+                    }
+                };
+        loggingTimer.schedule(timeoutLoggerTask, 0L, period.toMillis());
+    }
+
+    private void cancelTimeoutLogger() {
+        loggingTimer.cancel();
+    }
+
+    private Map<String, TopicDescription> describeExternalTopics() {
+        try (final AdminClient adminClient = AdminClient.create(getStandardProps())) {
+            final List<String> topics =
+                    adminClient.listTopics().listings().get().stream()
+                            .filter(listing -> !listing.isInternal())
+                            .map(TopicListing::name)
+                            .collect(Collectors.toList());
+
+            return adminClient.describeTopics(topics).all().get();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to list Kafka topics", e);
+        }
+    }
+
+    private void logTopicPartitionStatus(Map<String, TopicDescription> topicDescriptions) {
+        final Properties properties = getStandardProps();
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "flink-tests-debugging");
+        properties.setProperty(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getCanonicalName());
+        properties.setProperty(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getCanonicalName());
+        final KafkaConsumer<?, ?> consumer = new KafkaConsumer<String, String>(properties);
+        List<TopicPartition> partitions = new ArrayList<>();
+        topicDescriptions.forEach(
+                (topic, description) ->
+                        description
+                                .partitions()
+                                .forEach(
+                                        tpInfo ->
+                                                partitions.add(
+                                                        new TopicPartition(
+                                                                topic, tpInfo.partition()))));
+        final Map<TopicPartition, Long> beginningOffsets = consumer.beginningOffsets(partitions);
+        final Map<TopicPartition, Long> endOffsets = consumer.endOffsets(partitions);
+        partitions.forEach(
+                partition ->
+                        LOG.info(
+                                "TopicPartition \"{}\": starting offset: {}, stopping offset: {}",
+                                partition,
+                                beginningOffsets.get(partition),
+                                endOffsets.get(partition)));
+    }
+
+    protected void waitForResult(
+            List<String> expected, FileStoreTable table, RowType rowType, List<String> primaryKeys)
+            throws Exception {
+        assertThat(table.schema().primaryKeys()).isEqualTo(primaryKeys);
+
+        // wait for table schema to become our expected schema
+        while (true) {
+            if (rowType.getFieldCount() == table.schema().fields().size()) {
+                int cnt = 0;
+                for (int i = 0; i < table.schema().fields().size(); i++) {
+                    DataField field = table.schema().fields().get(i);
+                    boolean sameName = field.name().equals(rowType.getFieldNames().get(i));
+                    boolean sameType = field.type().equals(rowType.getFieldTypes().get(i));
+                    if (sameName && sameType) {
+                        cnt++;
+                    }
+                }
+                if (cnt == rowType.getFieldCount()) {
+                    break;
+                }
+            }
+            table = table.copyWithLatestSchema();
+            Thread.sleep(1000);
+        }
+
+        // wait for data to become expected
+        List<String> sortedExpected = new ArrayList<>(expected);
+        Collections.sort(sortedExpected);
+        while (true) {
+            ReadBuilder readBuilder = table.newReadBuilder();
+            TableScan.Plan plan = readBuilder.newScan().plan();
+            List<String> result =
+                    getResult(
+                            readBuilder.newRead(),
+                            plan == null ? Collections.emptyList() : plan.splits(),
+                            rowType);
+            List<String> sortedActual = new ArrayList<>(result);
+            Collections.sort(sortedActual);
+            if (sortedExpected.equals(sortedActual)) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    public static Properties getStandardProps() {
+        Properties standardProps = new Properties();
+        standardProps.put("bootstrap.servers", KAFKA_CONTAINER.getBootstrapServers());
+        standardProps.put("group.id", "paimon-tests");
+        standardProps.put("enable.auto.commit", false);
+        standardProps.put("auto.offset.reset", "earliest");
+        standardProps.put("max.partition.fetch.bytes", 256);
+        standardProps.put("zookeeper.session.timeout.ms", zkTimeoutMills);
+        standardProps.put("zookeeper.connection.timeout.ms", zkTimeoutMills);
+        return standardProps;
+    }
+
+    public String getBootstrapServers() {
+        return KAFKA_CONTAINER.getBootstrapServers();
+    }
+
+    protected Map<String, String> getBasicKafkaConfig() {
+        Map<String, String> config = new HashMap<>();
+        config.put("properties.bootstrap.servers", KAFKA_CONTAINER.getBootstrapServers());
+        config.put("properties.group.id", "paimon-tests");
+        config.put("properties.enable.auto.commit", "false");
+        config.put("properties.auto.offset.reset", "earliest");
+        return config;
+    }
+
+    public void createTestTopic(String topic, int numPartitions, int replicationFactor) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, getBootstrapServers());
+        try (AdminClient admin = AdminClient.create(properties)) {
+            admin.createTopics(
+                            Collections.singletonList(
+                                    new NewTopic(topic, numPartitions, (short) replicationFactor)))
+                    .all()
+                    .get();
+        } catch (Exception e) {
+            throw new IllegalStateException(
+                    String.format(
+                            "Fail to create topic [%s partitions: %d replication factor: %d].",
+                            topic, numPartitions, replicationFactor),
+                    e);
+        }
+    }
+
+    public static List<String> readLines(String resource) throws IOException {
+        final URL url =
+                KafkaCanalSyncTableActionITCase.class.getClassLoader().getResource(resource);
+        assertThat(url).isNotNull();
+        java.nio.file.Path path = new File(url.getFile()).toPath();
+        return Files.readAllLines(path);
+    }
+
+    void writeRecordsToKafka(String topic, List<String> lines) throws Exception {
+        Properties producerProperties = getStandardProps();
+        producerProperties.setProperty("retries", "0");
+        producerProperties.put(
+                "key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.put(
+                "value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer kafkaProducer = new KafkaProducer(producerProperties);
+        for (int i = 0; i < lines.size(); i++) {
+            try {
+                JsonNode jsonNode = objectMapper.readTree(lines.get(i));
+                if (!StringUtils.isEmpty(lines.get(i))) {
+                    kafkaProducer.send(new ProducerRecord<>(topic, lines.get(i)));
+                }
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+
+        kafkaProducer.close();
+    }
+
+    /** Kafka container extension for junit5. */
+    private static class KafkaContainerExtension extends KafkaContainer
+            implements BeforeAllCallback, AfterAllCallback {
+        private KafkaContainerExtension(DockerImageName dockerImageName) {
+            super(dockerImageName);
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            this.doStart();
+        }
+
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            this.close();
+        }
+    }
+
+    protected void waitJobRunning(JobClient client) throws Exception {
+        while (true) {
+            JobStatus status = client.getJobStatus().get();
+            if (status == JobStatus.RUNNING) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+    }
+}
