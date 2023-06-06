7542b237d9fd6d751962a542cb7a0f7a407ab39b
yejunhao <yejunhao12138@gmail.com>
2023-06-06 10:40:57 +0800

    [core] some non-bucket compaction class

---
+++ paimon-core/src/main/java/org/apache/paimon/append/AppendOnlyTableCompactionWorker.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.append;
+
+import org.apache.paimon.data.BinaryRow;
+import org.apache.paimon.operation.AppendOnlyFileStoreWrite;
+import org.apache.paimon.table.AppendOnlyFileStoreTable;
+import org.apache.paimon.table.sink.CommitMessage;
+import org.apache.paimon.utils.BucketUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/** {@link AppendOnlyFileStoreTable} compact worker. */
+public class AppendOnlyTableCompactionWorker {
+    private final List<AppendOnlyCompactionTask> tasks = new ArrayList<>();
+    private final List<CommitMessage> result = new ArrayList<>();
+    private final AppendOnlyFileStoreWrite write;
+    private final Map<BinaryRow, AppendOnlyCompactManager.CompactRewriter> rewriters =
+            new HashMap<>();
+
+    public AppendOnlyTableCompactionWorker(AppendOnlyFileStoreTable table, String commitUser) {
+        this.write = table.store().newWrite(commitUser);
+    }
+
+    public AppendOnlyTableCompactionWorker accept(List<AppendOnlyCompactionTask> tasks) {
+        this.tasks.addAll(tasks);
+        return this;
+    }
+
+    public AppendOnlyTableCompactionWorker accept(AppendOnlyCompactionTask task) {
+        this.tasks.add(task);
+        return this;
+    }
+
+    public List<CommitMessage> doCompact() throws Exception {
+        for (AppendOnlyCompactionTask task : tasks) {
+            AppendOnlyCompactManager.CompactRewriter rewriter =
+                    rewriters.computeIfAbsent(task.partition(), this::buildRewriter);
+            result.add(task.doCompact(rewriter));
+        }
+        ArrayList<CommitMessage> resultReturn = new ArrayList<>(result);
+        result.clear();
+        tasks.clear();
+        return resultReturn;
+    }
+
+    private AppendOnlyCompactManager.CompactRewriter buildRewriter(BinaryRow partition) {
+        return write.compactRewriter(partition, BucketUtils.NON_BUCKET_BUCKET);
+    }
+}
---
+++ paimon-core/src/main/java/org/apache/paimon/utils/BucketUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.utils;
+
+import org.apache.paimon.data.BinaryRow;
+import org.apache.paimon.table.NonBucketTable;
+import org.apache.paimon.table.Table;
+import org.apache.paimon.table.sink.KeyAndBucketExtractor;
+
+/** Processor for bucket, including calculate bucket num, handle non bucket, etc. */
+public class BucketUtils {
+    public static final int NON_BUCKET_NUM = -1;
+    public static final int NON_BUCKET_BUCKET = 0;
+
+    public static int calculateBucket(BinaryRow bucketKey, int bucketNum) {
+        if (checkNonBucket(bucketNum)) {
+            return NON_BUCKET_BUCKET;
+        } else {
+            return KeyAndBucketExtractor.bucket(
+                    KeyAndBucketExtractor.bucketKeyHashCode(bucketKey), bucketNum);
+        }
+    }
+
+    public static int calculateBucket(int hash, int bucketNum) {
+        if (checkNonBucket(bucketNum)) {
+            return NON_BUCKET_BUCKET;
+        } else {
+            return KeyAndBucketExtractor.bucket(hash, bucketNum);
+        }
+    }
+
+    public static boolean checkNonBucket(int bucketNum) {
+        return bucketNum == NON_BUCKET_NUM;
+    }
+
+    public static boolean checkNonBucketTable(Table table) {
+        if (table instanceof NonBucketTable) {
+            return ((NonBucketTable) table).nonBucket();
+        }
+        return false;
+    }
+}
