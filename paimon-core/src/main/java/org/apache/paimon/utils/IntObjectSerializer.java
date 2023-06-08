2b54313e7d74db8262e5064c9918d2462c1f36c9
JingsongLi <lzljs3620320@aliyun.com>
2023-06-08 15:57:43 +0800

    [core] Introduce IntObjectSerializer

---
+++ paimon-core/src/main/java/org/apache/paimon/utils/IntObjectSerializer.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.utils;
+
+import org.apache.paimon.data.GenericRow;
+import org.apache.paimon.data.InternalRow;
+import org.apache.paimon.types.DataTypes;
+import org.apache.paimon.types.RowType;
+
+/** A {@link ObjectSerializer} for int. */
+public class IntObjectSerializer extends ObjectSerializer<Integer> {
+
+    private static final long serialVersionUID = 1L;
+
+    public IntObjectSerializer() {
+        super(RowType.of(DataTypes.INT()));
+    }
+
+    @Override
+    public InternalRow toRow(Integer record) {
+        return GenericRow.of(record);
+    }
+
+    @Override
+    public Integer fromRow(InternalRow row) {
+        return row.getInt(0);
+    }
+}
