d81d5b4950ae8270884ec7b93f59f1014f18b321
JingsongLi <lzljs3620320@aliyun.com>
2023-06-12 15:53:22 +0800

    fix

--- paimon-core/src/main/java/org/apache/paimon/index/HashIndexFile.java
+++ paimon-core/src/main/java/org/apache/paimon/index/HashIndexFile.java
@@ -23,11 +23,11 @@ import org.apache.paimon.fs.Path;
 import org.apache.paimon.utils.IntIterator;
 import org.apache.paimon.utils.PathFactory;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+
+import static org.apache.paimon.utils.IntFileUtils.readInts;
+import static org.apache.paimon.utils.IntFileUtils.writeInts;
 
 /** Hash index file contains ints. */
 public class HashIndexFile {
@@ -51,53 +51,12 @@ public class HashIndexFile {
     }
 
     public IntIterator read(String fileName) throws IOException {
-        Path path = pathFactory.toPath(fileName);
-        BufferedInputStream in = new BufferedInputStream(fileIO.newInputStream(path));
-        return new IntIterator() {
-
-            @Override
-            public int next() throws IOException {
-                return readInt(in);
-            }
-
-            @Override
-            public void close() throws IOException {
-                in.close();
-            }
-        };
+        return readInts(fileIO, pathFactory.toPath(fileName));
     }
 
     public String write(IntIterator input) throws IOException {
         Path path = pathFactory.newPath();
-        try (BufferedOutputStream out =
-                        new BufferedOutputStream(fileIO.newOutputStream(path, false));
-                IntIterator iterator = input) {
-            while (true) {
-                try {
-                    writeInt(out, iterator.next());
-                } catch (EOFException ignored) {
-                    break;
-                }
-            }
-        }
+        writeInts(fileIO, path, input);
         return path.getName();
-    }
-
-    private int readInt(BufferedInputStream in) throws IOException {
-        int ch1 = in.read();
-        int ch2 = in.read();
-        int ch3 = in.read();
-        int ch4 = in.read();
-        if ((ch1 | ch2 | ch3 | ch4) < 0) {
-            throw new EOFException();
-        }
-        return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + ch4);
-    }
-
-    private void writeInt(BufferedOutputStream out, int v) throws IOException {
-        out.write((v >>> 24) & 0xFF);
-        out.write((v >>> 16) & 0xFF);
-        out.write((v >>> 8) & 0xFF);
-        out.write(v & 0xFF);
     }
 }
