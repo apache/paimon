bb8c663c5bef4a13c6d0a2f90ec2e74688c94a05
yejunhao <yejunhao12138@gmail.com>
2023-07-31 15:48:18 +0800

    [improve] compaction async checkpoint
    [fix] fix comment
    
    [fix] fix compaction does not happens in batch mode
    
    [fix] fix comment

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/sink/AppendOnlyTableCompactionWorkerOperator.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/sink/AppendOnlyTableCompactionWorkerOperator.java
@@ -18,18 +18,31 @@
 
 package org.apache.paimon.flink.sink;
 
+import org.apache.paimon.annotation.VisibleForTesting;
 import org.apache.paimon.append.AppendOnlyCompactionTask;
 import org.apache.paimon.append.AppendOnlyTableCompactionWorker;
 import org.apache.paimon.flink.source.BucketUnawareCompactSource;
+import org.apache.paimon.fs.FileIO;
+import org.apache.paimon.io.DataFileMeta;
+import org.apache.paimon.io.DataFilePathFactory;
 import org.apache.paimon.options.Options;
 import org.apache.paimon.table.AppendOnlyFileStoreTable;
 import org.apache.paimon.table.sink.CommitMessage;
+import org.apache.paimon.table.sink.CommitMessageImpl;
+import org.apache.paimon.utils.ExecutorThreadFactory;
 
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 /**
@@ -38,10 +51,15 @@ import java.util.stream.Collectors;
  */
 public class AppendOnlyTableCompactionWorkerOperator
         extends PrepareCommitOperator<AppendOnlyCompactionTask, Committable> {
+
+    private static final Logger LOG =
+            LoggerFactory.getLogger(AppendOnlyTableCompactionWorkerOperator.class);
+
     private final AppendOnlyFileStoreTable table;
-    private transient AppendOnlyTableCompactionWorker worker;
     private final String commitUser;
-    private transient List<CommitMessage> result;
+    private transient AppendOnlyTableCompactionWorker worker;
+    private transient ExecutorService lazyCompactExecutor;
+    transient Queue<Future<List<CommitMessage>>> result;
 
     public AppendOnlyTableCompactionWorkerOperator(
             AppendOnlyFileStoreTable table, String commitUser) {
@@ -52,25 +70,105 @@ public class AppendOnlyTableCompactionWorkerOperator
 
     @Override
     public void open() throws Exception {
+        LOG.debug("Opened a append-only table compaction worker.");
         this.worker = new AppendOnlyTableCompactionWorker(table, commitUser);
-        this.result = new ArrayList<>();
+        this.result = new LinkedList<>();
     }
 
     @Override
     protected List<Committable> prepareCommit(boolean waitCompaction, long checkpointId)
             throws IOException {
-        // ignore waitCompaction tag
-        ArrayList<CommitMessage> tempList = new ArrayList<>(result);
-        result.clear();
-        return tempList.stream()
-                .map(s -> new Committable(checkpointId, Committable.Kind.FILE, s))
-                .collect(Collectors.toList());
+        List<CommitMessage> tempList = new ArrayList<>();
+        try {
+            while (!result.isEmpty()) {
+                Future<List<CommitMessage>> future = result.peek();
+                if (!future.isDone()) {
+                    if (waitCompaction) {
+                        while (!future.isDone()) {
+                            Thread.sleep(1000L);
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                result.poll();
+                tempList.addAll(future.get());
+            }
+            return tempList.stream()
+                    .map(s -> new Committable(checkpointId, Committable.Kind.FILE, s))
+                    .collect(Collectors.toList());
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while waiting tasks done.", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Encountered an error while do compaction", e);
+        }
     }
 
     @Override
     public void processElement(StreamRecord<AppendOnlyCompactionTask> element) throws Exception {
         AppendOnlyCompactionTask task = element.getValue();
-        worker.accept(task);
-        result.addAll(worker.doCompact());
+        result.add(
+                workerExecutor()
+                        .submit(
+                                () -> {
+                                    worker.accept(task);
+                                    return worker.doCompact();
+                                }));
+    }
+
+    private ExecutorService workerExecutor() {
+        if (lazyCompactExecutor == null) {
+            lazyCompactExecutor =
+                    Executors.newSingleThreadScheduledExecutor(
+                            new ExecutorThreadFactory(
+                                    Thread.currentThread().getName()
+                                            + "-append-only-compact-worker"));
+        }
+        return lazyCompactExecutor;
+    }
+
+    @Override
+    public void close() throws Exception {
+        shutdown();
+    }
+
+    @VisibleForTesting
+    void shutdown() {
+        if (lazyCompactExecutor != null) {
+            // ignore runnable tasks in queue
+            lazyCompactExecutor.shutdownNow();
+
+            // remove the file already written
+            FileIO fileIO = table.fileIO();
+
+            for (Future<List<CommitMessage>> resultFuture : result) {
+                if (!resultFuture.isDone()) {
+                    // the later tasks should be stopped running
+                    break;
+                }
+                List<CommitMessage> messages;
+                try {
+                    messages = resultFuture.get();
+                } catch (Exception exception) {
+                    // exception should already be handled
+                    continue;
+                }
+                for (CommitMessage uncommitMessage : messages) {
+                    CommitMessageImpl uncommitMessageImpl = (CommitMessageImpl) uncommitMessage;
+                    List<DataFileMeta> fileMetas =
+                            uncommitMessageImpl.compactIncrement().compactAfter();
+                    DataFilePathFactory dataFilePathFactory =
+                            table.store()
+                                    .pathFactory()
+                                    .createDataFilePathFactory(
+                                            uncommitMessageImpl.partition(),
+                                            uncommitMessageImpl.bucket());
+                    for (DataFileMeta dataFileMeta : fileMetas) {
+                        fileIO.deleteQuietly(dataFilePathFactory.toPath(dataFileMeta.fileName()));
+                    }
+                }
+            }
+        }
     }
 }
