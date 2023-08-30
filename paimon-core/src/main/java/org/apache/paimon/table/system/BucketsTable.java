b82070571072e6b49a8fa469605bc6be52e6ca1e
LsomeYeah <lilei.200@qq.com>
2023-08-30 15:27:40 +0800

    [core] add database name and table name in BucketsTable

--- paimon-core/src/main/java/org/apache/paimon/table/system/BucketsTable.java
+++ paimon-core/src/main/java/org/apache/paimon/table/system/BucketsTable.java
@@ -19,6 +19,7 @@
 package org.apache.paimon.table.system;
 
 import org.apache.paimon.CoreOptions;
+import org.apache.paimon.data.BinaryString;
 import org.apache.paimon.data.GenericRow;
 import org.apache.paimon.data.InternalRow;
 import org.apache.paimon.disk.IOManager;
@@ -42,9 +43,12 @@ import org.apache.paimon.types.BigIntType;
 import org.apache.paimon.types.DataField;
 import org.apache.paimon.types.IntType;
 import org.apache.paimon.types.RowType;
+import org.apache.paimon.types.VarCharType;
 import org.apache.paimon.utils.IteratorRecordReader;
 import org.apache.paimon.utils.SnapshotManager;
 import org.apache.paimon.utils.TagManager;
+
+import javax.annotation.Nullable;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -67,9 +71,23 @@ public class BucketsTable implements DataTable, ReadonlyTable {
     private final FileStoreTable wrapped;
     private final boolean isContinuous;
 
+    @Nullable private final String databaseName;
+    private final String tableName;
+
     public BucketsTable(FileStoreTable wrapped, boolean isContinuous) {
         this.wrapped = wrapped;
         this.isContinuous = isContinuous;
+        this.databaseName = null;
+        this.tableName = wrapped.name();
+    }
+
+    // if need to specify the database of a table, use this method
+    public BucketsTable(
+            FileStoreTable wrapped, boolean isContinuous, String databaseName, String tableName) {
+        this.wrapped = wrapped;
+        this.isContinuous = isContinuous;
+        this.databaseName = databaseName;
+        this.tableName = tableName;
     }
 
     @Override
@@ -99,6 +117,9 @@ public class BucketsTable implements DataTable, ReadonlyTable {
         fields.add(new DataField(1, "_PARTITION", newBytesType(false)));
         fields.add(new DataField(2, "_BUCKET", new IntType(false)));
         fields.add(new DataField(3, "_FILES", newBytesType(false)));
+        fields.add(
+                new DataField(4, "_DATABASE_NAME", new VarCharType(Integer.MAX_VALUE).nullable()));
+        fields.add(new DataField(5, "_TABLE_NAME", new VarCharType(Integer.MAX_VALUE)));
         return new RowType(fields);
     }
 
@@ -140,6 +161,11 @@ public class BucketsTable implements DataTable, ReadonlyTable {
     @Override
     public BucketsTable copy(Map<String, String> dynamicOptions) {
         return new BucketsTable(wrapped.copy(dynamicOptions), isContinuous);
+    }
+
+    public BucketsTable copyWithName(Map<String, String> dynamicOptions) {
+        return new BucketsTable(
+                wrapped.copy(dynamicOptions), isContinuous, databaseName, tableName);
     }
 
     @Override
@@ -187,7 +213,9 @@ public class BucketsTable implements DataTable, ReadonlyTable {
                             dataSplit.snapshotId(),
                             serializeBinaryRow(dataSplit.partition()),
                             dataSplit.bucket(),
-                            dataFileMetaSerializer.serializeList(files));
+                            dataFileMetaSerializer.serializeList(files),
+                            BinaryString.fromString(databaseName),
+                            BinaryString.fromString(tableName));
 
             return new IteratorRecordReader<>(Collections.singletonList(row).iterator());
         }
