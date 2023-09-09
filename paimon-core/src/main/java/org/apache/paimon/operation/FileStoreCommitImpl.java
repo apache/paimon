2c79d556473fc43e8be6274dc88158895fe66a90
liming.1018 <69444450+liming30@users.noreply.github.com>
2023-09-09 16:10:40 +0800

    [core] fix the issue that partitionExpire will fail when logOffsets is not empty. (#1977)

--- paimon-core/src/main/java/org/apache/paimon/operation/FileStoreCommitImpl.java
+++ paimon-core/src/main/java/org/apache/paimon/operation/FileStoreCommitImpl.java
@@ -393,7 +393,7 @@ public class FileStoreCommitImpl implements FileStoreCommit {
                 Collections.emptyList(),
                 commitIdentifier,
                 null,
-                Collections.emptyMap());
+                new HashMap<>());
     }
 
     @Override
