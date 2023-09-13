96c4229b24508c224b4d29f8bcd5ab3b4367f8f0
zhuangchong <zhuang.kerwin@gmail.com>
2023-09-13 19:52:24 +0800

    fix code style

--- paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/TypeMapping.java
+++ paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/TypeMapping.java
@@ -19,7 +19,10 @@
 package org.apache.paimon.flink.action.cdc;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
