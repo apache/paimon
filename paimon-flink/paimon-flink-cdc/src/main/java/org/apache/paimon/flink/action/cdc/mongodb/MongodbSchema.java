76adcb11de4170124b2f36afe43c7614ff50083b
chenzy15 <chenzy15@ziroom.com>
2023-09-19 00:07:48 +0800

    Fix the review content

--- paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/mongodb/MongodbSchema.java
+++ paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/mongodb/MongodbSchema.java
@@ -21,16 +21,18 @@ package org.apache.paimon.flink.action.cdc.mongodb;
 import org.apache.paimon.types.DataType;
 import org.apache.paimon.types.DataTypes;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.ververica.cdc.connectors.mongodb.source.config.MongoDBSourceOptions;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.configuration.Configuration;
 import org.bson.Document;
+
+import javax.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,8 +40,9 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
+import static com.ververica.cdc.connectors.mongodb.internal.MongoDBEnvelope.encodeValue;
+import static org.apache.flink.util.Preconditions.checkNotNull;
 import static org.apache.paimon.flink.action.cdc.mongodb.MongoDBActionUtils.FIELD_NAME;
 import static org.apache.paimon.flink.action.cdc.mongodb.MongoDBActionUtils.START_MODE;
 
@@ -90,74 +93,111 @@ public class MongodbSchema {
         return primaryKeys;
     }
 
+    /**
+     * Utility class for creating a MongoDB schema based on the provided configuration. The schema
+     * can be created in one of the two modes:
+     *
+     * <ul>
+     *   <li><b>SPECIFIED</b>: In this mode, the schema is created based on the explicit column
+     *       names provided in the configuration. The data types for all columns are assumed to be
+     *       STRING.
+     *   <li><b>DYNAMIC</b>: In this mode, the schema is inferred dynamically from the first
+     *       document in the specified MongoDB collection.
+     * </ul>
+     *
+     * <p>The Configuration object passed to the createSchema method should have the necessary
+     * MongoDB configuration properties set, including the host address, database name, collection
+     * name, and optionally, the username and password for authentication. For the SPECIFIED mode,
+     * the field names should also be specified in the configuration.
+     */
     public static MongodbSchema getMongodbSchema(Configuration mongodbConfig) {
         SchemaAcquisitionMode mode = getModeFromConfig(mongodbConfig);
+        String databaseName =
+                Objects.requireNonNull(
+                        mongodbConfig.get(MongoDBSourceOptions.DATABASE),
+                        "Database name cannot be null");
+        String collectionName =
+                Objects.requireNonNull(
+                        mongodbConfig.get(MongoDBSourceOptions.COLLECTION),
+                        "Collection name cannot be null");
+
         switch (mode) {
             case SPECIFIED:
-                return createSchemaFromSpecifiedConfig(mongodbConfig);
+                String[] columnNames =
+                        Objects.requireNonNull(
+                                        mongodbConfig.get(FIELD_NAME), "Field names cannot be null")
+                                .split(",");
+                LinkedHashMap<String, DataType> schemaFields =
+                        generateSchemaFields(Arrays.asList(columnNames));
+                return new MongodbSchema(
+                        databaseName,
+                        collectionName,
+                        schemaFields,
+                        Collections.singletonList(ID_FIELD));
             case DYNAMIC:
-                return createSchemaFromDynamicConfig(mongodbConfig);
+                String hosts =
+                        Objects.requireNonNull(
+                                mongodbConfig.get(MongoDBSourceOptions.HOSTS),
+                                "Hosts cannot be null");
+
+                MongoClientSettings.Builder settingsBuilder = MongoClientSettings.builder();
+
+                settingsBuilder.applyConnectionString(
+                        new ConnectionString(
+                                buildConnectionString(
+                                        mongodbConfig.get(MongoDBSourceOptions.USERNAME),
+                                        mongodbConfig.get(MongoDBSourceOptions.PASSWORD),
+                                        mongodbConfig.get(MongoDBSourceOptions.SCHEME),
+                                        hosts,
+                                        mongodbConfig.get(
+                                                MongoDBSourceOptions.CONNECTION_OPTIONS))));
+
+                MongoClientSettings settings = settingsBuilder.build();
+
+                try (MongoClient mongoClient = MongoClients.create(settings)) {
+                    MongoDatabase database = mongoClient.getDatabase(databaseName);
+                    MongoCollection<Document> collection = database.getCollection(collectionName);
+                    Document firstDocument = collection.find().first();
+
+                    if (firstDocument == null) {
+                        throw new IllegalStateException(
+                                "No documents in collection to infer schema");
+                    }
+
+                    return createMongodbSchema(
+                            databaseName, collectionName, getColumnNames(firstDocument));
+                } catch (Exception e) {
+                    throw new RuntimeException(
+                            "Failed to create schema from MongoDB collection", e);
+                }
             default:
                 throw new IllegalArgumentException("Unsupported schema acquisition mode: " + mode);
         }
     }
 
+    public static String buildConnectionString(
+            @Nullable String username,
+            @Nullable String password,
+            String scheme,
+            String hosts,
+            @Nullable String connectionOptions) {
+        StringBuilder sb = new StringBuilder(scheme).append("://");
+
+        if (StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password)) {
+            sb.append(encodeValue(username)).append(":").append(encodeValue(password)).append("@");
+        }
+
+        sb.append(checkNotNull(hosts));
+
+        if (StringUtils.isNotEmpty(connectionOptions)) {
+            sb.append("/?").append(connectionOptions);
+        }
+
+        return sb.toString();
+    }
+
     private static SchemaAcquisitionMode getModeFromConfig(Configuration mongodbConfig) {
         return SchemaAcquisitionMode.valueOf(mongodbConfig.get(START_MODE).toUpperCase());
-    }
-
-    private static MongodbSchema createSchemaFromSpecifiedConfig(Configuration mongodbConfig) {
-        String[] columnNames = mongodbConfig.get(FIELD_NAME).split(",");
-        LinkedHashMap<String, DataType> schemaFields =
-                generateSchemaFields(Arrays.asList(columnNames));
-        String databaseName = mongodbConfig.get(MongoDBSourceOptions.DATABASE);
-        String collectionName = mongodbConfig.get(MongoDBSourceOptions.COLLECTION);
-        return new MongodbSchema(
-                databaseName, collectionName, schemaFields, Collections.singletonList(ID_FIELD));
-    }
-
-    /**
-     * Utility class for creating a MongoDB schema based on a MongoDB collection's first document.
-     * It uses the MongoDB Java driver to connect to the database and retrieve the schema.
-     */
-    private static MongodbSchema createSchemaFromDynamicConfig(Configuration mongodbConfig) {
-        String hosts = mongodbConfig.get(MongoDBSourceOptions.HOSTS);
-        String databaseName = mongodbConfig.get(MongoDBSourceOptions.DATABASE);
-        String collectionName = mongodbConfig.get(MongoDBSourceOptions.COLLECTION);
-
-        List<ServerAddress> serverAddresses =
-                Arrays.stream(hosts.split(","))
-                        .map(ServerAddress::new)
-                        .collect(Collectors.toList());
-
-        MongoClientSettings.Builder settingsBuilder =
-                MongoClientSettings.builder()
-                        .applyToClusterSettings(builder -> builder.hosts(serverAddresses));
-
-        if (mongodbConfig.contains(MongoDBSourceOptions.USERNAME)
-                && mongodbConfig.contains(MongoDBSourceOptions.PASSWORD)) {
-            settingsBuilder.credential(
-                    MongoCredential.createCredential(
-                            mongodbConfig.get(MongoDBSourceOptions.USERNAME),
-                            databaseName,
-                            mongodbConfig.get(MongoDBSourceOptions.PASSWORD).toCharArray()));
-        }
-
-        MongoClientSettings settings = settingsBuilder.build();
-
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            MongoDatabase database = mongoClient.getDatabase(databaseName);
-            MongoCollection<Document> collection = database.getCollection(collectionName);
-            Document firstDocument = collection.find().first();
-
-            if (firstDocument == null) {
-                throw new IllegalStateException("No documents in collection to infer schema");
-            }
-
-            return createMongodbSchema(databaseName, collectionName, getColumnNames(firstDocument));
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create schema from MongoDB collection", e);
-        }
     }
 
     private static List<String> getColumnNames(Document document) {
