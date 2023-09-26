055b12fa5c52eda4d38b90c8e20da399588007d6
zhuangchong <zhuang.kerwin@gmail.com>
2023-09-26 12:14:05 +0800

    Test class modification read file.

--- paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/mysql/DebeziumEventTest.java
+++ paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/mysql/DebeziumEventTest.java
@@ -21,6 +21,7 @@ package org.apache.paimon.flink.action.cdc.mysql;
 import org.apache.paimon.flink.action.cdc.mysql.format.DebeziumEvent;
 
 import org.apache.paimon.shade.guava30.com.google.common.collect.Iterators;
+import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.core.JsonParser;
 import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.databind.DeserializationFeature;
 import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -29,6 +30,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Iterator;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,137 +44,19 @@ public class DebeziumEventTest {
     public void before() {
         objectMapper = new ObjectMapper();
         objectMapper
+                .configure(JsonParser.Feature.ALLOW_COMMENTS, true)
                 .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     @Test
     public void testDeserialize() throws IOException {
-        String json =
-                "{\n"
-                        + "    \"schema\":{\n"
-                        + "        \"type\":\"struct\",\n"
-                        + "        \"fields\":[\n"
-                        + "            {\n"
-                        + "                \"type\":\"struct\",\n"
-                        + "                \"fields\":[\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"version\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"connector\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"name\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"int64\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"ts_ms\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":true,\n"
-                        + "                        \"name\":\"io.debezium.data.Enum\",\n"
-                        + "                        \"version\":1,\n"
-                        + "                        \"parameters\":{\n"
-                        + "                            \"allowed\":\"true,last,false\"\n"
-                        + "                        },\n"
-                        + "                        \"default\":\"false\",\n"
-                        + "                        \"field\":\"snapshot\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"db\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":true,\n"
-                        + "                        \"field\":\"sequence\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":true,\n"
-                        + "                        \"field\":\"table\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"int64\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"server_id\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":true,\n"
-                        + "                        \"field\":\"gtid\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"file\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"int64\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"pos\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"int32\",\n"
-                        + "                        \"optional\":false,\n"
-                        + "                        \"field\":\"row\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"int64\",\n"
-                        + "                        \"optional\":true,\n"
-                        + "                        \"field\":\"thread\"\n"
-                        + "                    },\n"
-                        + "                    {\n"
-                        + "                        \"type\":\"string\",\n"
-                        + "                        \"optional\":true,\n"
-                        + "                        \"field\":\"query\"\n"
-                        + "                    }\n"
-                        + "                ],\n"
-                        + "                \"optional\":false,\n"
-                        + "                \"name\":\"io.debezium.connector.mysql.Source\",\n"
-                        + "                \"field\":\"source\"\n"
-                        + "            },\n"
-                        + "            {\n"
-                        + "                \"type\":\"string\",\n"
-                        + "                \"optional\":true,\n"
-                        + "                \"field\":\"historyRecord\"\n"
-                        + "            }\n"
-                        + "        ],\n"
-                        + "        \"optional\":false,\n"
-                        + "        \"name\":\"io.debezium.connector.mysql.SchemaChangeValue\"\n"
-                        + "    },\n"
-                        + "    \"payload\":{\n"
-                        + "        \"source\":{\n"
-                        + "            \"version\":\"1.6.4.Final\",\n"
-                        + "            \"connector\":\"mysql\",\n"
-                        + "            \"name\":\"mysql_binlog_source\",\n"
-                        + "            \"ts_ms\":1695203563233,\n"
-                        + "            \"snapshot\":\"false\",\n"
-                        + "            \"db\":\"tinyint1_not_bool_test\",\n"
-                        + "            \"sequence\":null,\n"
-                        + "            \"table\":\"t1\",\n"
-                        + "            \"server_id\":223344,\n"
-                        + "            \"gtid\":null,\n"
-                        + "            \"file\":\"mysql-bin.000003\",\n"
-                        + "            \"pos\":219,\n"
-                        + "            \"row\":0,\n"
-                        + "            \"thread\":null,\n"
-                        + "            \"query\":null\n"
-                        + "        },\n"
-                        + "        \"historyRecord\":\"{\\\"source\\\":{\\\"file\\\":\\\"mysql-bin.000003\\\",\\\"pos\\\":219,\\\"server_id\\\":223344},\\\"position\\\":{\\\"transaction_id\\\":null,\\\"ts_sec\\\":1695203563,\\\"file\\\":\\\"mysql-bin.000003\\\",\\\"pos\\\":379,\\\"server_id\\\":223344},\\\"databaseName\\\":\\\"tinyint1_not_bool_test\\\",\\\"ddl\\\":\\\"ALTER TABLE t1 ADD COLUMN _new_tinyint1 TINYINT(1)\\\",\\\"tableChanges\\\":[{\\\"type\\\":\\\"ALTER\\\",\\\"id\\\":\\\"\\\\\\\"tinyint1_not_bool_test\\\\\\\".\\\\\\\"t1\\\\\\\"\\\",\\\"table\\\":{\\\"defaultCharsetName\\\":\\\"latin1\\\",\\\"primaryKeyColumnNames\\\":[\\\"pk\\\"],\\\"columns\\\":[{\\\"name\\\":\\\"pk\\\",\\\"jdbcType\\\":4,\\\"typeName\\\":\\\"INT\\\",\\\"typeExpression\\\":\\\"INT\\\",\\\"charsetName\\\":null,\\\"length\\\":11,\\\"position\\\":1,\\\"optional\\\":false,\\\"autoIncremented\\\":false,\\\"generated\\\":false},{\\\"name\\\":\\\"_tinyint1\\\",\\\"jdbcType\\\":5,\\\"typeName\\\":\\\"TINYINT\\\",\\\"typeExpression\\\":\\\"TINYINT\\\",\\\"charsetName\\\":null,\\\"length\\\":1,\\\"position\\\":2,\\\"optional\\\":true,\\\"autoIncremented\\\":false,\\\"generated\\\":false},{\\\"name\\\":\\\"_new_tinyint1\\\",\\\"jdbcType\\\":5,\\\"typeName\\\":\\\"TINYINT\\\",\\\"typeExpression\\\":\\\"TINYINT\\\",\\\"charsetName\\\":null,\\\"length\\\":1,\\\"position\\\":3,\\\"optional\\\":true,\\\"autoIncremented\\\":false,\\\"generated\\\":false}]}}]}\"\n"
-                        + "    }\n"
-                        + "}";
-        DebeziumEvent debeziumEvent = objectMapper.readValue(json, DebeziumEvent.class);
+        final URL url =
+                DebeziumEventTest.class
+                        .getClassLoader()
+                        .getResource("mysql/debezium-event-change.json");
+        assertThat(url).isNotNull();
+        DebeziumEvent debeziumEvent = objectMapper.readValue(url, DebeziumEvent.class);
         assertThat(debeziumEvent).isNotNull();
         assertThat(debeziumEvent.payload().hasHistoryRecord()).isTrue();
         Iterator<TableChanges.TableChange> tableChanges = debeziumEvent.payload().getTableChanges();
