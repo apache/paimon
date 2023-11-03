0ef9d5dd876a51ea251f750791ef2e9639090c5b
zhangjun <zhangjunemail@126.com>
2023-11-03 10:23:49 +0800

    list tables sorted

--- paimon-core/src/main/java/org/apache/paimon/catalog/AbstractCatalog.java
+++ paimon-core/src/main/java/org/apache/paimon/catalog/AbstractCatalog.java
@@ -43,6 +43,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.apache.paimon.options.CatalogOptions.LINEAGE_META;
 
@@ -145,7 +146,7 @@ public abstract class AbstractCatalog implements Catalog {
             throw new DatabaseNotExistException(databaseName);
         }
 
-        return listTablesImpl(databaseName);
+        return listTablesImpl(databaseName).stream().sorted().collect(Collectors.toList());
     }
 
     protected abstract List<String> listTablesImpl(String databaseName);
