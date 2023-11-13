7179e4a0111ebd7b87f43c5d2d10cbfed207b986
liming.1018 <liming.1018@bytedance.com>
2023-11-13 16:37:46 +0800

    [flink] fix the problem that DynamicFilteringEvent does not exist in version 1.16- of flink.

--- paimon-flink/paimon-flink-1.15/src/main/java/org/apache/paimon/flink/source/StaticFileStoreSplitEnumerator.java
+++ paimon-flink/paimon-flink-1.15/src/main/java/org/apache/paimon/flink/source/StaticFileStoreSplitEnumerator.java
@@ -19,23 +19,15 @@
 package org.apache.paimon.flink.source;
 
 import org.apache.paimon.Snapshot;
-import org.apache.paimon.flink.source.assigners.DynamicPartitionPruningAssigner;
 import org.apache.paimon.flink.source.assigners.SplitAssigner;
 
-import org.apache.flink.api.connector.source.SourceEvent;
 import org.apache.flink.api.connector.source.SplitEnumerator;
 import org.apache.flink.api.connector.source.SplitEnumeratorContext;
-import org.apache.flink.table.connector.source.DynamicFilteringData;
-import org.apache.flink.table.connector.source.DynamicFilteringEvent;
 
 import javax.annotation.Nullable;
 
-import static org.apache.paimon.utils.Preconditions.checkNotNull;
-
 /** A {@link SplitEnumerator} implementation for {@link StaticFileStoreSource} input. */
 public class StaticFileStoreSplitEnumerator extends StaticFileStoreSplitEnumeratorBase {
-
-    @Nullable private final DynamicPartitionFilteringInfo dynamicPartitionFilteringInfo;
 
     public StaticFileStoreSplitEnumerator(
             SplitEnumeratorContext<FileStoreSourceSplit> context,
@@ -50,32 +42,5 @@ public class StaticFileStoreSplitEnumerator extends StaticFileStoreSplitEnumerat
             SplitAssigner splitAssigner,
             @Nullable DynamicPartitionFilteringInfo dynamicPartitionFilteringInfo) {
         super(context, snapshot, splitAssigner);
-        this.dynamicPartitionFilteringInfo = dynamicPartitionFilteringInfo;
-    }
-
-    @Override
-    public void handleSourceEvent(int subtaskId, SourceEvent sourceEvent) {
-        if (sourceEvent instanceof DynamicFilteringEvent) {
-            DynamicFilteringData dynamicFilteringData =
-                    ((DynamicFilteringEvent) sourceEvent).getData();
-            LOG.info(
-                    "Received DynamicFilteringEvent: {}, is filtering: {}.",
-                    subtaskId,
-                    dynamicFilteringData.isFiltering());
-
-            checkNotNull(
-                    dynamicPartitionFilteringInfo,
-                    "Cannot apply dynamic filtering because dynamicPartitionFilteringInfo hasn't been set.");
-
-            if (dynamicFilteringData.isFiltering()) {
-                this.splitAssigner =
-                        new DynamicPartitionPruningAssigner(
-                                splitAssigner,
-                                dynamicPartitionFilteringInfo.getPartitionRowProjection(),
-                                dynamicFilteringData);
-            }
-        } else {
-            super.handleSourceEvent(subtaskId, sourceEvent);
-        }
     }
 }
