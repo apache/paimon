df962f36349a0ac576c4bd461f07c87d3e073b1a
1161623489@qq.com <1161623489@qq.com>
2023-11-15 23:10:22 +0800

    integration test

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/FlinkExternalCatalogITCase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/FlinkExternalCatalogITCase.java
@@ -22,6 +22,7 @@ import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.streaming.api.environment.ExecutionCheckpointingOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,10 +33,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /** Integration test for {@link FlinkExternalCatalog}. */
 public class FlinkExternalCatalogITCase {
 
-    protected TableEnvironment tEnv;
+    private TableEnvironment tEnv;
+    private String catalog = "PAIMON_EXTERNAL";
+    private String catalog2 = "PAIMON_EXTERNAL2";
 
     @TempDir public static java.nio.file.Path path;
 
@@ -47,7 +52,7 @@ public class FlinkExternalCatalogITCase {
         env.getConfig().setRestartStrategy(RestartStrategies.noRestart());
         env.setParallelism(1);
         env.setRuntimeMode(RuntimeExecutionMode.BATCH);
-        String catalog = "PAIMON_EXTERNAL";
+
         Map<String, String> options = new HashMap<>();
         options.put("type", "paimon-external");
         options.put("warehouse", path.toUri().getPath());
@@ -62,13 +67,20 @@ public class FlinkExternalCatalogITCase {
                         options.entrySet().stream()
                                 .map(e -> String.format("'%s'='%s'", e.getKey(), e.getValue()))
                                 .collect(Collectors.joining(","))));
-        tEnv.useCatalog(catalog);
+        tEnv.executeSql(
+                String.format(
+                        "CREATE CATALOG %s WITH (" + "%s" + ")",
+                        catalog2,
+                        options.entrySet().stream()
+                                .map(e -> String.format("'%s'='%s'", e.getKey(), e.getValue()))
+                                .collect(Collectors.joining(","))));
     }
 
     @Test
-    public void testUseExternalTable() {
+    public void testMix() {
+        tEnv.useCatalog(catalog);
         tEnv.executeSql(
-                "CREATE TABLE word_count (\n"
+                "CREATE TABLE word_count1 (\n"
                         + "    word STRING PRIMARY KEY NOT ENFORCED,\n"
                         + "    cnt BIGINT\n"
                         + ") with (\n"
@@ -76,7 +88,7 @@ public class FlinkExternalCatalogITCase {
                         + ")");
         tEnv.executeSql(
                 String.format(
-                        "CREATE TABLE file (\n"
+                        "CREATE TABLE file1 (\n"
                                 + "    word STRING PRIMARY KEY NOT ENFORCED,\n"
                                 + "    cnt BIGINT\n"
                                 + ") with (\n"
@@ -85,13 +97,14 @@ public class FlinkExternalCatalogITCase {
                                 + "    'format'='json' \n"
                                 + ")",
                         path2.toUri().getPath()));
-        tEnv.executeSql("insert into file select * from `word_count`");
+        tEnv.executeSql("insert into file1 select * from `word_count1`");
     }
 
     @Test
     public void testUseSystemTable() {
+        tEnv.useCatalog(catalog);
         tEnv.executeSql(
-                "CREATE TABLE word_count (\n"
+                "CREATE TABLE word_count2 (\n"
                         + "    word STRING PRIMARY KEY NOT ENFORCED,\n"
                         + "    cnt BIGINT\n"
                         + ") with (\n"
@@ -99,7 +112,7 @@ public class FlinkExternalCatalogITCase {
                         + ")");
         tEnv.executeSql(
                 String.format(
-                        "CREATE TABLE file (\n"
+                        "CREATE TABLE file2 (\n"
                                 + "    word STRING PRIMARY KEY NOT ENFORCED,\n"
                                 + "    rowkind String,\n"
                                 + "    cnt BIGINT\n"
@@ -109,6 +122,27 @@ public class FlinkExternalCatalogITCase {
                                 + "    'format'='json' \n"
                                 + ")",
                         path2.toUri().getPath()));
-        tEnv.executeSql("insert into file select * from word_count$audit_log");
+        tEnv.executeSql("insert into file2 select * from word_count2$audit_log");
+    }
+
+    @Test
+    public void testUseExternalTable() {
+        tEnv.useCatalog(catalog);
+        tEnv.executeSql(
+                String.format(
+                        "CREATE TABLE file3 (\n"
+                                + "    word STRING PRIMARY KEY NOT ENFORCED,\n"
+                                + "    rowkind String,\n"
+                                + "    cnt BIGINT\n"
+                                + ") with (\n"
+                                + "    'connector'='filesystem',\n"
+                                + "    'path'='file://%s', \n"
+                                + "    'format'='json' \n"
+                                + ")",
+                        path2.toUri().getPath()));
+        Table fileTable = tEnv.from("file3");
+        tEnv.useCatalog(catalog2);
+        Table fileTable2 = tEnv.from("file3");
+        assertThat(fileTable.getResolvedSchema()).isEqualTo(fileTable2.getResolvedSchema());
     }
 }
