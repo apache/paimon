b68304e7bf8cf5dfe97fd8ec59a70243e5f25736
李国君 <liguojun@bytedance.com>
2023-11-19 00:22:40 +0800

    [Flink] Fix license

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/source/FileStoreSourceMetricsTest.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/source/FileStoreSourceMetricsTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.paimon.flink.source;
 
 import org.apache.paimon.data.GenericRow;
@@ -36,7 +54,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /** Tests for file store sources with metrics. */
 public class FileStoreSourceMetricsTest {
     private FileStoreTable table;
-    private TestingSplitEnumeratorContextWithRegisteringGroup<FileStoreSourceSplit> context;
+    private TestingSplitEnumeratorContextWithRegisteringGroup context;
     private MetricGroup scanMetricGroup;
 
     @BeforeEach
@@ -51,7 +69,7 @@ public class FileStoreSourceMetricsTest {
                                 .column("b", DataTypes.BIGINT())
                                 .build());
         table = FileStoreTableFactory.create(fileIO, tablePath, tableSchema);
-        context = new TestingSplitEnumeratorContextWithRegisteringGroup<>(1);
+        context = new TestingSplitEnumeratorContextWithRegisteringGroup(1);
         scanMetricGroup =
                 context.metricGroup()
                         .addGroup("paimon")
@@ -141,8 +159,8 @@ public class FileStoreSourceMetricsTest {
         writer.close();
     }
 
-    class TestingSplitEnumeratorContextWithRegisteringGroup<FileStoreSourceSplit>
-            extends TestingSplitEnumeratorContext {
+    private class TestingSplitEnumeratorContextWithRegisteringGroup
+            extends TestingSplitEnumeratorContext<FileStoreSourceSplit> {
         private final SplitEnumeratorMetricGroup metricGroup;
 
         public TestingSplitEnumeratorContextWithRegisteringGroup(int parallelism) {
