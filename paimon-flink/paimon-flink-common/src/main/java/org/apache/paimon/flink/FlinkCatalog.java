38aa07b3096c8886d9ef8b7eda1e76bddae2f851
Jingsong <jingsonglee0@gmail.com>
2023-12-11 14:24:35 +0800

    [flink][spark] Check Exists before create database to avoid Permission issue

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/FlinkCatalog.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/FlinkCatalog.java
@@ -148,9 +148,11 @@ public class FlinkCatalog extends AbstractCatalog {
         this.logStoreAutoRegister = options.get(LOG_SYSTEM_AUTO_REGISTER);
         this.logStoreAutoRegisterTimeout = options.get(REGISTER_TIMEOUT);
         this.disableCreateTableInDefaultDatabase = options.get(DISABLE_CREATE_TABLE_IN_DEFAULT_DB);
-        try {
-            this.catalog.createDatabase(defaultDatabase, true);
-        } catch (Catalog.DatabaseAlreadyExistException ignore) {
+        if (!catalog.databaseExists(defaultDatabase)) {
+            try {
+                catalog.createDatabase(defaultDatabase, true);
+            } catch (Catalog.DatabaseAlreadyExistException ignore) {
+            }
         }
     }
 
