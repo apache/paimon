6a43ed516098d7d594b9d5f53aedabc3c934a185
wgcn <1026688210@qq.com>
2023-12-20 17:10:35 +0800

    drop ut

--- paimon-hive/paimon-hive-catalog/src/main/java/org/apache/paimon/hive/HiveCatalog.java
+++ paimon-hive/paimon-hive-catalog/src/main/java/org/apache/paimon/hive/HiveCatalog.java
@@ -252,8 +252,6 @@ public class HiveCatalog extends AbstractCatalog {
                     .filter(
                             tableName -> {
                                 Identifier identifier = new Identifier(databaseName, tableName);
-                                // the environment here may not be able to access non-paimon
-                                // tables, so we just check the schema file first
                                 return tableExists(identifier);
                             })
                     .collect(Collectors.toList());
