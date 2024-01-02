4da02b8ae20e70966b7beb6348afbc681f701be0
monster <60029759+MonsterChenzhuo@users.noreply.github.com>
2024-01-02 10:21:58 +0800

    [flink] Fix mongo primary key field not objectId type (#2606)

--- paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/mongodb/strategy/MongoVersionStrategy.java
+++ paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/mongodb/strategy/MongoVersionStrategy.java
@@ -98,7 +98,9 @@ public interface MongoVersionStrategy {
         }
         JsonNode document =
                 mongodbConfig.getBoolean(DEFAULT_ID_GENERATION)
-                        ? objectNode.set(ID_FIELD, idNode.get(OID_FIELD))
+                        ? objectNode.set(
+                                ID_FIELD,
+                                idNode.get(OID_FIELD) == null ? idNode : idNode.get(OID_FIELD))
                         : objectNode;
         Map<String, String> row;
         switch (mode) {
