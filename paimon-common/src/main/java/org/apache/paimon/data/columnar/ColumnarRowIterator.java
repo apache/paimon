508411d94fd68c90ee20eaaa0eecf83efc2cc15f
Zouxxyy <zouxinyu.zxy@alibaba-inc.com>
2024-02-28 19:52:01 +0800

    [core] Introduce RecordWithPositionIterator interface (#2916)

--- paimon-common/src/main/java/org/apache/paimon/data/columnar/ColumnarRowIterator.java
+++ paimon-common/src/main/java/org/apache/paimon/data/columnar/ColumnarRowIterator.java
@@ -21,6 +21,7 @@ package org.apache.paimon.data.columnar;
 import org.apache.paimon.data.InternalRow;
 import org.apache.paimon.data.PartitionInfo;
 import org.apache.paimon.reader.RecordReader;
+import org.apache.paimon.reader.RecordWithPositionIterator;
 import org.apache.paimon.utils.RecyclableIterator;
 import org.apache.paimon.utils.VectorMappingUtils;
 
@@ -30,23 +31,35 @@ import javax.annotation.Nullable;
  * A {@link RecordReader.RecordIterator} that returns {@link InternalRow}s. The next row is set by
  * {@link ColumnarRow#setRowId}.
  */
-public class ColumnarRowIterator extends RecyclableIterator<InternalRow> {
+public class ColumnarRowIterator extends RecyclableIterator<InternalRow>
+        implements RecordWithPositionIterator<InternalRow> {
 
     private final ColumnarRow rowData;
     private final Runnable recycler;
 
     private int num;
     private int pos;
+    private long rowPosition;
 
     public ColumnarRowIterator(ColumnarRow rowData, @Nullable Runnable recycler) {
         super(recycler);
         this.rowData = rowData;
         this.recycler = recycler;
+        this.rowPosition = 0;
     }
 
-    public void set(int num) {
+    /** Reset the number of rows in the vectorized batch and the start position in this batch. */
+    public void reset(int num) {
         this.num = num;
         this.pos = 0;
+    }
+
+    /**
+     * Reset the current record's row position in the file, and it needs to be ensured that the row
+     * position after reset is strictly incremented by 1.
+     */
+    public void resetRowPosition(long rowPosition) {
+        this.rowPosition = rowPosition;
     }
 
     @Nullable
@@ -54,10 +67,16 @@ public class ColumnarRowIterator extends RecyclableIterator<InternalRow> {
     public InternalRow next() {
         if (pos < num) {
             rowData.setRowId(pos++);
+            rowPosition++;
             return rowData;
         } else {
             return null;
         }
+    }
+
+    @Override
+    public long rowPosition() {
+        return rowPosition;
     }
 
     public ColumnarRowIterator mapping(
@@ -72,7 +91,7 @@ public class ColumnarRowIterator extends RecyclableIterator<InternalRow> {
                 vectors = VectorMappingUtils.createIndexMappedVectors(indexMapping, vectors);
             }
             ColumnarRowIterator iterator = new ColumnarRowIterator(rowData.copy(vectors), recycler);
-            iterator.set(num);
+            iterator.reset(num);
             return iterator;
         }
         return this;
