842adc82a719ac48f51b470801c1deea231d2f0f
zouxxyy <zouxinyu.zxy@alibaba-inc.com>
2024-03-18 13:51:24 +0800

    add test case

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/DeletionVectorITCase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/DeletionVectorITCase.java
@@ -48,7 +48,7 @@ public class DeletionVectorITCase extends CatalogITCaseBase {
         // test read from APPEND snapshot
         try (BlockingIterator<Row, Row> iter =
                 streamSqlBlockIter(
-                        "SELECT * FROM T /*+ OPTIONS('scan.mode'='from-snapshot-full','scan.snapshot-id' = '3') */"); ) {
+                        "SELECT * FROM T /*+ OPTIONS('scan.mode'='from-snapshot-full','scan.snapshot-id' = '3') */")) {
             assertThat(iter.collect(12))
                     .containsExactlyInAnyOrder(
                             Row.ofKind(RowKind.INSERT, 1, "111111111"),
@@ -68,7 +68,7 @@ public class DeletionVectorITCase extends CatalogITCaseBase {
         // test read from COMPACT snapshot
         try (BlockingIterator<Row, Row> iter =
                 streamSqlBlockIter(
-                        "SELECT * FROM T /*+ OPTIONS('scan.mode'='from-snapshot-full','scan.snapshot-id' = '4') */"); ) {
+                        "SELECT * FROM T /*+ OPTIONS('scan.mode'='from-snapshot-full','scan.snapshot-id' = '4') */")) {
             assertThat(iter.collect(8))
                     .containsExactlyInAnyOrder(
                             Row.ofKind(RowKind.INSERT, 1, "111111111"),
@@ -112,5 +112,90 @@ public class DeletionVectorITCase extends CatalogITCaseBase {
         assertThat(batchSql("SELECT * FROM T /*+ OPTIONS('scan.snapshot-id'='4') */"))
                 .containsExactlyInAnyOrder(
                         Row.of(1, "111111111"), Row.of(2, "2_1"), Row.of(3, "3_1"), Row.of(4, "4"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"none", "lookup"})
+    public void testDVTableWithAggregationMergeEngine(String changelogProducer) throws Exception {
+        sql(
+                String.format(
+                        "CREATE TABLE T (id INT PRIMARY KEY NOT ENFORCED, v INT) "
+                                + "WITH ('deletion-vectors.enabled' = 'true', 'changelog-producer' = '%s', "
+                                + "'merge-engine'='aggregation', 'fields.v.aggregate-function'='sum')",
+                        changelogProducer));
+
+        sql("INSERT INTO T VALUES (1, 111111111), (2, 2), (3, 3), (4, 4)");
+
+        sql("INSERT INTO T VALUES (2, 1), (3, 1)");
+
+        sql("INSERT INTO T VALUES (2, 1), (4, 1)");
+
+        // test batch read
+        assertThat(batchSql("SELECT * FROM T"))
+                .containsExactlyInAnyOrder(
+                        Row.of(1, 111111111), Row.of(2, 4), Row.of(3, 4), Row.of(4, 5));
+
+        // test streaming read
+        if (changelogProducer.equals("lookup")) {
+            try (BlockingIterator<Row, Row> iter =
+                    streamSqlBlockIter(
+                            "SELECT * FROM T /*+ OPTIONS('scan.mode'='from-snapshot-full','scan.snapshot-id' = '4') */")) {
+                assertThat(iter.collect(8))
+                        .containsExactlyInAnyOrder(
+                                Row.ofKind(RowKind.INSERT, 1, 111111111),
+                                Row.ofKind(RowKind.INSERT, 2, 3),
+                                Row.ofKind(RowKind.INSERT, 3, 4),
+                                Row.ofKind(RowKind.INSERT, 4, 4),
+                                Row.ofKind(RowKind.UPDATE_BEFORE, 2, 3),
+                                Row.ofKind(RowKind.UPDATE_AFTER, 2, 4),
+                                Row.ofKind(RowKind.UPDATE_BEFORE, 4, 4),
+                                Row.ofKind(RowKind.UPDATE_AFTER, 4, 5));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"none", "lookup"})
+    public void testDVTableWithPartialUpdateMergeEngine(String changelogProducer) throws Exception {
+        sql(
+                String.format(
+                        "CREATE TABLE T (id INT PRIMARY KEY NOT ENFORCED, v1 STRING, v2 STRING) "
+                                + "WITH ('deletion-vectors.enabled' = 'true', 'changelog-producer' = '%s', "
+                                + "'merge-engine'='partial-update')",
+                        changelogProducer));
+
+        sql(
+                "INSERT INTO T VALUES (1, '111111111', '1'), (2, '2', CAST(NULL AS STRING)), (3, '3', '3'), (4, CAST(NULL AS STRING), '4')");
+
+        sql("INSERT INTO T VALUES (2, CAST(NULL AS STRING), '2'), (3, '3_1', '3_1')");
+
+        sql(
+                "INSERT INTO T VALUES (2, '2_1', CAST(NULL AS STRING)), (4, '4', CAST(NULL AS STRING))");
+
+        // test batch read
+        assertThat(batchSql("SELECT * FROM T"))
+                .containsExactlyInAnyOrder(
+                        Row.of(1, "111111111", "1"),
+                        Row.of(2, "2_1", "2"),
+                        Row.of(3, "3_1", "3_1"),
+                        Row.of(4, "4", "4"));
+
+        // test streaming read
+        if (changelogProducer.equals("lookup")) {
+            try (BlockingIterator<Row, Row> iter =
+                    streamSqlBlockIter(
+                            "SELECT * FROM T /*+ OPTIONS('scan.mode'='from-snapshot-full','scan.snapshot-id' = '4') */")) {
+                assertThat(iter.collect(8))
+                        .containsExactlyInAnyOrder(
+                                Row.ofKind(RowKind.INSERT, 1, "111111111", "1"),
+                                Row.ofKind(RowKind.INSERT, 2, "2", "2"),
+                                Row.ofKind(RowKind.INSERT, 3, "3_1", "3_1"),
+                                Row.ofKind(RowKind.INSERT, 4, null, "4"),
+                                Row.ofKind(RowKind.UPDATE_BEFORE, 2, "2", "2"),
+                                Row.ofKind(RowKind.UPDATE_AFTER, 2, "2_1", "2"),
+                                Row.ofKind(RowKind.UPDATE_BEFORE, 4, null, "4"),
+                                Row.ofKind(RowKind.UPDATE_AFTER, 4, "4", "4"));
+            }
+        }
     }
 }
