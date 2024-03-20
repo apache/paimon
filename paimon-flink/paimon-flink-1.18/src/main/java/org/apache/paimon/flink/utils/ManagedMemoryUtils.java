47bc30fd13f4e05798f35ff87a0dc83cb3ae67d6
yuzelin <33053040+yuzelin@users.noreply.github.com>
2024-03-20 17:36:07 +0800

    [flink] Bump flink version to 1.19 (#3049)

--- paimon-flink/paimon-flink-1.18/src/main/java/org/apache/paimon/flink/utils/ManagedMemoryUtils.java
+++ paimon-flink/paimon-flink-1.18/src/main/java/org/apache/paimon/flink/utils/ManagedMemoryUtils.java
@@ -43,7 +43,6 @@ public class ManagedMemoryUtils {
                         operator.getOperatorConfig()
                                 .getManagedMemoryFractionOperatorUseCaseOfSlot(
                                         ManagedMemoryUseCase.OPERATOR,
-                                        environment.getJobConfiguration(),
                                         environment.getTaskManagerInfo().getConfiguration(),
                                         environment.getUserCodeClassLoader().asClassLoader()));
     }
