apache/incubator-paimon

introduce encryption interface

paimon-core/src/main/java/org/apache/paimon/encryption/PlaintextEncryptionManager.java
@@ -1,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.encryption;
+
+import org.apache.paimon.CoreOptions;
+
+/** Do not encrypt the data files. */
+public class PlaintextEncryptionManager implements EncryptionManager {
+
+    public static final String IDENTIFIER = "plaintext";
+
+    @Override
+    public KeyMetadata createLocalDataKey(KmsClient.CreateKeyResult createKeyResult) {
+        return null;
+    }
+
+    @Override
+    public byte[] decryptLocalDataKey(KeyMetadata keyMetadata) {
+        return null;
+    }
+
+    @Override
+    public String identifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public void configure(CoreOptions options) {}
+
+    @Override
+    public KmsClient kmsClient() {
+        return null;
+    }
+}

apache/paimon

paimon-core/src/main/java/org/apache/paimon/encryption/PlaintextEncryptionManager.java
@@ -16,27 +16,31 @@
  * limitations under the License.
  */
 
-package org.apache.paimon.format.orc;
+package org.apache.paimon.encryption;
 
-import org.apache.paimon.format.FileFormat;
-import org.apache.paimon.format.FileFormatFactory;
-import org.apache.paimon.format.FormatReadWriteTest;
+import org.apache.paimon.fs.FileIO;
 import org.apache.paimon.options.Options;
 
-/** An orc {@link FormatReadWriteTest}. */
-public class OrcFormatReadWriteTest extends FormatReadWriteTest {
+/** Do not encrypt the data files. */
+public class PlaintextEncryptionManager implements EncryptionManager {
 
-    protected OrcFormatReadWriteTest() {
-        super("orc");
+    public static final String IDENTIFIER = "plaintext";
+
+    @Override
+    public String identifier() {
+        return IDENTIFIER;
     }
 
     @Override
-    protected FileFormat fileFormat() {
-        FileFormatFactory.FormatContext formatContext =
-                FileFormatFactory.formatContextBuilder()
-                        .formatOptions(new Options())
-                        .readBatchSize(1024)
-                        .build();
-        return new OrcFileFormat(formatContext);
+    public void configure(Options options) {}
+
+    @Override
+    public FileIO decrypt(EnvelopeEncryptionManager.EncryptedFileIO encryptedFileIO) {
+        return encryptedFileIO.fileIO();
+    }
+
+    @Override
+    public EnvelopeEncryptionManager.EncryptedFileIO encrypt(FileIO fileIO) {
+        return new EnvelopeEncryptionManager.EncryptedFileIO(fileIO);
     }
 }