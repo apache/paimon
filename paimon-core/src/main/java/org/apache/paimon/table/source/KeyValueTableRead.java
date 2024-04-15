82ded222f022fa4cfc3f43bb22d5187b2c296e92
Jingsong <jingsonglee0@gmail.com>
2024-04-15 14:11:41 +0800

    fix

--- paimon-core/src/main/java/org/apache/paimon/table/source/KeyValueTableRead.java
+++ paimon-core/src/main/java/org/apache/paimon/table/source/KeyValueTableRead.java
@@ -117,7 +117,7 @@ public final class KeyValueTableRead extends AbstractDataTableRead<KeyValue> {
     @Override
     public RecordReader<InternalRow> reader(Split split) throws IOException {
         DataSplit dataSplit = (DataSplit) split;
-        if (!forceKeepDelete && !dataSplit.isStreaming() && split.convertToRawFiles().isPresent()) {
+        if (!dataSplit.isStreaming() && split.convertToRawFiles().isPresent()) {
             return batchRawRead.get().createReader(dataSplit);
         }
 
