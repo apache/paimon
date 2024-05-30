e77ea331067175cdb5c7261858f1d5e49a0f481f
wangwj <hongli.wwj@gmail.com>
2024-05-30 23:12:14 +0800

    [minor] fix LoggerFactor in PartitionTimeExtractor (#3444)

--- paimon-core/src/main/java/org/apache/paimon/partition/PartitionTimeExtractor.java
+++ paimon-core/src/main/java/org/apache/paimon/partition/PartitionTimeExtractor.java
@@ -18,8 +18,6 @@
 
 package org.apache.paimon.partition;
 
-import org.apache.paimon.operation.FileStoreCommitImpl;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,7 +50,8 @@ import static java.time.temporal.ChronoField.YEAR;
 /** Time extractor to extract time from partition values. */
 public class PartitionTimeExtractor {
 
-    private static final Logger LOG = LoggerFactory.getLogger(FileStoreCommitImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PartitionTimeExtractor.class);
+
     private static final DateTimeFormatter TIMESTAMP_FORMATTER =
             new DateTimeFormatterBuilder()
                     .appendValue(YEAR, 1, 10, SignStyle.NORMAL)
