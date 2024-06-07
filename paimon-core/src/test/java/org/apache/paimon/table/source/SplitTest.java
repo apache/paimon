04b7030da6d36251b3987a4ec22111cef2b0c61f
yejunhao <yejunhao.yjh@alibaba-inc.com>
2024-06-07 16:37:07 +0800

    fix comment

--- paimon-core/src/test/java/org/apache/paimon/table/source/SplitTest.java
+++ paimon-core/src/test/java/org/apache/paimon/table/source/SplitTest.java
@@ -19,30 +19,30 @@
 package org.apache.paimon.table.source;
 
 import org.apache.paimon.data.BinaryRow;
+import org.apache.paimon.data.BinaryRowWriter;
+import org.apache.paimon.data.BinaryString;
+import org.apache.paimon.data.Timestamp;
 import org.apache.paimon.io.DataFileMeta;
-import org.apache.paimon.io.DataFileMeta08Serializer;
 import org.apache.paimon.io.DataFileTestDataGenerator;
 import org.apache.paimon.io.DataInputDeserializer;
-import org.apache.paimon.io.DataInputView;
-import org.apache.paimon.io.DataInputViewStreamWrapper;
-import org.apache.paimon.io.DataOutputView;
 import org.apache.paimon.io.DataOutputViewStreamWrapper;
+import org.apache.paimon.stats.SimpleStats;
+import org.apache.paimon.utils.IOUtils;
 import org.apache.paimon.utils.InstantiationUtil;
-import org.apache.paimon.utils.SerializationUtils;
 
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.Nullable;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static org.apache.paimon.data.BinaryArray.fromLongArray;
+import static org.apache.paimon.data.BinaryRow.singleColumn;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /** Test for {@link DataSplit}. */
@@ -126,175 +126,61 @@ public class SplitTest {
     }
 
     @Test
-    public void testSerializerCompatibleStrictly() throws Exception {
-        DataFileTestDataGenerator gen = DataFileTestDataGenerator.builder().build();
-        DataFileTestDataGenerator.Data data = gen.next();
-        List<DataFileMeta> files = new ArrayList<>();
-        List<DataFileMeta> files2 = new ArrayList<>();
-        for (int i = 0; i < ThreadLocalRandom.current().nextInt(10); i++) {
-            DataFileMeta meta = gen.next().meta;
-            files.add(meta);
-            files2.add(
-                    new DataFileMeta(
-                            meta.fileName(),
-                            meta.fileSize(),
-                            meta.rowCount(),
-                            meta.minKey(),
-                            meta.maxKey(),
-                            meta.keyStats(),
-                            meta.valueStats(),
-                            meta.minSequenceNumber(),
-                            meta.maxSequenceNumber(),
-                            meta.schemaId(),
-                            meta.level(),
-                            meta.extraFiles(),
-                            meta.creationTime(),
-                            meta.deleteRowCount().orElse(null),
-                            meta.embeddedIndex(),
-                            null));
-        }
-        long snapshotId = ThreadLocalRandom.current().nextLong(100);
-        DataSplitWith08SerializerAnd09Deserializer split =
-                new DataSplitWith08SerializerAnd09Deserializer(
-                        snapshotId, data.partition, data.bucket, "my path", files);
-        DataSplitWith08SerializerAnd09Deserializer split2 =
-                new DataSplitWith08SerializerAnd09Deserializer(
-                        snapshotId, data.partition, data.bucket, "my path", files2);
+    public void testSerializer2() throws Exception {
+        SimpleStats keyStats =
+                new SimpleStats(
+                        singleColumn("min_key"),
+                        singleColumn("max_key"),
+                        fromLongArray(new Long[] {0L}));
+        SimpleStats valueStats =
+                new SimpleStats(
+                        singleColumn("min_value"),
+                        singleColumn("max_value"),
+                        fromLongArray(new Long[] {0L}));
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        DataOutputViewStreamWrapper view = new DataOutputViewStreamWrapper(out);
-        InstantiationUtil.serializeObject(view, split);
-        DataSplitWith08SerializerAnd09Deserializer actual =
-                InstantiationUtil.deserializeObject(
-                        out.toByteArray(), SplitTest.class.getClassLoader());
-        assertThat(actual).isEqualTo(split2);
-    }
+        DataFileMeta dataFile =
+                new DataFileMeta(
+                        "my_file",
+                        1024 * 1024,
+                        1024,
+                        singleColumn("min_key"),
+                        singleColumn("max_key"),
+                        keyStats,
+                        valueStats,
+                        15,
+                        200,
+                        5,
+                        3,
+                        Arrays.asList("extra1", "extra2"),
+                        Timestamp.fromLocalDateTime(LocalDateTime.parse("2022-03-02T20:20:12")),
+                        11L,
+                        new byte[] {1, 2, 4},
+                        null);
+        List<DataFileMeta> dataFiles = Collections.singletonList(dataFile);
 
-    /** To test compatible. DataSplit with 08 version serializer and 09 version deserializer. */
-    private static class DataSplitWith08SerializerAnd09Deserializer implements Split {
+        BinaryRow partition = new BinaryRow(1);
+        BinaryRowWriter binaryRowWriter = new BinaryRowWriter(partition);
+        binaryRowWriter.writeString(0, BinaryString.fromString("aaaaa"));
+        binaryRowWriter.complete();
 
-        private static final long serialVersionUID = 7L;
+        DataSplit split =
+                DataSplit.builder()
+                        .withSnapshot(18)
+                        .withPartition(partition)
+                        .withBucket(20)
+                        .withDataFiles(dataFiles)
+                        .withBucketPath("my path")
+                        .build();
 
-        private long snapshotId;
-        private BinaryRow partition;
-        private int bucket;
-        private String bucketPath;
+        byte[] v2Bytes =
+                IOUtils.readFully(
+                        SplitTest.class
+                                .getClassLoader()
+                                .getResourceAsStream("compatibility/datasplit-v1"),
+                        true);
 
-        private List<DataFileMeta> beforeFiles = new ArrayList<>();
-        @Nullable private List<DeletionFile> beforeDeletionFiles;
-
-        private List<DataFileMeta> dataFiles;
-        @Nullable private List<DeletionFile> dataDeletionFiles;
-
-        private boolean isStreaming = false;
-        private boolean rawConvertible;
-
-        public DataSplitWith08SerializerAnd09Deserializer(
-                long snapshotId,
-                BinaryRow partition,
-                int bucket,
-                String bucketPath,
-                List<DataFileMeta> dataFiles) {
-            this.snapshotId = snapshotId;
-            this.partition = partition;
-            this.bucket = bucket;
-            this.bucketPath = bucketPath;
-            this.dataFiles = dataFiles;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            DataSplitWith08SerializerAnd09Deserializer dataSplit =
-                    (DataSplitWith08SerializerAnd09Deserializer) o;
-            return snapshotId == dataSplit.snapshotId
-                    && bucket == dataSplit.bucket
-                    && isStreaming == dataSplit.isStreaming
-                    && rawConvertible == dataSplit.rawConvertible
-                    && Objects.equals(partition, dataSplit.partition)
-                    && Objects.equals(bucketPath, dataSplit.bucketPath)
-                    && Objects.equals(beforeFiles, dataSplit.beforeFiles)
-                    && Objects.equals(beforeDeletionFiles, dataSplit.beforeDeletionFiles)
-                    && Objects.equals(dataFiles, dataSplit.dataFiles)
-                    && Objects.equals(dataDeletionFiles, dataSplit.dataDeletionFiles);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(
-                    snapshotId,
-                    partition,
-                    bucket,
-                    bucketPath,
-                    beforeFiles,
-                    beforeDeletionFiles,
-                    dataFiles,
-                    dataDeletionFiles,
-                    isStreaming,
-                    rawConvertible);
-        }
-
-        private void writeObject(ObjectOutputStream out) throws IOException {
-            serialize(new DataOutputViewStreamWrapper(out));
-        }
-
-        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-            assign(deserialize(new DataInputViewStreamWrapper(in)));
-        }
-
-        private void assign(DataSplit other) {
-            this.snapshotId = other.snapshotId();
-            this.partition = other.partition();
-            this.bucket = other.bucket();
-            this.bucketPath = other.bucketPath();
-            this.beforeFiles = other.beforeFiles();
-            this.beforeDeletionFiles = other.beforeDeletionFiles().orElse(null);
-            this.dataFiles = other.dataFiles();
-            this.dataDeletionFiles = other.deletionFiles().orElse(null);
-            this.isStreaming = other.isStreaming();
-            this.rawConvertible = other.rawConvertible();
-        }
-
-        // 08 serialize method
-        public void serialize(DataOutputView out) throws IOException {
-            out.writeLong(snapshotId);
-            SerializationUtils.serializeBinaryRow(partition, out);
-            out.writeInt(bucket);
-            out.writeUTF(bucketPath);
-
-            DataFileMeta08Serializer dataFileSer = new DataFileMeta08Serializer();
-            out.writeInt(beforeFiles.size());
-            for (DataFileMeta file : beforeFiles) {
-                dataFileSer.serialize(file, out);
-            }
-
-            DeletionFile.serializeList(out, beforeDeletionFiles);
-
-            out.writeInt(dataFiles.size());
-            for (DataFileMeta file : dataFiles) {
-                dataFileSer.serialize(file, out);
-            }
-
-            DeletionFile.serializeList(out, dataDeletionFiles);
-
-            out.writeBoolean(isStreaming);
-
-            out.writeBoolean(rawConvertible);
-        }
-
-        // 09 desrialize method
-        public static DataSplit deserialize(DataInputView in) throws IOException {
-            return DataSplit.deserialize(in);
-        }
-
-        @Override
-        public long rowCount() {
-            return -1;
-        }
+        DataSplit actual =
+                InstantiationUtil.deserializeObject(v2Bytes, DataSplit.class.getClassLoader());
+        assertThat(actual).isEqualTo(split);
     }
 }
