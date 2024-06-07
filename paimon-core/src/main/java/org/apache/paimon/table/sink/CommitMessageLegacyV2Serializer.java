0bfc3600c33eb623fd2fb6b0ebe7cd61a612ef40
Jingsong <jingsonglee0@gmail.com>
2024-06-07 11:35:39 +0800

    fix

--- paimon-core/src/main/java/org/apache/paimon/table/sink/CommitMessageLegacyV2Serializer.java
+++ paimon-core/src/main/java/org/apache/paimon/table/sink/CommitMessageLegacyV2Serializer.java
@@ -18,19 +18,36 @@
 
 package org.apache.paimon.table.sink;
 
-import org.apache.paimon.index.IndexFileMetaLegacyV2Serializer;
+import org.apache.paimon.data.BinaryString;
+import org.apache.paimon.data.GenericRow;
+import org.apache.paimon.data.InternalRow;
+import org.apache.paimon.index.IndexFileMeta;
 import org.apache.paimon.io.CompactIncrement;
-import org.apache.paimon.io.DataFileMetaLegacyV2Serializer;
+import org.apache.paimon.io.DataFileMeta;
 import org.apache.paimon.io.DataIncrement;
 import org.apache.paimon.io.DataInputView;
 import org.apache.paimon.io.IndexIncrement;
+import org.apache.paimon.stats.BinaryTableStats;
+import org.apache.paimon.stats.FieldStatsArraySerializer;
+import org.apache.paimon.types.ArrayType;
+import org.apache.paimon.types.BigIntType;
+import org.apache.paimon.types.DataField;
+import org.apache.paimon.types.DataTypes;
+import org.apache.paimon.types.IntType;
+import org.apache.paimon.types.RowType;
+import org.apache.paimon.utils.ObjectSerializer;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.apache.paimon.utils.InternalRowUtils.fromStringArrayData;
+import static org.apache.paimon.utils.InternalRowUtils.toStringArrayData;
 import static org.apache.paimon.utils.SerializationUtils.deserializeBinaryRow;
+import static org.apache.paimon.utils.SerializationUtils.newBytesType;
+import static org.apache.paimon.utils.SerializationUtils.newStringType;
+import static org.apache.paimon.utils.SerializationUtils.serializeBinaryRow;
 
 /** A legacy version serializer for {@link CommitMessage}. */
 public class CommitMessageLegacyV2Serializer {
@@ -64,5 +81,107 @@ public class CommitMessageLegacyV2Serializer {
                         dataFileSerializer.deserializeList(view),
                         dataFileSerializer.deserializeList(view)),
                 new IndexIncrement(indexEntrySerializer.deserializeList(view)));
+    }
+
+    private static RowType legacyDataFileSchema() {
+        List<DataField> fields = new ArrayList<>();
+        fields.add(new DataField(0, "_FILE_NAME", newStringType(false)));
+        fields.add(new DataField(1, "_FILE_SIZE", new BigIntType(false)));
+        fields.add(new DataField(2, "_ROW_COUNT", new BigIntType(false)));
+        fields.add(new DataField(3, "_MIN_KEY", newBytesType(false)));
+        fields.add(new DataField(4, "_MAX_KEY", newBytesType(false)));
+        fields.add(new DataField(5, "_KEY_STATS", FieldStatsArraySerializer.schema()));
+        fields.add(new DataField(6, "_VALUE_STATS", FieldStatsArraySerializer.schema()));
+        fields.add(new DataField(7, "_MIN_SEQUENCE_NUMBER", new BigIntType(false)));
+        fields.add(new DataField(8, "_MAX_SEQUENCE_NUMBER", new BigIntType(false)));
+        fields.add(new DataField(9, "_SCHEMA_ID", new BigIntType(false)));
+        fields.add(new DataField(10, "_LEVEL", new IntType(false)));
+        fields.add(new DataField(11, "_EXTRA_FILES", new ArrayType(false, newStringType(false))));
+        fields.add(new DataField(12, "_CREATION_TIME", DataTypes.TIMESTAMP_MILLIS()));
+        return new RowType(fields);
+    }
+
+    private static RowType legacyIndexFileSchema() {
+        List<DataField> fields = new ArrayList<>();
+        fields.add(new DataField(0, "_INDEX_TYPE", newStringType(false)));
+        fields.add(new DataField(1, "_FILE_NAME", newStringType(false)));
+        fields.add(new DataField(2, "_FILE_SIZE", new BigIntType(false)));
+        fields.add(new DataField(3, "_ROW_COUNT", new BigIntType(false)));
+        return new RowType(fields);
+    }
+
+    /** A legacy version serializer for {@link DataFileMeta}. */
+    private static class DataFileMetaLegacyV2Serializer extends ObjectSerializer<DataFileMeta> {
+
+        private static final long serialVersionUID = 1L;
+
+        public DataFileMetaLegacyV2Serializer() {
+            super(legacyDataFileSchema());
+        }
+
+        @Override
+        public InternalRow toRow(DataFileMeta meta) {
+            return GenericRow.of(
+                    BinaryString.fromString(meta.fileName()),
+                    meta.fileSize(),
+                    meta.rowCount(),
+                    serializeBinaryRow(meta.minKey()),
+                    serializeBinaryRow(meta.maxKey()),
+                    meta.keyStats().toRow(),
+                    meta.valueStats().toRow(),
+                    meta.minSequenceNumber(),
+                    meta.maxSequenceNumber(),
+                    meta.schemaId(),
+                    meta.level(),
+                    toStringArrayData(meta.extraFiles()),
+                    meta.creationTime());
+        }
+
+        @Override
+        public DataFileMeta fromRow(InternalRow row) {
+            return new DataFileMeta(
+                    row.getString(0).toString(),
+                    row.getLong(1),
+                    row.getLong(2),
+                    deserializeBinaryRow(row.getBinary(3)),
+                    deserializeBinaryRow(row.getBinary(4)),
+                    BinaryTableStats.fromRow(row.getRow(5, 3)),
+                    BinaryTableStats.fromRow(row.getRow(6, 3)),
+                    row.getLong(7),
+                    row.getLong(8),
+                    row.getLong(9),
+                    row.getInt(10),
+                    fromStringArrayData(row.getArray(11)),
+                    row.getTimestamp(12, 3),
+                    null,
+                    null);
+        }
+    }
+
+    /** A legacy version serializer for {@link IndexFileMeta}. */
+    private static class IndexFileMetaLegacyV2Serializer extends ObjectSerializer<IndexFileMeta> {
+
+        public IndexFileMetaLegacyV2Serializer() {
+            super(legacyIndexFileSchema());
+        }
+
+        @Override
+        public InternalRow toRow(IndexFileMeta record) {
+            return GenericRow.of(
+                    BinaryString.fromString(record.indexType()),
+                    BinaryString.fromString(record.fileName()),
+                    record.fileSize(),
+                    record.rowCount());
+        }
+
+        @Override
+        public IndexFileMeta fromRow(InternalRow row) {
+            return new IndexFileMeta(
+                    row.getString(0).toString(),
+                    row.getString(1).toString(),
+                    row.getLong(2),
+                    row.getLong(3),
+                    null);
+        }
     }
 }
