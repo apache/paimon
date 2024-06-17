6b4df7a556ef74f8b5386b555ed9e956b31bb859
gsralex <gsralex@163.com>
2024-06-17 23:58:35 +0800

    [core] Fix expired partition sync meta store

--- paimon-core/src/main/java/org/apache/paimon/AbstractFileStore.java
+++ paimon-core/src/main/java/org/apache/paimon/AbstractFileStore.java
@@ -258,7 +258,8 @@ abstract class AbstractFileStore<T> implements FileStore<T> {
         MetastoreClient.Factory metastoreClientFactory =
                 catalogEnvironment.metastoreClientFactory();
         MetastoreClient metastoreClient = null;
-        if (options.partitionedTableInMetastore() && metastoreClientFactory != null) {
+        if (options.partitionedTableInMetastore()
+                && metastoreClientFactory != null) {
             metastoreClient = metastoreClientFactory.create();
         }
 
