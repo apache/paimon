apache/incubator-paimon

Kafka debezium json supports automatic discovery of primary keys

paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/kafka/KafkaDebeziumSyncTableActionITCase.java
@@ -103,4 +103,16 @@ public class KafkaDebeziumSyncTableActionITCase extends KafkaSyncTableActionITCa
     public void testAllTypesWithSchema() throws Exception {
         testAllTypesWithSchemaImpl(DEBEZIUM);
     }
+
+    @Test
+    @Timeout(120)
+    public void testRecordAndAutoDiscoveryPrimaryKeys() throws Exception {
+        testRecordWithPrimaryKeys(DEBEZIUM);
+    }
+
+    @Test
+    @Timeout(120)
+    public void testSchemaIncludeRecordAndAutoDiscoveryPrimaryKeys() throws Exception {
+        testSchemaIncludeRecordWithPrimaryKeys(DEBEZIUM);
+    }
 }

apache/paimon

paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/kafka/KafkaDebeziumSyncTableActionITCase.java
@@ -147,4 +147,16 @@ public class KafkaDebeziumSyncTableActionITCase extends KafkaSyncTableActionITCa
                 rowType,
                 Collections.singletonList("id"));
     }
+
+    @Timeout(120)
+    @Test
+    public void testRecordAndAutoDiscoveryPrimaryKeys() throws Exception {
+        testRecordWithPrimaryKeys(DEBEZIUM);
+    }
+
+    @Test
+    @Timeout(120)
+    public void testRecordIncludeSchemaAndAutoDiscoveryPrimaryKeys() throws Exception {
+        testSchemaIncludeRecordWithPrimaryKeys(DEBEZIUM);
+    }
 }