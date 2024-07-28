a84c3e766dfbfd3013d758b14480824f16d62dc8
xuyu <11161569@vivo.com>
2024-07-28 12:54:57 +0800

    fix ut

--- paimon-spark/paimon-spark-common/src/test/java/org/apache/paimon/spark/SparkGenericCatalogWithHiveTest.java
+++ paimon-spark/paimon-spark-common/src/test/java/org/apache/paimon/spark/SparkGenericCatalogWithHiveTest.java
@@ -62,8 +62,8 @@ public class SparkGenericCatalogWithHiveTest {
                         .master("local[2]")
                         .getOrCreate();
 
-        spark.sql("CREATE DATABASE IF NOT EXISTS my_db");
-        spark.sql("USE my_db");
+        spark.sql("CREATE DATABASE IF NOT EXISTS my_db1");
+        spark.sql("USE my_db1");
         spark.sql(
                 "CREATE TABLE IF NOT EXISTS t1 (a INT, Bb INT, c STRING) USING paimon TBLPROPERTIES"
                         + " ('file.format'='avro')");
@@ -88,7 +88,7 @@ public class SparkGenericCatalogWithHiveTest {
                         .master("local[2]")
                         .getOrCreate();
 
-        spark1.sql("USE my_db");
+        spark1.sql("USE my_db1");
         assertThrows(
                 RuntimeException.class,
                 () ->
