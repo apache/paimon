695083bce7abb9857c0f93d9fb8253bba847d2d9
Aitozi <aitozi@apache.org>
2024-07-31 14:20:57 +0800

    fix comments

--- paimon-common/src/test/java/org/apache/paimon/utils/InternalRowPartitionComputerTest.java
+++ paimon-common/src/test/java/org/apache/paimon/utils/InternalRowPartitionComputerTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/** Test for */
+/** Test for {@link InternalRowPartitionComputer}. */
 public class InternalRowPartitionComputerTest {
 
     @Test
--- paimon-core/src/test/java/org/apache/paimon/mergetree/LookupFileTest.java
+++ paimon-core/src/test/java/org/apache/paimon/mergetree/LookupFileTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/** Test for {@link LookupFile}. */
 public class LookupFileTest {
 
     @Test
