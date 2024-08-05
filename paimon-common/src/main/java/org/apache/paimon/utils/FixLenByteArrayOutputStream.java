5e686d9ba595eea5f57af87e7d61189f2bed7354
Jingsong <jingsonglee0@gmail.com>
2024-08-05 21:37:16 +0800

    fix

--- paimon-common/src/main/java/org/apache/paimon/utils/FixLenByteArrayOutputStream.java
+++ paimon-common/src/main/java/org/apache/paimon/utils/FixLenByteArrayOutputStream.java
@@ -34,7 +34,7 @@ public class FixLenByteArrayOutputStream {
         return buf;
     }
 
-    public int write(byte b[], int off, int len) {
+    public int write(byte[] b, int off, int len) {
         if ((off < 0) || (off > b.length) || (len < 0) || ((off + len) - b.length > 0)) {
             throw new IndexOutOfBoundsException();
         }
