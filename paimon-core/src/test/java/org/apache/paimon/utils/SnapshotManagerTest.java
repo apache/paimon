b770de7b6af4a32d7254d8e8360fa4048746e31c
yrf余若凡 <rfyu@trip.com>
2024-08-07 14:59:06 +0800

    format

--- paimon-core/src/test/java/org/apache/paimon/utils/SnapshotManagerTest.java
+++ paimon-core/src/test/java/org/apache/paimon/utils/SnapshotManagerTest.java
@@ -29,7 +29,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
