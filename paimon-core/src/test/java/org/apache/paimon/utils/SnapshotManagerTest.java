e07244b2170bd337f11706a9869edbb3b3b7a0a4
Jingsong <jingsonglee0@gmail.com>
2024-08-11 21:23:52 +0800

    [test] Fix compile in SnapshotManagerTest

--- paimon-core/src/test/java/org/apache/paimon/utils/SnapshotManagerTest.java
+++ paimon-core/src/test/java/org/apache/paimon/utils/SnapshotManagerTest.java
@@ -30,8 +30,12 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
