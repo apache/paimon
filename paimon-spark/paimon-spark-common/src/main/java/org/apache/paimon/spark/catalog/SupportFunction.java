aa42a73c6c93cb97ad76fdcd5e5bb617889f2b48
zouxxyy <zouxinyu.zxy@alibaba-inc.com>
2024-08-20 22:39:59 +0800

    1

---
+++ paimon-spark/paimon-spark-common/src/main/java/org/apache/paimon/spark/catalog/SupportFunction.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.spark.catalog;
+
+import org.apache.paimon.spark.catalog.functions.PaimonFunctions;
+
+import org.apache.spark.sql.catalyst.analysis.NoSuchFunctionException;
+import org.apache.spark.sql.catalyst.analysis.NoSuchNamespaceException;
+import org.apache.spark.sql.connector.catalog.FunctionCatalog;
+import org.apache.spark.sql.connector.catalog.Identifier;
+import org.apache.spark.sql.connector.catalog.SupportsNamespaces;
+import org.apache.spark.sql.connector.catalog.functions.UnboundFunction;
+
+import java.util.Arrays;
+
+import scala.Option;
+
+import static org.apache.paimon.catalog.Catalog.SYSTEM_DATABASE_NAME;
+
+/** Catalog methods for working with Functions. */
+public interface SupportFunction extends FunctionCatalog, SupportsNamespaces {
+
+    default boolean isFunctionNamespace(String[] namespace) {
+        // Allow for empty namespace, as Spark's bucket join will use `bucket` function with empty
+        // namespace to generate transforms for partitioning. Otherwise, use `sys` namespace.
+        return namespace.length == 0 || isSystemNamespace(namespace);
+    }
+
+    static boolean isSystemNamespace(String[] namespace) {
+        return namespace.length == 1 && namespace[0].equalsIgnoreCase(SYSTEM_DATABASE_NAME);
+    }
+
+    @Override
+    default Identifier[] listFunctions(String[] namespace) throws NoSuchNamespaceException {
+        if (isFunctionNamespace(namespace)) {
+            return PaimonFunctions.names().stream()
+                    .map(name -> Identifier.of(namespace, name))
+                    .toArray(Identifier[]::new);
+        } else if (namespaceExists(namespace)) {
+            return new Identifier[0];
+        } else
+            throw new NoSuchNamespaceException(
+                    "Namespace " + Arrays.toString(namespace) + " is not valid", Option.empty());
+    }
+
+    @Override
+    default UnboundFunction loadFunction(Identifier ident) throws NoSuchFunctionException {
+        if (isFunctionNamespace(ident.namespace())) {
+            UnboundFunction func = PaimonFunctions.load(ident.name());
+            if (func != null) {
+                return func;
+            }
+        }
+        throw new NoSuchFunctionException(
+                "Function " + ident + " is not a paimon function", Option.empty());
+    }
+}
---
+++ paimon-spark/paimon-spark-common/src/test/scala/org/apache/paimon/spark/sql/PaimonFunctionTest.scala
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.spark.sql
+
+import org.apache.paimon.spark.PaimonHiveTestBase
+import org.apache.paimon.spark.catalog.functions.PaimonFunctions
+
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.connector.catalog.{FunctionCatalog, Identifier}
+
+class PaimonFunctionTest extends PaimonHiveTestBase {
+
+  test("Paimon function: list and load functions") {
+    Seq("paimon", sparkCatalogName, paimonHiveCatalogName).foreach {
+      catalogName =>
+        {
+          sql(s"use $catalogName")
+          val functionCatalog =
+            spark.sessionState.catalogManager.currentCatalog.asInstanceOf[FunctionCatalog]
+          val identifiers = functionCatalog.listFunctions(Array.empty)
+
+          // test load paimon function
+          PaimonFunctions
+            .names()
+            .forEach(
+              name => {
+                val bucketFunIdent = identifiers.find(x => x.name().equals(name))
+                assert(bucketFunIdent.isDefined)
+                val function = functionCatalog.loadFunction(bucketFunIdent.get)
+                assert(function.name().equals(name))
+              })
+
+          // test load fake function
+          assertThrows[Throwable] {
+            functionCatalog.loadFunction(Identifier.of(Array.empty, "fake_fun"))
+          }
+        }
+    }
+  }
+
+  test("Paimon function: show functions") {
+    Seq("paimon", sparkCatalogName, paimonHiveCatalogName).foreach {
+      catalogName =>
+        {
+          sql(s"use $catalogName")
+          assert(sql("show functions").collect().length > 0)
+        }
+    }
+  }
+
+  test("Paimon function: bucket join with SparkGenericCatalog") {
+    sql(s"use $sparkCatalogName")
+    assume(gteqSpark3_3)
+    withTable("t1", "t2") {
+      sql(
+        "CREATE TABLE t1 (id INT, c STRING) using paimon TBLPROPERTIES ('primary-key' = 'id', 'bucket'='10')")
+      sql("INSERT INTO t1 VALUES (1, 'x1'), (2, 'x3'), (3, 'x3'), (4, 'x4'), (5, 'x5')")
+      sql(
+        "CREATE TABLE t2 (id INT, c STRING) using paimon TBLPROPERTIES ('primary-key' = 'id', 'bucket'='10')")
+      sql("INSERT INTO t2 VALUES (1, 'x1'), (2, 'x3'), (3, 'x3'), (4, 'x4'), (5, 'x5')")
+      checkAnswer(
+        sql("SELECT * FROM t1 JOIN t2 on t1.id = t2.id order by t1.id"),
+        Seq(
+          Row(1, "x1", 1, "x1"),
+          Row(2, "x3", 2, "x3"),
+          Row(3, "x3", 3, "x3"),
+          Row(4, "x4", 4, "x4"),
+          Row(5, "x5", 5, "x5"))
+      )
+    }
+  }
+}
