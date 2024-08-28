f029eb2b85a6a9dd67063a71b1d4997b3108a1b0
joey.ljy <joey.ljy@alibaba-inc.com>
2024-08-28 15:09:03 +0800

    Fix PaimonStatsUtils to include all methods

--- paimon-spark/paimon-spark-3.3/src/main/scala/org/apache/spark/sql/PaimonStatsUtils.scala
+++ paimon-spark/paimon-spark-3.3/src/main/scala/org/apache/spark/sql/PaimonStatsUtils.scala
@@ -19,12 +19,20 @@
 package org.apache.spark.sql
 
 import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.plans.logical.{ColumnStat, LogicalPlan}
 import org.apache.spark.sql.connector.catalog.Identifier
 import org.apache.spark.sql.execution.command.CommandUtils
 import org.apache.spark.sql.internal.SessionState
+import org.apache.spark.sql.types.{BinaryType, BooleanType, DataType, DatetimeType, DecimalType, DoubleType, FloatType, IntegralType, StringType}
 
 import java.net.URI
 
+/**
+ * Some classes or methods defined in the spark project are marked as private under
+ * [[org.apache.spark.sql]] package, Hence, use this class to adapt then so that we can use them
+ * indirectly.
+ */
 object PaimonStatsUtils {
 
   def calculateTotalSize(
@@ -36,5 +44,32 @@ object PaimonStatsUtils {
       sessionState,
       new TableIdentifier(identifier.name(), Some(identifier.namespace().head)),
       locationUri)
+  }
+
+  def computeColumnStats(
+      sparkSession: SparkSession,
+      relation: LogicalPlan,
+      columns: Seq[Attribute]): (Long, Map[Attribute, ColumnStat]) = {
+    CommandUtils.computeColumnStats(sparkSession, relation, columns)
+  }
+
+  /** [[IntegralType]] is private in spark, therefore we need add it here. */
+  def analyzeSupportsType(dataType: DataType): Boolean = dataType match {
+    case _: IntegralType => true
+    case _: DecimalType => true
+    case DoubleType | FloatType => true
+    case BooleanType => true
+    case _: DatetimeType => true
+    case BinaryType | StringType => true
+    case _ => false
+  }
+
+  def hasMinMax(dataType: DataType): Boolean = dataType match {
+    case _: IntegralType => true
+    case _: DecimalType => true
+    case DoubleType | FloatType => true
+    case BooleanType => true
+    case _: DatetimeType => true
+    case _ => false
   }
 }
