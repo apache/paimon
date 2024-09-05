be450b85b2bbca53baaa2328c9361d784a926e3b
Fang Yong <zjureel@gmail.com>
2024-09-05 16:25:59 +0800

    [core] Support bloom filter in sort file store reader

--- paimon-common/src/test/java/org/apache/paimon/lookup/sort/SortLookupStoreFactoryTest.java
+++ paimon-common/src/test/java/org/apache/paimon/lookup/sort/SortLookupStoreFactoryTest.java
@@ -24,33 +24,59 @@ import org.apache.paimon.data.serializer.RowCompactedSerializer;
 import org.apache.paimon.io.cache.CacheManager;
 import org.apache.paimon.lookup.LookupStoreFactory.Context;
 import org.apache.paimon.options.MemorySize;
+import org.apache.paimon.testutils.junit.parameterized.ParameterizedTestExtension;
+import org.apache.paimon.testutils.junit.parameterized.Parameters;
 import org.apache.paimon.types.IntType;
 import org.apache.paimon.types.RowType;
+import org.apache.paimon.utils.BloomFilter;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /** Test for {@link SortLookupStoreFactory}. */
+@ExtendWith(ParameterizedTestExtension.class)
 public class SortLookupStoreFactoryTest {
     private static final int VALUE_COUNT = 10_000_000;
     private static final int QUERY_COUNT = 10_000;
 
     private final ThreadLocalRandom rnd = ThreadLocalRandom.current();
+    private final boolean bloomFilterEnabled;
+    private final CompressOptions compress;
 
     @TempDir Path tempDir;
 
     private File file;
+
+    public SortLookupStoreFactoryTest(List<Object> var) {
+        this.bloomFilterEnabled = (Boolean) var.get(0);
+        this.compress = new CompressOptions((String) var.get(1), 1);
+    }
+
+    @SuppressWarnings("unused")
+    @Parameters(name = "enableBf&compress-{0}")
+    public static List<List<Object>> getVarSeg() {
+        return Arrays.asList(
+                Arrays.asList(true, "none"),
+                Arrays.asList(false, "none"),
+                Arrays.asList(false, "lz4"),
+                Arrays.asList(true, "lz4"),
+                Arrays.asList(false, "zstd"),
+                Arrays.asList(true, "zstd"));
+    }
 
     @BeforeEach
     public void before() throws Exception {
@@ -60,16 +86,17 @@ public class SortLookupStoreFactoryTest {
         }
     }
 
-    @Test
+    @TestTemplate
     public void testNormal() throws IOException {
         SortLookupStoreFactory factory =
                 new SortLookupStoreFactory(
                         Comparator.naturalOrder(),
                         new CacheManager(MemorySize.ofMebiBytes(1)),
                         1024,
-                        CompressOptions.defaultOptions());
+                        compress);
 
-        SortLookupStoreWriter writer = factory.createWriter(file, null);
+        SortLookupStoreWriter writer =
+                factory.createWriter(file, createBloomFiler(bloomFilterEnabled));
         for (int i = 0; i < VALUE_COUNT; i++) {
             byte[] bytes = toBytes(i);
             writer.put(bytes, bytes);
@@ -82,10 +109,13 @@ public class SortLookupStoreFactoryTest {
             byte[] bytes = toBytes(query);
             assertThat(fromBytes(reader.lookup(bytes))).isEqualTo(query);
         }
+
+        assertThat(reader.lookup(toBytes(VALUE_COUNT + 1000))).isNull();
+
         reader.close();
     }
 
-    @Test
+    @TestTemplate
     public void testIntKey() throws IOException {
         RowCompactedSerializer keySerializer =
                 new RowCompactedSerializer(RowType.of(new IntType()));
@@ -95,8 +125,9 @@ public class SortLookupStoreFactoryTest {
                         keySerializer.createSliceComparator(),
                         new CacheManager(MemorySize.ofMebiBytes(1)),
                         64 * 1024,
-                        CompressOptions.defaultOptions());
-        SortLookupStoreWriter writer = factory.createWriter(file, null);
+                        compress);
+        SortLookupStoreWriter writer =
+                factory.createWriter(file, createBloomFiler(bloomFilterEnabled));
         for (int i = 0; i < VALUE_COUNT; i++) {
             byte[] bytes = toBytes(keySerializer, row, i);
             writer.put(bytes, toBytes(i));
@@ -114,7 +145,17 @@ public class SortLookupStoreFactoryTest {
                 throw new RuntimeException(e);
             }
         }
+
+        assertThat(reader.lookup(toBytes(VALUE_COUNT + 1000))).isNull();
+
         reader.close();
+    }
+
+    private BloomFilter.Builder createBloomFiler(boolean enabled) {
+        if (!enabled) {
+            return null;
+        }
+        return BloomFilter.builder(100, 0.01);
     }
 
     private byte[] toBytes(RowCompactedSerializer serializer, GenericRow row, int i) {
