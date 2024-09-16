468ccc5bcdaf394e123b3eeface7fac3b597f9f3
xuzifu666 <1206332514@qq.com>
2024-09-16 11:17:10 +0800

    fix style

--- paimon-spark/paimon-spark-common/src/main/java/org/apache/paimon/spark/procedure/RemoveOrphanFilesProcedure.java
+++ paimon-spark/paimon-spark-common/src/main/java/org/apache/paimon/spark/procedure/RemoveOrphanFilesProcedure.java
@@ -119,8 +119,7 @@ public class RemoveOrphanFilesProcedure extends BaseProcedure {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        int parallelism =
-                args.isNullAt(3) ? tableCleans.size() : args.getInt(3);
+        int parallelism = args.isNullAt(3) ? tableCleans.size() : args.getInt(3);
         JavaSparkContext javaSparkContext = new JavaSparkContext(spark().sparkContext());
         List<Path> rets =
                 javaSparkContext
