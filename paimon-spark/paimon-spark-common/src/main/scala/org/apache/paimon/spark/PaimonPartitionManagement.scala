7c8746bc5f549ba386002280bd534d9da798dba0
xuzifu666 <1206332514@qq.com>
2024-10-15 21:48:27 +0800

    remote import

--- paimon-spark/paimon-spark-common/src/main/scala/org/apache/paimon/spark/PaimonPartitionManagement.scala
+++ paimon-spark/paimon-spark-common/src/main/scala/org/apache/paimon/spark/PaimonPartitionManagement.scala
@@ -32,7 +32,6 @@ import org.apache.spark.sql.catalyst.util.CharVarcharUtils
 import org.apache.spark.sql.connector.catalog.SupportsAtomicPartitionManagement
 import org.apache.spark.sql.types.StructType
 
-import java.lang.reflect.Method
 import java.util.{LinkedHashMap, Map => JMap, Objects, UUID}
 
 import scala.collection.JavaConverters._
