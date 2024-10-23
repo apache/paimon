0aa514116193f7d283ec009e5461f3608970781e
tsreaper <tsreaper96@gmail.com>
2024-10-23 14:28:52 +0800

    [fix] Fix failed tests

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/sink/CommitterOperatorTest.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/sink/CommitterOperatorTest.java
@@ -595,7 +595,7 @@ public class CommitterOperatorTest extends CommitterOperatorTestBase {
                         table, commit, Committer.createContext("", metricGroup, true, false, null));
         committer.commit(Collections.singletonList(manifestCommittable));
         CommitterMetrics metrics = committer.getCommitterMetrics();
-        assertThat(metrics.getNumBytesOutCounter().getCount()).isEqualTo(529);
+        assertThat(metrics.getNumBytesOutCounter().getCount()).isEqualTo(533);
         assertThat(metrics.getNumRecordsOutCounter().getCount()).isEqualTo(2);
         committer.close();
     }
