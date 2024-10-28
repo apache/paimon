f7e4563797132ec5c22d3d3e911e12d633fdaec7
xuyu <11161569@vivo.com>
2024-10-28 09:48:14 +0800

    fix comments

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/SchemaChangeITCase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/SchemaChangeITCase.java
@@ -1010,7 +1010,7 @@ public class SchemaChangeITCase extends CatalogITCaseBase {
     @Test
     public void testSequenceFieldSortOrder() {
 
-        // test default condition
+        // test default condition which get the largest record
         sql(
                 "CREATE TABLE T1 (a STRING PRIMARY KEY NOT ENFORCED, b STRING, c STRING) WITH ('sequence.field'='c')");
         sql("INSERT INTO T1 VALUES ('a', 'b', 'l')");
@@ -1019,7 +1019,7 @@ public class SchemaChangeITCase extends CatalogITCaseBase {
         List<Row> sql = sql("select * from T1");
         assertThat(sql("select * from T1").toString()).isEqualTo("[+I[a, d, n]]");
 
-        // test for get largest record
+        // test for get small record
         sql(
                 "CREATE TABLE T2 (a STRING PRIMARY KEY NOT ENFORCED, b STRING, c BIGINT) WITH ('sequence.field'='c', 'sequence.field.sort.is.ascending'='false')");
         sql("INSERT INTO T2 VALUES ('a', 'b', 1)");
@@ -1028,7 +1028,7 @@ public class SchemaChangeITCase extends CatalogITCaseBase {
         sql = sql("select * from T2");
         assertThat(sql("select * from T2").toString()).isEqualTo("[+I[a, b, 1]]");
 
-        // test for get small record
+        // test for get largest record
         sql(
                 "CREATE TABLE T3 (a STRING PRIMARY KEY NOT ENFORCED, b STRING, c DOUBLE) WITH ('sequence.field'='c', 'sequence.field.sort.is.ascending'='true')");
         sql("INSERT INTO T3 VALUES ('a', 'b', 1.0)");
