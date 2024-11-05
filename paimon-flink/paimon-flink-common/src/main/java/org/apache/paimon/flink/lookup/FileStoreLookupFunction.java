3ff13dc821e26320ee27d4952db953aca44a5c8e
yuzelin <33053040+yuzelin@users.noreply.github.com>
2024-11-05 18:09:12 +0800

    [flink] Lookup join supports time periods black list (#4413)

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/lookup/FileStoreLookupFunction.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/lookup/FileStoreLookupFunction.java
@@ -32,9 +32,12 @@ import org.apache.paimon.table.FileStoreTable;
 import org.apache.paimon.table.Table;
 import org.apache.paimon.table.source.OutOfRangeException;
 import org.apache.paimon.types.RowType;
+import org.apache.paimon.utils.DateTimeUtils;
 import org.apache.paimon.utils.FileIOUtils;
 import org.apache.paimon.utils.Filter;
+import org.apache.paimon.utils.Pair;
 import org.apache.paimon.utils.RowDataToObjectArrayConverter;
+import org.apache.paimon.utils.StringUtils;
 
 import org.apache.paimon.shade.guava30.com.google.common.primitives.Ints;
 
@@ -53,6 +56,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.time.Duration;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -62,6 +66,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -69,6 +74,7 @@ import java.util.stream.IntStream;
 
 import static org.apache.paimon.CoreOptions.CONTINUOUS_DISCOVERY_INTERVAL;
 import static org.apache.paimon.flink.FlinkConnectorOptions.LOOKUP_CACHE_MODE;
+import static org.apache.paimon.flink.FlinkConnectorOptions.LOOKUP_REFRESH_TIME_PERIODS_BLACKLIST;
 import static org.apache.paimon.flink.query.RemoteTableQuery.isRemoteServiceAvailable;
 import static org.apache.paimon.lookup.RocksDBOptions.LOOKUP_CACHE_ROWS;
 import static org.apache.paimon.lookup.RocksDBOptions.LOOKUP_CONTINUOUS_DISCOVERY_INTERVAL;
@@ -88,12 +94,16 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
     private final List<String> joinKeys;
     @Nullable private final Predicate predicate;
 
-    private transient Duration refreshInterval;
+    private final List<Pair<Long, Long>> timePeriodsBlacklist;
+    private long nextBlacklistCheckTime;
+
     private transient File path;
     private transient LookupTable lookupTable;
 
-    // timestamp when cache expires
-    private transient long nextLoadTime;
+    // interval of refreshing lookup table
+    private transient Duration refreshInterval;
+    // timestamp when refreshing lookup table
+    private transient long nextRefreshTime;
 
     protected FunctionContext functionContext;
 
@@ -131,6 +141,45 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
         }
 
         this.predicate = predicate;
+
+        this.timePeriodsBlacklist =
+                parseTimePeriodsBlacklist(
+                        table.options().get(LOOKUP_REFRESH_TIME_PERIODS_BLACKLIST.key()));
+        this.nextBlacklistCheckTime = -1;
+    }
+
+    private List<Pair<Long, Long>> parseTimePeriodsBlacklist(String blacklist) {
+        if (StringUtils.isNullOrWhitespaceOnly(blacklist)) {
+            return Collections.emptyList();
+        }
+        String[] timePeriods = blacklist.split(",");
+        List<Pair<Long, Long>> result = new ArrayList<>();
+        for (String period : timePeriods) {
+            String[] times = period.split("->");
+            if (times.length != 2) {
+                throw new IllegalArgumentException(
+                        String.format("Incorrect time periods format: [%s].", blacklist));
+            }
+
+            long left = parseToMillis(times[0]);
+            long right = parseToMillis(times[1]);
+            if (left > right) {
+                throw new IllegalArgumentException(
+                        String.format("Incorrect time period: [%s->%s].", times[0], times[1]));
+            }
+            result.add(Pair.of(left, right));
+        }
+        return result;
+    }
+
+    private long parseToMillis(String dateTime) {
+        try {
+            return DateTimeUtils.parseTimestampData(dateTime + ":00", 3, TimeZone.getDefault())
+                    .getMillisecond();
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(
+                    String.format("Date time format error: [%s].", dateTime), e);
+        }
     }
 
     public void open(FunctionContext context) throws Exception {
@@ -149,11 +198,7 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
     }
 
     private void open() throws Exception {
-        if (partitionLoader != null) {
-            partitionLoader.open();
-        }
-
-        this.nextLoadTime = -1;
+        this.nextRefreshTime = -1;
 
         Options options = Options.fromMap(table.options());
         this.refreshInterval =
@@ -197,7 +242,15 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
             this.lookupTable = FullCacheLookupTable.create(context, options.get(LOOKUP_CACHE_ROWS));
         }
 
-        refreshDynamicPartition(false);
+        if (partitionLoader != null) {
+            partitionLoader.open();
+            partitionLoader.checkRefresh();
+            BinaryRow partition = partitionLoader.partition();
+            if (partition != null) {
+                lookupTable.specificPartitionFilter(createSpecificPartFilter(partition));
+            }
+        }
+
         if (cacheRowFilter != null) {
             lookupTable.specifyCacheRowFilter(cacheRowFilter);
         }
@@ -222,15 +275,14 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
 
     public Collection<RowData> lookup(RowData keyRow) {
         try {
-            checkRefresh();
+            tryRefresh();
 
             InternalRow key = new FlinkRowWrapper(keyRow);
             if (partitionLoader != null) {
-                InternalRow partition = refreshDynamicPartition(true);
-                if (partition == null) {
+                if (partitionLoader.partition() == null) {
                     return Collections.emptyList();
                 }
-                key = JoinedRow.join(key, partition);
+                key = JoinedRow.join(key, partitionLoader.partition());
             }
 
             List<InternalRow> results = lookupTable.get(key);
@@ -245,28 +297,6 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Nullable
-    private BinaryRow refreshDynamicPartition(boolean reopen) throws Exception {
-        if (partitionLoader == null) {
-            return null;
-        }
-
-        boolean partitionChanged = partitionLoader.checkRefresh();
-        BinaryRow partition = partitionLoader.partition();
-        if (partition == null) {
-            return null;
-        }
-
-        lookupTable.specificPartitionFilter(createSpecificPartFilter(partition));
-
-        if (partitionChanged && reopen) {
-            lookupTable.close();
-            lookupTable.open();
-        }
-
-        return partition;
     }
 
     private Predicate createSpecificPartFilter(BinaryRow partition) {
@@ -293,20 +323,73 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
         }
     }
 
-    private void checkRefresh() throws Exception {
-        if (nextLoadTime > System.currentTimeMillis()) {
+    @VisibleForTesting
+    void tryRefresh() throws Exception {
+        // 1. check if this time is in black list
+        long currentTimeMillis = System.currentTimeMillis();
+        if (nextBlacklistCheckTime > currentTimeMillis) {
             return;
         }
-        if (nextLoadTime > 0) {
+
+        Pair<Long, Long> period = getFirstTimePeriods(timePeriodsBlacklist, currentTimeMillis);
+        if (period != null) {
+            LOG.info(
+                    "Current time {} is in black list {}-{}, so try to refresh cache next time.",
+                    currentTimeMillis,
+                    period.getLeft(),
+                    period.getRight());
+            nextBlacklistCheckTime = period.getRight() + 1;
+            return;
+        }
+
+        // 2. refresh dynamic partition
+        if (partitionLoader != null) {
+            boolean partitionChanged = partitionLoader.checkRefresh();
+            BinaryRow partition = partitionLoader.partition();
+            if (partition == null) {
+                // no data to be load, fast exit
+                return;
+            }
+
+            if (partitionChanged) {
+                // reopen with latest partition
+                lookupTable.specificPartitionFilter(createSpecificPartFilter(partition));
+                lookupTable.close();
+                lookupTable.open();
+                // no need to refresh the lookup table because it is reopened
+                return;
+            }
+        }
+
+        // 3. refresh lookup table
+        if (shouldRefreshLookupTable()) {
+            lookupTable.refresh();
+            nextRefreshTime = System.currentTimeMillis() + refreshInterval.toMillis();
+        }
+    }
+
+    @Nullable
+    private Pair<Long, Long> getFirstTimePeriods(List<Pair<Long, Long>> timePeriods, long time) {
+        for (Pair<Long, Long> period : timePeriods) {
+            if (period.getLeft() <= time && time <= period.getRight()) {
+                return period;
+            }
+        }
+        return null;
+    }
+
+    private boolean shouldRefreshLookupTable() {
+        if (nextRefreshTime > System.currentTimeMillis()) {
+            return false;
+        }
+
+        if (nextRefreshTime > 0) {
             LOG.info(
                     "Lookup table {} has refreshed after {} second(s), refreshing",
                     table.name(),
                     refreshInterval.toMillis() / 1000);
         }
-
-        refresh();
-
-        nextLoadTime = System.currentTimeMillis() + refreshInterval.toMillis();
+        return true;
     }
 
     @VisibleForTesting
@@ -314,8 +397,9 @@ public class FileStoreLookupFunction implements Serializable, Closeable {
         return lookupTable;
     }
 
-    private void refresh() throws Exception {
-        lookupTable.refresh();
+    @VisibleForTesting
+    long nextBlacklistCheckTime() {
+        return nextBlacklistCheckTime;
     }
 
     @Override
