a626cae8865eef0a546351ac88e7db01708a4168
zouxxyy <zouxinyu.zxy@alibaba-inc.com>
2024-11-11 12:46:40 +0800

    update spark common pom

--- paimon-spark/paimon-spark-common/src/test/java/org/apache/paimon/spark/SparkCatalogWithHiveTest.java
+++ paimon-spark/paimon-spark-common/src/test/java/org/apache/paimon/spark/SparkCatalogWithHiveTest.java
@@ -96,8 +96,8 @@ public class SparkCatalogWithHiveTest {
                 .isGreaterThan(0);
 
         // todo: There are some bugs with Spark CSV table's options. In Spark 3.x, both reading and
-        // writing using the default delimiter value ',' even if we set specific it. In Spark 4.x,
-        // reading is correct, but writing is still incorrect, just skip it for now.
+        // writing using the default delimiter value ',' even if we specific it. In Spark 4.x,
+        // reading is correct, but writing is still incorrect, just skip setting it for now.
         // test csv table
 
         spark.sql(
