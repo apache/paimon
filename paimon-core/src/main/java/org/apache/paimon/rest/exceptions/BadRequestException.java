a52eb443a87c9eba2872d96beea4b580f7894903
yantian <lining.jln@alibaba-inc.com>
2024-11-20 13:53:52 +0800

    [core] Add implementation to support REST Catalog

--- paimon-core/src/main/java/org/apache/paimon/rest/exceptions/BadRequestException.java
+++ paimon-core/src/main/java/org/apache/paimon/rest/exceptions/BadRequestException.java
@@ -24,8 +24,4 @@ public class BadRequestException extends RESTException {
     public BadRequestException(String message, Object... args) {
         super(message, args);
     }
-
-    public BadRequestException(Throwable cause, String message, Object... args) {
-        super(cause, message, args);
-    }
 }
