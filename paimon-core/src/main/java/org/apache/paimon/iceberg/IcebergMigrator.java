021a40747c098417b69610aa0abc677ad112863f
LsomeYeah <lleiyeah.cat@outlook.com>
2024-12-02 18:20:36 +0800

    second version and test

--- paimon-core/src/main/java/org/apache/paimon/iceberg/IcebergMigrator.java
+++ paimon-core/src/main/java/org/apache/paimon/iceberg/IcebergMigrator.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.paimon.iceberg;
 
 import org.apache.paimon.catalog.Catalog;
@@ -24,89 +42,152 @@ import org.apache.paimon.table.sink.BatchTableCommit;
 import org.apache.paimon.table.sink.CommitMessage;
 import org.apache.paimon.types.DataField;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
+
+import static org.apache.paimon.utils.ThreadPoolUtils.createCachedThreadPool;
 
 /** doc. */
 public class IcebergMigrator implements Migrator {
+    private static final Logger LOG = LoggerFactory.getLogger(IcebergMigrator.class);
     private static final String VERSION_HINT_FILENAME = "version-hint.text";
 
+    private final ThreadPoolExecutor executor;
+
     private final Catalog paimonCatalog;
-    // TODO:paimon的FileIO应该和iceberg的FileIO区分开来？用各自的FileIO去读各自的文件
-    private final FileIO fileIO;
+    private final FileIO paimonFileIO;
     private final String paimonDatabaseName;
     private final String paimonTableNameame;
 
+    private final int icebergNewestSnapshotId;
+    // Path factory for iceberg metadata
     private final IcebergPathFactory icebergPathFactory;
+    // metadata for newest iceberg snapshot
     private final IcebergMetadata icebergMetadata;
+
+    private boolean ignoreDelete = false;
 
     public IcebergMigrator(
             Catalog paimonCatalog,
-            IcebergPathFactory icebergPathFactory,
+            Path icebergMetaPath,
             String paimonDatabaseName,
-            String paimonTableNameame) {
+            String paimonTableNameame,
+            Integer parallelism) {
         this.paimonCatalog = paimonCatalog;
-        this.fileIO = paimonCatalog.fileIO();
+        this.paimonFileIO = paimonCatalog.fileIO();
         this.paimonDatabaseName = paimonDatabaseName;
         this.paimonTableNameame = paimonTableNameame;
 
-        this.icebergPathFactory = icebergPathFactory;
+        this.icebergPathFactory = new IcebergPathFactory(icebergMetaPath);
+        this.icebergNewestSnapshotId = getIcebergNewestSnapshotId();
         this.icebergMetadata =
                 IcebergMetadata.fromPath(
-                        fileIO, icebergPathFactory.toMetadataPath(getIcebergNewestSnapshotId()));
+                        paimonFileIO, icebergPathFactory.toMetadataPath(icebergNewestSnapshotId));
+
+        this.executor = createCachedThreadPool(parallelism, "ICEBERG_MIGRATOR");
     }
 
     @Override
     public void executeMigrate() throws Exception {
-
         Schema paimonSchema = icebergSchemaToPaimonSchema(icebergMetadata);
         Identifier paimonIdentifier = Identifier.create(paimonDatabaseName, paimonTableNameame);
-        paimonCatalog.createDatabase(paimonDatabaseName, true);
+
+        paimonCatalog.createDatabase(paimonDatabaseName, false);
         paimonCatalog.createTable(paimonIdentifier, paimonSchema, false);
 
-        FileStoreTable paimonTable = (FileStoreTable) paimonCatalog.getTable(paimonIdentifier);
+        try {
+            FileStoreTable paimonTable = (FileStoreTable) paimonCatalog.getTable(paimonIdentifier);
 
-        IcebergManifestFile manifestFile =
-                IcebergManifestFile.create(paimonTable, icebergPathFactory);
-        IcebergManifestList manifestList =
-                IcebergManifestList.create(paimonTable, icebergPathFactory);
+            // TODO: IcebergManifestFile.create() is suitable here?
+            IcebergManifestFile manifestFile =
+                    IcebergManifestFile.create(paimonTable, icebergPathFactory);
+            IcebergManifestList manifestList =
+                    IcebergManifestList.create(paimonTable, icebergPathFactory);
 
-        // get all IcebergManifestEntrys so that we can get all datafiles
-        List<IcebergManifestFileMeta> icebergManifestFileMetas =
-                manifestList.read(icebergMetadata.currentSnapshot().manifestList());
-        List<IcebergManifestEntry> icebergEntries =
-                icebergManifestFileMetas.stream()
-                        .flatMap(
-                                fileMeta ->
-                                        manifestFile
-                                                .read(new Path(fileMeta.manifestPath()).getName())
-                                                .stream())
-                        .filter(IcebergManifestEntry::isLive)
-                        .collect(Collectors.toList());
+            List<IcebergManifestFileMeta> icebergManifestFileMetas =
+                    manifestList.read(icebergMetadata.currentSnapshot().manifestList());
 
-        // construct paimon DataFileMeta from iceberg DataFileMeta
-        List<IcebergDataFileMeta> icebergDataFileMetas =
-                icebergEntries.stream()
-                        .map(IcebergManifestEntry::file)
-                        .collect(Collectors.toList());
+            // check manifest file with 'DELETE' kind
+            icebergManifestFileMetas = checkAndFilterManifestFiles(icebergManifestFileMetas);
 
-        BinaryRow partitionRow = BinaryRow.EMPTY_ROW;
-        Path newDir = paimonTable.store().pathFactory().bucketPath(partitionRow, 0);
-        // TODO:需要获取到iceberg目前有几个分区，光有分区字段不够，需要真实的分区，如2000/12，2001/12这种
-        if (!paimonTable.partitionKeys().isEmpty()) {
-            newDir = paimonTable.store().pathFactory().bucketPath(BinaryRow.EMPTY_ROW, 0);
-        }
-        Map<Path, Path> rollBack = new ConcurrentHashMap<>();
-        List<DataFileMeta> dataFileMetas =
-                FileMetaUtils.construct(
-                        icebergDataFileMetas, fileIO, paimonTable, newDir, rollBack);
-        CommitMessage commitMessage = FileMetaUtils.commitFile(partitionRow, dataFileMetas);
-        try (BatchTableCommit commit = paimonTable.newBatchWriteBuilder().newCommit()) {
-            commit.commit(Collections.singletonList(commitMessage));
+            // get all live iceberg entries
+            List<IcebergManifestEntry> icebergEntries =
+                    icebergManifestFileMetas.stream()
+                            .flatMap(fileMeta -> manifestFile.read(fileMeta).stream())
+                            .filter(IcebergManifestEntry::isLive)
+                            .collect(Collectors.toList());
+            if (icebergEntries.isEmpty()) {
+                LOG.info(
+                        "No live manifest entry in iceberg table for snapshot {}, iceberg table meta path is {}.",
+                        icebergNewestSnapshotId,
+                        icebergPathFactory.toMetadataPath(icebergNewestSnapshotId));
+                return;
+            }
+
+            List<IcebergDataFileMeta> icebergDataFileMetas =
+                    icebergEntries.stream()
+                            .map(IcebergManifestEntry::file)
+                            .collect(Collectors.toList());
+
+            // Again, check if the file is a Delete File
+            icebergDataFileMetas = checkAndFilterDataFiles(icebergDataFileMetas);
+
+            LOG.info(
+                    "Begin to create Migrate Task, the number of iceberg data files is {}",
+                    icebergDataFileMetas.size());
+
+            List<MigrateTask> tasks = new ArrayList<>();
+            Map<Path, Path> rollback = new ConcurrentHashMap<>();
+            if (paimonTable.partitionKeys().isEmpty()) {
+                tasks.add(importUnPartitionedTable(icebergDataFileMetas, paimonTable, rollback));
+            } else {
+                tasks.addAll(importPartitionedTable(icebergDataFileMetas, paimonTable, rollback));
+            }
+
+            List<Future<CommitMessage>> futures =
+                    tasks.stream().map(executor::submit).collect(Collectors.toList());
+            List<CommitMessage> commitMessages = new ArrayList<>();
+            try {
+                for (Future<CommitMessage> future : futures) {
+                    commitMessages.add(future.get());
+                }
+            } catch (Exception e) {
+                futures.forEach(f -> f.cancel(true));
+                for (Future<?> future : futures) {
+                    // wait all task cancelled or finished
+                    while (!future.isDone()) {
+                        //noinspection BusyWait
+                        Thread.sleep(100);
+                    }
+                }
+                // roll back all renamed path
+                for (Map.Entry<Path, Path> entry : rollback.entrySet()) {
+                    Path newPath = entry.getKey();
+                    Path origin = entry.getValue();
+                    if (paimonFileIO.exists(newPath)) {
+                        paimonFileIO.rename(newPath, origin);
+                    }
+                }
+
+                throw new RuntimeException("Migrating failed because exception happens", e);
+            }
+            try (BatchTableCommit commit = paimonTable.newBatchWriteBuilder().newCommit()) {
+                commit.commit(new ArrayList<>(commitMessages));
+            }
+        } catch (Exception e) {
+            paimonCatalog.dropTable(paimonIdentifier, true);
+            throw new RuntimeException("Migrating failed", e);
         }
     }
 
@@ -120,7 +201,7 @@ public class IcebergMigrator implements Migrator {
         Path versionHintPath =
                 new Path(icebergPathFactory.metadataDirectory(), VERSION_HINT_FILENAME);
         try {
-            return Integer.parseInt(fileIO.readFileUtf8(versionHintPath));
+            return Integer.parseInt(paimonFileIO.readFileUtf8(versionHintPath));
         } catch (IOException e) {
             throw new RuntimeException(
                     "read iceberg version-hint.text failed. Iceberg metadata path: "
@@ -152,5 +233,110 @@ public class IcebergMigrator implements Migrator {
                 dataFields, partitionKeys, Collections.emptyList(), Collections.emptyMap(), null);
     }
 
-    public void createPaimonTable() {}
+    private List<IcebergManifestFileMeta> checkAndFilterManifestFiles(
+            List<IcebergManifestFileMeta> icebergManifestFileMetas) {
+        if (!ignoreDelete) {
+            for (IcebergManifestFileMeta meta : icebergManifestFileMetas) {
+                if (meta.content() == IcebergManifestFileMeta.Content.DELETES) {
+                    throw new RuntimeException(
+                            "IcebergMigrator don't support analyzing manifest file with 'DELETE' content. "
+                                    + "You can set 'ignore-delete' to ignore manifest file with 'DELETE' content.");
+                }
+            }
+            return icebergManifestFileMetas;
+        } else {
+            return icebergManifestFileMetas.stream()
+                    .filter(meta -> meta.content() != IcebergManifestFileMeta.Content.DELETES)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private List<IcebergDataFileMeta> checkAndFilterDataFiles(
+            List<IcebergDataFileMeta> icebergDataFileMetas) {
+        if (!ignoreDelete) {
+            for (IcebergDataFileMeta meta : icebergDataFileMetas) {
+                if (meta.content() != IcebergDataFileMeta.Content.DATA) {
+                    throw new RuntimeException(
+                            "IcebergMigrator don't support analyzing iceberg delete file. "
+                                    + "You can set 'ignore-delete' to ignore iceberg delete files.");
+                }
+            }
+            return icebergDataFileMetas;
+        } else {
+            return icebergDataFileMetas.stream()
+                    .filter(meta -> meta.content() == IcebergDataFileMeta.Content.DATA)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private MigrateTask importUnPartitionedTable(
+            List<IcebergDataFileMeta> icebergDataFileMetas,
+            FileStoreTable paimonTable,
+            Map<Path, Path> rollback) {
+        BinaryRow partitionRow = BinaryRow.EMPTY_ROW;
+        Path newDir = paimonTable.store().pathFactory().bucketPath(partitionRow, 0);
+
+        return new MigrateTask(
+                icebergDataFileMetas, paimonFileIO, paimonTable, partitionRow, newDir, rollback);
+    }
+
+    private List<MigrateTask> importPartitionedTable(
+            List<IcebergDataFileMeta> icebergDataFileMetas,
+            FileStoreTable paimonTable,
+            Map<Path, Path> rollback) {
+        Map<BinaryRow, List<IcebergDataFileMeta>> dataInPartition =
+                icebergDataFileMetas.stream()
+                        .collect(Collectors.groupingBy(IcebergDataFileMeta::partition));
+        List<MigrateTask> migrateTasks = new ArrayList<>();
+        for (Map.Entry<BinaryRow, List<IcebergDataFileMeta>> entry : dataInPartition.entrySet()) {
+            BinaryRow partitionRow = entry.getKey();
+            Path newDir = paimonTable.store().pathFactory().bucketPath(partitionRow, 0);
+            migrateTasks.add(
+                    new MigrateTask(
+                            entry.getValue(),
+                            paimonFileIO,
+                            paimonTable,
+                            partitionRow,
+                            newDir,
+                            rollback));
+        }
+        return migrateTasks;
+    }
+
+    /** One import task for one partition. */
+    public static class MigrateTask implements Callable<CommitMessage> {
+
+        private final List<IcebergDataFileMeta> icebergDataFileMetas;
+        private final FileIO fileIO;
+        private final FileStoreTable paimonTable;
+        private final BinaryRow partitionRow;
+        private final Path newDir;
+        private final Map<Path, Path> rollback;
+
+        public MigrateTask(
+                List<IcebergDataFileMeta> icebergDataFileMetas,
+                FileIO fileIO,
+                FileStoreTable paimonTable,
+                BinaryRow partitionRow,
+                Path newDir,
+                Map<Path, Path> rollback) {
+            this.icebergDataFileMetas = icebergDataFileMetas;
+            this.fileIO = fileIO;
+            this.paimonTable = paimonTable;
+            this.partitionRow = partitionRow;
+            this.newDir = newDir;
+            this.rollback = rollback;
+        }
+
+        @Override
+        public CommitMessage call() throws Exception {
+            if (!fileIO.exists(newDir)) {
+                fileIO.mkdirs(newDir);
+            }
+            List<DataFileMeta> fileMetas =
+                    FileMetaUtils.construct(
+                            icebergDataFileMetas, fileIO, paimonTable, newDir, rollback);
+            return FileMetaUtils.commitFile(partitionRow, fileMetas);
+        }
+    }
 }
