8c35b11043c63d15ac9abaaa5a5d79a611dc8e7b
Jingsong <jingsonglee0@gmail.com>
2024-12-09 19:59:38 +0800

    [hive] Make HiveMetastoreClient.addPartition thread safe

--- paimon-hive/paimon-hive-catalog/src/main/java/org/apache/paimon/hive/HiveMetastoreClient.java
+++ paimon-hive/paimon-hive-catalog/src/main/java/org/apache/paimon/hive/HiveMetastoreClient.java
@@ -31,6 +31,7 @@ import org.apache.paimon.utils.PartitionPathUtils;
 
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.IMetaStoreClient;
+import org.apache.hadoop.hive.metastore.api.AlreadyExistsException;
 import org.apache.hadoop.hive.metastore.api.NoSuchObjectException;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.PartitionEventType;
@@ -92,21 +93,15 @@ public class HiveMetastoreClient implements MetastoreClient {
 
     @Override
     public void addPartition(LinkedHashMap<String, String> partitionSpec) throws Exception {
-        List<String> partitionValues = new ArrayList<>(partitionSpec.values());
-        try {
-            clients.execute(
-                    client ->
-                            client.getPartition(
-                                    identifier.getDatabaseName(),
-                                    identifier.getTableName(),
-                                    partitionValues));
-            // do nothing if the partition already exists
-        } catch (NoSuchObjectException e) {
-            // partition not found, create new partition
-            Partition hivePartition =
-                    toHivePartition(partitionSpec, (int) (System.currentTimeMillis() / 1000));
-            clients.execute(client -> client.add_partition(hivePartition));
-        }
+        Partition hivePartition =
+                toHivePartition(partitionSpec, (int) (System.currentTimeMillis() / 1000));
+        clients.execute(
+                client -> {
+                    try {
+                        client.add_partition(hivePartition);
+                    } catch (AlreadyExistsException ignore) {
+                    }
+                });
     }
 
     @Override
