009b8f5572692e0f22f54ef02f120196ad8f87a4
李鹏程 <li.pc@topsports.com.cn>
2024-12-13 20:20:11 +0800

    kafka_sync_database supports different prefix and suffix for different database

--- paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/TableNameConverterTest.java
+++ paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/TableNameConverterTest.java
@@ -33,13 +33,47 @@ public class TableNameConverterTest {
         tableMapping.put("mapped_src", "mapped_TGT");
         TableNameConverter caseConverter =
                 new TableNameConverter(true, true, "pre_", "_pos", tableMapping);
-        Assert.assertEquals(caseConverter.convert("mapped_SRC"), "mapped_TGT");
+        Assert.assertEquals(caseConverter.convert("", "mapped_SRC"), "mapped_TGT");
 
-        Assert.assertEquals(caseConverter.convert("unmapped_src"), "pre_unmapped_src_pos");
+        Assert.assertEquals(caseConverter.convert("", "unmapped_src"), "pre_unmapped_src_pos");
 
         TableNameConverter noCaseConverter =
                 new TableNameConverter(false, true, "pre_", "_pos", tableMapping);
-        Assert.assertEquals(noCaseConverter.convert("mapped_src"), "mapped_tgt");
-        Assert.assertEquals(noCaseConverter.convert("unmapped_src"), "pre_unmapped_src_pos");
+        Assert.assertEquals(noCaseConverter.convert("", "mapped_src"), "mapped_tgt");
+        Assert.assertEquals(noCaseConverter.convert("", "unmapped_src"), "pre_unmapped_src_pos");
+    }
+
+    @Test
+    public void testConvertTableNameByDBPrefix_Suffix() {
+        Map<String, String> dbPrefix = new HashMap<>(2);
+        dbPrefix.put("db_with_prefix", "db_pref_");
+        dbPrefix.put("db_with_prefix_suffix", "db_pref_");
+
+        Map<String, String> dbSuffix = new HashMap<>(2);
+        dbSuffix.put("db_with_suffix", "_db_suff");
+        dbSuffix.put("db_with_prefix_suffix", "_db_suff");
+
+        TableNameConverter tblNameConverter =
+                new TableNameConverter(false, true, dbPrefix, dbSuffix, "pre_", "_suf", null);
+
+        // Tables in the specified db should have the specified prefix and suffix.
+
+        // db prefix + normal suffix
+        Assert.assertEquals(
+                "db_pref_table_name_suf", tblNameConverter.convert("db_with_prefix", "table_name"));
+
+        // normal prefix + db suffix
+        Assert.assertEquals(
+                "pre_table_name_db_suff", tblNameConverter.convert("db_with_suffix", "table_name"));
+
+        // db prefix + db suffix
+        Assert.assertEquals(
+                "db_pref_table_name_db_suff",
+                tblNameConverter.convert("db_with_prefix_suffix", "table_name"));
+
+        // only normal prefix and suffix
+        Assert.assertEquals(
+                "pre_table_name_suf",
+                tblNameConverter.convert("db_without_prefix_suffix", "table_name"));
     }
 }
