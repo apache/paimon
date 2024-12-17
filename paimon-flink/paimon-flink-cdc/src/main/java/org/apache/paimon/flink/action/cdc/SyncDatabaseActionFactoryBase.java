85f1deb71c2a315e1af62fe09d9971827a2694c2
李鹏程 <li.pc@topsports.com.cn>
2024-12-17 18:45:32 +0800

    kafka_sync_database supports db whitelist and blacklist

--- paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/SyncDatabaseActionFactoryBase.java
+++ paimon-flink/paimon-flink-cdc/src/main/java/org/apache/paimon/flink/action/cdc/SyncDatabaseActionFactoryBase.java
@@ -24,7 +24,9 @@ import org.apache.paimon.flink.action.MultipleParameterToolAdapter;
 
 import java.util.Optional;
 
+import static org.apache.paimon.flink.action.cdc.CdcActionCommonUtils.EXCLUDING_DBS;
 import static org.apache.paimon.flink.action.cdc.CdcActionCommonUtils.EXCLUDING_TABLES;
+import static org.apache.paimon.flink.action.cdc.CdcActionCommonUtils.INCLUDING_DBS;
 import static org.apache.paimon.flink.action.cdc.CdcActionCommonUtils.INCLUDING_TABLES;
 import static org.apache.paimon.flink.action.cdc.CdcActionCommonUtils.MULTIPLE_TABLE_PARTITION_KEYS;
 import static org.apache.paimon.flink.action.cdc.CdcActionCommonUtils.PARTITION_KEYS;
@@ -59,6 +61,8 @@ public abstract class SyncDatabaseActionFactoryBase<T extends SyncDatabaseAction
                 .withTableMapping(optionalConfigMap(params, TABLE_MAPPING))
                 .includingTables(params.get(INCLUDING_TABLES))
                 .excludingTables(params.get(EXCLUDING_TABLES))
+                .includingDbs(params.get(INCLUDING_DBS))
+                .excludingDbs(params.get(EXCLUDING_DBS))
                 .withPartitionKeyMultiple(
                         optionalConfigMapList(params, MULTIPLE_TABLE_PARTITION_KEYS))
                 .withPartitionKeys();
---
+++ paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/SyncDatabaseActionBaseTest.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.flink.action.cdc;
+
+import org.apache.paimon.flink.action.cdc.kafka.KafkaSyncDatabaseAction;
+import org.apache.paimon.flink.sink.cdc.CdcRecord;
+import org.apache.paimon.flink.sink.cdc.RichCdcMultiplexRecord;
+import org.apache.paimon.flink.sink.cdc.RichCdcMultiplexRecordEventParser;
+import org.apache.paimon.fs.Path;
+import org.apache.paimon.fs.local.LocalFileIO;
+import org.apache.paimon.types.RowKind;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/** Tests for {@link SyncDatabaseActionBase}. */
+public class SyncDatabaseActionBaseTest {
+    private static final String ANY_DB = "any_db";
+    private static final String WHITE_DB = "white_db";
+    private static final String BLACK_DB = "black_db";
+    private static final String WHITE_TBL = "white_tbl";
+    private static final String BLACK_TBL = "black_tbl";
+
+    private SyncDatabaseActionBase kafkaSyncDbAction;
+    private RichCdcMultiplexRecord whiteAnyDbCdcRecord;
+    private RichCdcMultiplexRecord blackAnyDbCdcRecord;
+    private RichCdcMultiplexRecord whiteCdcRecord;
+    private RichCdcMultiplexRecord blackCdcRecord;
+    private RichCdcMultiplexRecord whiteDbBlackTblCdcRecord;
+    private RichCdcMultiplexRecord blackDbWhiteTblCdcRecord;
+
+    @TempDir private java.nio.file.Path tmp;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        LocalFileIO localFileIO = new LocalFileIO();
+        Path defaultDb = new Path(tmp.toString(), "default.db");
+        localFileIO.mkdirs(defaultDb);
+
+        kafkaSyncDbAction =
+                new KafkaSyncDatabaseAction(
+                        tmp.toString(), "default", new HashMap<>(), new HashMap<>());
+
+        Map<String, String> rawData = new HashMap<>();
+        rawData.put("field", "value");
+
+        CdcRecord cdcData = new CdcRecord(RowKind.INSERT, rawData);
+        whiteAnyDbCdcRecord =
+                new RichCdcMultiplexRecord(
+                        ANY_DB, WHITE_TBL, Arrays.asList(), Arrays.asList(), cdcData);
+        blackAnyDbCdcRecord =
+                new RichCdcMultiplexRecord(
+                        ANY_DB, BLACK_TBL, Arrays.asList(), Arrays.asList(), cdcData);
+        whiteCdcRecord =
+                new RichCdcMultiplexRecord(
+                        WHITE_DB, WHITE_TBL, Arrays.asList(), Arrays.asList(), cdcData);
+        blackCdcRecord =
+                new RichCdcMultiplexRecord(
+                        BLACK_DB, WHITE_TBL, Arrays.asList(), Arrays.asList(), cdcData);
+
+        whiteDbBlackTblCdcRecord =
+                new RichCdcMultiplexRecord(
+                        WHITE_DB, BLACK_TBL, Arrays.asList(), Arrays.asList(), cdcData);
+        blackDbWhiteTblCdcRecord =
+                new RichCdcMultiplexRecord(
+                        BLACK_DB, WHITE_TBL, Arrays.asList(), Arrays.asList(), cdcData);
+    }
+
+    @Test
+    public void testSyncTablesWithoutDbLists() throws NoSuchMethodException, IOException {
+
+        kafkaSyncDbAction.includingTables(WHITE_TBL);
+        kafkaSyncDbAction.excludingTables(BLACK_TBL);
+
+        RichCdcMultiplexRecordEventParser parser =
+                (RichCdcMultiplexRecordEventParser)
+                        kafkaSyncDbAction.buildEventParserFactory().create();
+        List<CdcRecord> parsedRecords;
+
+        parser.setRawEvent(whiteAnyDbCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(1, parsedRecords.size());
+
+        parser.setRawEvent(blackAnyDbCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(0, parsedRecords.size());
+    }
+
+    @Test
+    public void testSyncTablesWithDbList() {
+        kafkaSyncDbAction.includingDbs(WHITE_DB);
+        kafkaSyncDbAction.excludingDbs(BLACK_DB);
+        RichCdcMultiplexRecordEventParser parser =
+                (RichCdcMultiplexRecordEventParser)
+                        kafkaSyncDbAction.buildEventParserFactory().create();
+        List<CdcRecord> parsedRecords;
+
+        parser.setRawEvent(whiteAnyDbCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(0, parsedRecords.size());
+
+        parser.setRawEvent(blackAnyDbCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(0, parsedRecords.size());
+
+        // white db and white table
+        parser.setRawEvent(whiteCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(1, parsedRecords.size());
+
+        parser.setRawEvent(blackAnyDbCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(0, parsedRecords.size());
+    }
+
+    @Test
+    public void testSycTablesCrossDB() {
+        kafkaSyncDbAction.includingDbs(WHITE_DB);
+        kafkaSyncDbAction.excludingDbs(BLACK_DB);
+        kafkaSyncDbAction.excludingTables(BLACK_TBL);
+        RichCdcMultiplexRecordEventParser parser =
+                (RichCdcMultiplexRecordEventParser)
+                        kafkaSyncDbAction.buildEventParserFactory().create();
+        List<CdcRecord> parsedRecords;
+        parser.setRawEvent(whiteDbBlackTblCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(0, parsedRecords.size());
+        parser.setRawEvent(blackDbWhiteTblCdcRecord);
+        parsedRecords = parser.parseRecords();
+        Assert.assertEquals(0, parsedRecords.size());
+    }
+}
