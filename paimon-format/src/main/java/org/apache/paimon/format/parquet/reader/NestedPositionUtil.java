381835a7f8635f23edf9e5863626384c783e5f49
yuzelin <zelin.yzl@alibaba-inc.com>
2024-12-20 22:51:08 +0800

    fix

--- paimon-format/src/main/java/org/apache/paimon/format/parquet/reader/NestedPositionUtil.java
+++ paimon-format/src/main/java/org/apache/paimon/format/parquet/reader/NestedPositionUtil.java
@@ -50,13 +50,16 @@ public class NestedPositionUtil {
         int nullValuesCount = 0;
         BooleanArrayList nullRowFlags = new BooleanArrayList(0);
         for (int i = 0; i < fieldDefinitionLevels.length; i++) {
-            if (fieldRepetitionLevels[i] > rowRepetitionLevel) {
-                throw new IllegalStateException(
-                        format(
-                                "In parquet's row type field repetition level should not larger than row's repetition level. "
-                                        + "Row repetition level is %s, row field repetition level is %s.",
-                                rowRepetitionLevel, fieldRepetitionLevels[i]));
-            }
+            // TODO: this is not correct ?
+            //            if (fieldRepetitionLevels[i] > rowRepetitionLevel) {
+            //                throw new IllegalStateException(
+            //                        format(
+            //                                "In parquet's row type field repetition level should
+            // not larger than row's repetition level. "
+            //                                        + "Row repetition level is %s, row field
+            // repetition level is %s.",
+            //                                rowRepetitionLevel, fieldRepetitionLevels[i]));
+            //            }
 
             if (fieldDefinitionLevels[i] >= rowDefinitionLevel) {
                 // current row is defined and not empty
