440fb6c6e2221e7f186191dcad4ffeaa2b9baf93
xuyu <11161569@vivo.com>
2025-01-20 11:15:09 +0800

    move ut

--- paimon-spark/paimon-spark-3.4/src/test/scala/org/apache/paimon/spark/sql/RollbackProcedureTest.scala
+++ paimon-spark/paimon-spark-3.4/src/test/scala/org/apache/paimon/spark/sql/RollbackProcedureTest.scala
@@ -93,44 +93,4 @@ class RollbackProcedureTest extends PaimonSparkTestBase with StreamTest {
       }
     }
   }
-
-  test("Paimon Procedure: rollback to tag check test") {
-    spark.sql(s"""
-                 |CREATE TABLE T (a INT, b STRING)
-                 |TBLPROPERTIES ('primary-key'='a', 'bucket'='3', 'file.format'='orc')
-                 |""".stripMargin)
-
-    val query = () => spark.sql("SELECT * FROM T ORDER BY a")
-
-    // snapshot-1
-    spark.sql("insert into T select 1, 'a'")
-    checkAnswer(query(), Row(1, "a") :: Nil)
-
-    checkAnswer(
-      spark.sql("CALL paimon.sys.create_tag(table => 'test.T', tag => '20250122', snapshot => 1)"),
-      Row(true) :: Nil)
-
-    // snapshot-2
-    spark.sql("insert into T select 2, 'b'")
-    checkAnswer(query(), Row(1, "a") :: Row(2, "b") :: Nil)
-
-    // snapshot-3
-    spark.sql("insert into T select 2, 'b2'")
-    checkAnswer(query(), Row(1, "a") :: Row(2, "b2") :: Nil)
-    assertThrows[RuntimeException] {
-      spark.sql("CALL paimon.sys.rollback(table => 'test.T_exception', version => '2')")
-    }
-    // rollback to snapshot
-    checkAnswer(
-      spark.sql("CALL paimon.sys.rollback(table => 'test.T', version => '2')"),
-      Row(true) :: Nil)
-    checkAnswer(query(), Row(1, "a") :: Row(2, "b") :: Nil)
-
-    // rollback to tag
-    checkAnswer(
-      spark.sql("CALL paimon.sys.rollback(table => 'test.T', version => '20250122')"),
-      Row(true) :: Nil)
-    checkAnswer(query(), Row(1, "a") :: Nil)
-  }
-
 }
