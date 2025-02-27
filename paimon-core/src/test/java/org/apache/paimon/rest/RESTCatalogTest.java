f11221cc445fb2a802b0d3b2e5dddff6f451364e
yantian <lining.jln@alibaba-inc.com>
2025-02-27 14:59:26 +0800

    delete no need code

--- paimon-core/src/test/java/org/apache/paimon/rest/RESTCatalogTest.java
+++ paimon-core/src/test/java/org/apache/paimon/rest/RESTCatalogTest.java
@@ -56,6 +56,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
