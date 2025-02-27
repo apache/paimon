73134594aa3b11c8d9f339ee99452767fde055c0
tsreaper <tsreaper96@gmail.com>
2025-02-27 16:47:51 +0800

    [metrics] Add total file size metrics of active buckets for primary key tables (#5158)
    This closes #5158.

--- paimon-core/src/test/java/org/apache/paimon/operation/metrics/CompactionMetricsTest.java
+++ paimon-core/src/test/java/org/apache/paimon/operation/metrics/CompactionMetricsTest.java
@@ -18,18 +18,44 @@
 
 package org.apache.paimon.operation.metrics;
 
+import org.apache.paimon.CoreOptions;
+import org.apache.paimon.catalog.FileSystemCatalog;
+import org.apache.paimon.catalog.Identifier;
 import org.apache.paimon.data.BinaryRow;
+import org.apache.paimon.data.GenericRow;
+import org.apache.paimon.fs.Path;
+import org.apache.paimon.fs.local.LocalFileIO;
+import org.apache.paimon.io.DataFileMeta;
 import org.apache.paimon.metrics.Counter;
 import org.apache.paimon.metrics.Gauge;
 import org.apache.paimon.metrics.Metric;
 import org.apache.paimon.metrics.TestMetricRegistry;
+import org.apache.paimon.operation.AbstractFileStoreWrite;
+import org.apache.paimon.options.Options;
+import org.apache.paimon.schema.Schema;
+import org.apache.paimon.table.FileStoreTable;
+import org.apache.paimon.table.sink.TableCommitImpl;
+import org.apache.paimon.table.sink.TableWriteImpl;
+import org.apache.paimon.table.source.DataSplit;
+import org.apache.paimon.table.source.Split;
+import org.apache.paimon.types.DataType;
+import org.apache.paimon.types.DataTypes;
+import org.apache.paimon.types.RowType;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /** Tests for {@link CompactionMetrics}. */
 public class CompactionMetricsTest {
+
+    @TempDir java.nio.file.Path tempDir;
 
     @Test
     public void testReportMetrics() {
@@ -80,6 +106,74 @@ public class CompactionMetricsTest {
         reporters[1].decreaseCompactionsQueuedCount();
         assertThat(getMetric(metrics, CompactionMetrics.COMPACTION_COMPLETED_COUNT)).isEqualTo(1L);
         assertThat(getMetric(metrics, CompactionMetrics.COMPACTION_QUEUED_COUNT)).isEqualTo(1L);
+    }
+
+    @Test
+    public void testTotalFileSizeForPrimaryKeyTables() throws Exception {
+        LocalFileIO fileIO = LocalFileIO.create();
+        Path path = new Path(tempDir.toString());
+
+        RowType rowType =
+                RowType.of(
+                        new DataType[] {DataTypes.INT(), DataTypes.INT()}, new String[] {"k", "v"});
+
+        int bucketNum = 2;
+        Options options = new Options();
+        options.set(CoreOptions.BUCKET, bucketNum);
+        Schema schema =
+                new Schema(
+                        rowType.getFields(),
+                        Collections.emptyList(),
+                        Collections.singletonList("k"),
+                        options.toMap(),
+                        "");
+
+        FileStoreTable table;
+        try (FileSystemCatalog paimonCatalog = new FileSystemCatalog(fileIO, path)) {
+            paimonCatalog.createDatabase("mydb", false);
+            Identifier paimonIdentifier = Identifier.create("mydb", "mytable");
+            paimonCatalog.createTable(paimonIdentifier, schema, false);
+            table = (FileStoreTable) paimonCatalog.getTable(paimonIdentifier);
+        }
+
+        String commitUser = UUID.randomUUID().toString();
+        TableWriteImpl<?> write = table.newWrite(commitUser);
+        TableCommitImpl commit = table.newCommit(commitUser);
+        write.withMetricRegistry(new TestMetricRegistry());
+
+        int numKeys = 300;
+        int numRounds = 10;
+        int recordsPerRound = 100;
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int commitIdentifier = 0;
+        for (int i = 0; i < numRounds; i++) {
+            for (int j = 0; j < recordsPerRound; j++) {
+                write.write(GenericRow.of(random.nextInt(numKeys), random.nextInt()));
+            }
+
+            commitIdentifier++;
+            commit.commit(
+                    commitIdentifier, write.prepareCommit(random.nextBoolean(), commitIdentifier));
+
+            long[] totalFileSizes = new long[bucketNum];
+            for (Split split : table.newScan().plan().splits()) {
+                DataSplit dataSplit = (DataSplit) split;
+                totalFileSizes[dataSplit.bucket()] +=
+                        dataSplit.dataFiles().stream().mapToLong(DataFileMeta::fileSize).sum();
+            }
+
+            CompactionMetrics metrics =
+                    ((AbstractFileStoreWrite<?>) write.getWrite()).compactionMetrics();
+            assertThat(metrics.getTotalFileSizeStream()).hasSize(bucketNum);
+            assertThat(getMetric(metrics, CompactionMetrics.MAX_TOTAL_FILE_SIZE))
+                    .isEqualTo(Arrays.stream(totalFileSizes).max().orElse(0));
+            assertThat(getMetric(metrics, CompactionMetrics.AVG_TOTAL_FILE_SIZE))
+                    .isEqualTo(Arrays.stream(totalFileSizes).average().orElse(0));
+        }
+
+        write.close();
+        commit.close();
     }
 
     private Object getMetric(CompactionMetrics metrics, String metricName) {
