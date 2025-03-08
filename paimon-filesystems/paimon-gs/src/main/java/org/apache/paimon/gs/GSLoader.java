ff18dd03094e1dac48dbe8310018363b3a31f4f3
gkatzioura <gkatzioura@gmail.com>
2025-03-08 23:23:31 +0000

    5235: Support for Google Cloud Storage

---
+++ paimon-filesystems/paimon-gs/src/main/java/org/apache/paimon/gs/GSLoader.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.gs;
+
+import org.apache.paimon.catalog.CatalogContext;
+import org.apache.paimon.fs.FileIO;
+import org.apache.paimon.fs.FileIOLoader;
+import org.apache.paimon.fs.Path;
+import org.apache.paimon.fs.PluginFileIO;
+import org.apache.paimon.plugin.PluginLoader;
+
+/** A {@link PluginLoader} to load oss. */
+public class GSLoader implements FileIOLoader {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String GS_CLASSES_DIR = "paimon-plugin-gs";
+
+    private static final String GCS_CLASS = "org.apache.paimon.gs.GSFileIO";
+
+    private static PluginLoader loader;
+
+    private static synchronized PluginLoader getLoader() {
+        if (loader == null) {
+            // Avoid NoClassDefFoundError without cause by exception
+            loader = new PluginLoader(GS_CLASSES_DIR);
+        }
+        return loader;
+    }
+
+    @Override
+    public String getScheme() {
+        return "gs";
+    }
+
+    @Override
+    public FileIO load(Path path) {
+        return new GCSPluginFileIO();
+    }
+
+    private static class GCSPluginFileIO extends PluginFileIO {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public boolean isObjectStore() {
+            return true;
+        }
+
+        @Override
+        protected FileIO createFileIO(Path path) {
+            FileIO fileIO = getLoader().newInstance(GCS_CLASS);
+            fileIO.configure(CatalogContext.create(options));
+            return fileIO;
+        }
+
+        @Override
+        protected ClassLoader pluginClassLoader() {
+            return getLoader().submoduleClassLoader();
+        }
+    }
+}
