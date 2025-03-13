f6e3c8efc629b40ec1690e2787acbcaf73f19356
JingsongLi <jingsonglee0@gmail.com>
2025-03-13 15:18:37 +0800

    [core] Add comments to partition methods in Catalog

--- paimon-core/src/main/java/org/apache/paimon/catalog/Catalog.java
+++ paimon-core/src/main/java/org/apache/paimon/catalog/Catalog.java
@@ -267,10 +267,7 @@ public interface Catalog extends AutoCloseable {
     // ======================= partition methods ===============================
 
     /**
-     * Create partitions of the specify table.
-     *
-     * <p>Only catalog with metastore can support this method, and only table with
-     * 'metastore.partitioned-table' can support this method.
+     * Create partitions of the specify table. Ignore existing partitions.
      *
      * @param identifier path of the table to create partitions
      * @param partitions partitions to be created
@@ -280,7 +277,7 @@ public interface Catalog extends AutoCloseable {
             throws TableNotExistException;
 
     /**
-     * Drop partitions of the specify table.
+     * Drop partitions of the specify table. Ignore non-existent partitions.
      *
      * @param identifier path of the table to drop partitions
      * @param partitions partitions to be deleted
@@ -290,7 +287,8 @@ public interface Catalog extends AutoCloseable {
             throws TableNotExistException;
 
     /**
-     * Alter partitions of the specify table.
+     * Alter partitions of the specify table. For non-existent partitions, partitions will be
+     * created directly.
      *
      * <p>Only catalog with metastore can support this method, and only table with
      * 'metastore.partitioned-table' can support this method.
@@ -303,7 +301,8 @@ public interface Catalog extends AutoCloseable {
             throws TableNotExistException;
 
     /**
-     * Mark partitions done of the specify table.
+     * Mark partitions done of the specify table. For non-existent partitions, partitions will be
+     * created directly.
      *
      * <p>Only catalog with metastore can support this method, and only table with
      * 'metastore.partitioned-table' can support this method.
