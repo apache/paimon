add377cc0051a2d6d2143fcb8e5c6d4f8f766ec7
yangjf2019 <54518670+yangjf2019@users.noreply.github.com>
2025-03-27 12:31:17 +0800

    [hotfix] Remove invalid declarations from the code (#5348)

--- paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/kafka/KafkaActionITCaseBase.java
+++ paimon-flink/paimon-flink-cdc/src/test/java/org/apache/paimon/flink/action/cdc/kafka/KafkaActionITCaseBase.java
@@ -25,7 +25,6 @@ import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.core.JsonProcessin
 import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.flink.util.DockerImageVersions;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -176,10 +175,6 @@ public abstract class KafkaActionITCaseBase extends CdcActionITCaseBase {
         return standardProps;
     }
 
-    public String getBootstrapServers() {
-        return KAFKA_CONTAINER.getBootstrapServers();
-    }
-
     protected Map<String, String> getBasicKafkaConfig() {
         Map<String, String> config = new HashMap<>();
         config.put("properties.bootstrap.servers", KAFKA_CONTAINER.getBootstrapServers());
@@ -221,8 +216,6 @@ public abstract class KafkaActionITCaseBase extends CdcActionITCaseBase {
     }
 
     protected void createTestTopic(String topic, int numPartitions, int replicationFactor) {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, getBootstrapServers());
         try {
             adminClient
                     .createTopics(
@@ -250,6 +243,7 @@ public abstract class KafkaActionITCaseBase extends CdcActionITCaseBase {
                 KafkaActionITCaseBase.class
                         .getClassLoader()
                         .getResource(String.format(resourceDirFormat, args));
+        assert url != null;
         Files.readAllLines(Paths.get(url.toURI())).stream()
                 .filter(this::isRecordLine)
                 .forEach(r -> send(topic, r, wait));
