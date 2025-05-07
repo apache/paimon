db8847ddb7997811a3ee24c84f2d91db537873bf
Jack1007 <yanvcl@gmail.com>
2025-05-07 11:42:51 +0800

    fix the timestamp type validation issue-5450

--- paimon-hive/paimon-hive-connector-common/src/main/java/org/apache/paimon/hive/HiveSchema.java
+++ paimon-hive/paimon-hive-connector-common/src/main/java/org/apache/paimon/hive/HiveSchema.java
@@ -255,6 +255,16 @@ public class HiveSchema {
         for (int i = 0; i < hiveFieldNames.size(); i++) {
             if (!hiveFieldNames.get(i).equalsIgnoreCase(schemaFieldNames.get(i))
                     || !Objects.equals(hiveFieldTypeInfos.get(i), schemaFieldTypeInfos.get(i))) {
+                if (schemaFieldTypeInfos
+                        .get(i)
+                        .getTypeName()
+                        .equals("timestamp with local time zone")) {
+                    // Hive timestamp is compatible with paimon timestamp
+                    // with local time zone
+                    LOG.info(
+                            "Hive DDL and paimon schema mismatched, but Hive timestamp is compatible with paimon timestamp with local time zone");
+                    continue;
+                }
                 String ddlField =
                         hiveFieldNames.get(i) + " " + hiveFieldTypeInfos.get(i).getTypeName();
                 String schemaField =
