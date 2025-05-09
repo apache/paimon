20701366d5b5d0e99ff2720f3d55c5bf75eabd88
yantian <lining.jln@alibaba-inc.com>
2025-05-09 17:53:59 +0800

    delete no need code

--- paimon-core/src/main/java/org/apache/paimon/function/FunctionImpl.java
+++ paimon-core/src/main/java/org/apache/paimon/function/FunctionImpl.java
@@ -20,63 +20,39 @@ package org.apache.paimon.function;
 
 import org.apache.paimon.types.DataField;
 
-import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonCreator;
-import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonGetter;
-import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.apache.paimon.shade.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
 import java.util.Map;
 
 /** Function implementation. */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class FunctionImpl implements Function {
 
-    private static final String FIELD_UUID = "uuid";
-    private static final String FIELD_NAME = "name";
-    private static final String FIELD_INPUT_PARAMETERS = "inputParams";
-    private static final String FIELD_RETURN_PARAMETERS = "returnParams";
-    private static final String FIELD_DEFINITIONS = "definitions";
-    private static final String FIELD_DETERMINISTIC = "deterministic";
-    private static final String FIELD_COMMENT = "comment";
-    private static final String FIELD_OPTIONS = "options";
-
-    @JsonProperty(FIELD_UUID)
     private final String uuid;
 
-    @JsonProperty(FIELD_NAME)
     private final String functionName;
 
-    @JsonProperty(FIELD_INPUT_PARAMETERS)
     private final List<DataField> inputParams;
 
-    @JsonProperty(FIELD_RETURN_PARAMETERS)
     private final List<DataField> returnParams;
 
-    @JsonProperty(FIELD_DETERMINISTIC)
     private final boolean deterministic;
 
-    @JsonProperty(FIELD_DEFINITIONS)
     private final Map<String, FunctionDefinition> definitions;
 
-    @JsonProperty(FIELD_COMMENT)
     private final String comment;
 
-    @JsonProperty(FIELD_OPTIONS)
     private final Map<String, String> options;
 
-    @JsonCreator
     public FunctionImpl(
-            @JsonProperty(FIELD_UUID) String uuid,
-            @JsonProperty(FIELD_NAME) String functionName,
-            @JsonProperty(FIELD_INPUT_PARAMETERS) List<DataField> inputParams,
-            @JsonProperty(FIELD_RETURN_PARAMETERS) List<DataField> returnParams,
-            @JsonProperty(FIELD_DETERMINISTIC) boolean deterministic,
-            @JsonProperty(FIELD_DEFINITIONS) Map<String, FunctionDefinition> definitions,
-            @JsonProperty(FIELD_COMMENT) String comment,
-            @JsonProperty(FIELD_OPTIONS) Map<String, String> options) {
-        this.functionName = functionName;
+            String uuid,
+            String functionName,
+            List<DataField> inputParams,
+            List<DataField> returnParams,
+            boolean deterministic,
+            Map<String, FunctionDefinition> definitions,
+            String comment,
+            Map<String, String> options) {
         this.uuid = uuid;
+        this.functionName = functionName;
         this.inputParams = inputParams;
         this.returnParams = returnParams;
         this.deterministic = deterministic;
@@ -86,52 +62,46 @@ public class FunctionImpl implements Function {
     }
 
     @Override
-    @JsonGetter(FIELD_UUID)
     public String uuid() {
         return this.uuid;
     }
 
     @Override
-    @JsonGetter(FIELD_NAME)
     public String name() {
         return this.functionName;
     }
 
     @Override
-    @JsonGetter(FIELD_INPUT_PARAMETERS)
     public List<DataField> inputParams() {
         return inputParams;
     }
 
     @Override
-    @JsonGetter(FIELD_RETURN_PARAMETERS)
     public List<DataField> returnParams() {
         return returnParams;
     }
 
     @Override
-    @JsonGetter(FIELD_DETERMINISTIC)
     public boolean isDeterministic() {
         return deterministic;
     }
 
-    @JsonGetter(FIELD_DEFINITIONS)
+    @Override
     public Map<String, FunctionDefinition> definitions() {
         return definitions;
     }
 
+    @Override
     public FunctionDefinition definition(String dialect) {
         return definitions.get(dialect);
     }
 
     @Override
-    @JsonGetter(FIELD_COMMENT)
     public String comment() {
         return comment;
     }
 
     @Override
-    @JsonGetter(FIELD_OPTIONS)
     public Map<String, String> options() {
         return options;
     }
