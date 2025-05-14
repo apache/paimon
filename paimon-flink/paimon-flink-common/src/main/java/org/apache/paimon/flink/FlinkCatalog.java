275a4e61199d793f9707b85a47f430d168e644d3
yantian <lining.jln@alibaba-inc.com>
2025-05-14 13:53:02 +0800

    add engine support function

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/FlinkCatalog.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/FlinkCatalog.java
@@ -29,6 +29,7 @@ import org.apache.paimon.catalog.PropertyChange;
 import org.apache.paimon.flink.procedure.ProcedureUtil;
 import org.apache.paimon.flink.utils.FlinkCatalogPropertiesUtil;
 import org.apache.paimon.flink.utils.FlinkDescriptorProperties;
+import org.apache.paimon.function.FunctionDefinition;
 import org.apache.paimon.manifest.PartitionEntry;
 import org.apache.paimon.options.Options;
 import org.apache.paimon.schema.Schema;
@@ -54,6 +55,7 @@ import org.apache.flink.table.catalog.CatalogBaseTable;
 import org.apache.flink.table.catalog.CatalogDatabase;
 import org.apache.flink.table.catalog.CatalogDatabaseImpl;
 import org.apache.flink.table.catalog.CatalogFunction;
+import org.apache.flink.table.catalog.CatalogFunctionImpl;
 import org.apache.flink.table.catalog.CatalogMaterializedTable;
 import org.apache.flink.table.catalog.CatalogPartition;
 import org.apache.flink.table.catalog.CatalogPartitionImpl;
@@ -61,6 +63,7 @@ import org.apache.flink.table.catalog.CatalogPartitionSpec;
 import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.catalog.CatalogView;
 import org.apache.flink.table.catalog.Column;
+import org.apache.flink.table.catalog.FunctionLanguage;
 import org.apache.flink.table.catalog.IntervalFreshness;
 import org.apache.flink.table.catalog.ObjectPath;
 import org.apache.flink.table.catalog.ResolvedCatalogBaseTable;
@@ -101,6 +104,8 @@ import org.apache.flink.table.catalog.stats.CatalogTableStatistics;
 import org.apache.flink.table.expressions.Expression;
 import org.apache.flink.table.factories.Factory;
 import org.apache.flink.table.procedures.Procedure;
+import org.apache.flink.table.resource.ResourceType;
+import org.apache.flink.table.resource.ResourceUri;
 import org.apache.flink.table.types.logical.RowType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -168,6 +173,7 @@ import static org.apache.paimon.utils.Preconditions.checkNotNull;
 public class FlinkCatalog extends AbstractCatalog {
 
     public static final String DIALECT = "flink";
+    public static final String FUNCTION_DEFINITION_NAME = "flink";
 
     private static final Logger LOG = LoggerFactory.getLogger(FlinkCatalog.class);
 
@@ -1460,18 +1466,42 @@ public class FlinkCatalog extends AbstractCatalog {
 
     @Override
     public final List<String> listFunctions(String dbName) throws CatalogException {
-        return Collections.emptyList();
+        return catalog.listFunctions();
     }
 
     @Override
     public final CatalogFunction getFunction(ObjectPath functionPath)
             throws FunctionNotExistException, CatalogException {
-        throw new FunctionNotExistException(getName(), functionPath);
+        try {
+            org.apache.paimon.function.Function function =
+                    catalog.getFunction(functionPath.getObjectName());
+            FunctionDefinition functionDefinition = function.definition(FUNCTION_DEFINITION_NAME);
+            if (functionDefinition instanceof FunctionDefinition.FileFunctionDefinition) {
+                FunctionDefinition.FileFunctionDefinition fileFunctionDefinition =
+                        (FunctionDefinition.FileFunctionDefinition) functionDefinition;
+                List<ResourceUri> resourceUris =
+                        fileFunctionDefinition.storagePaths().stream()
+                                .map(
+                                        path ->
+                                                new ResourceUri(
+                                                        ResourceType.valueOf(
+                                                                fileFunctionDefinition.fileType()),
+                                                        path))
+                                .collect(Collectors.toList());
+                return new CatalogFunctionImpl(
+                        fileFunctionDefinition.className(),
+                        FunctionLanguage.valueOf(fileFunctionDefinition.language()),
+                        resourceUris);
+            }
+            throw new FunctionNotExistException(getName(), functionPath);
+        } catch (Catalog.FunctionNotExistException e) {
+            throw new FunctionNotExistException(getName(), functionPath);
+        }
     }
 
     @Override
     public final boolean functionExists(ObjectPath functionPath) throws CatalogException {
-        return false;
+        return catalog.listFunctions().contains(functionPath.getObjectName());
     }
 
     @Override
@@ -1492,8 +1522,12 @@ public class FlinkCatalog extends AbstractCatalog {
 
     @Override
     public final void dropFunction(ObjectPath functionPath, boolean ignoreIfNotExists)
-            throws CatalogException {
-        throw new UnsupportedOperationException();
+            throws FunctionNotExistException, CatalogException {
+        try {
+            catalog.dropFunction(functionPath.getObjectName(), ignoreIfNotExists);
+        } catch (Catalog.FunctionNotExistException e) {
+            throw new FunctionNotExistException(getName(), functionPath);
+        }
     }
 
     @Override
