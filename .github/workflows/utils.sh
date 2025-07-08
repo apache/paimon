f87ea8de440e8ccec552e66cfcb991a5cc2192ee
仟弋 <yejunhao.yjh@alibaba-inc.com>
2025-07-08 16:45:43 +0800

    Fix test

--- .github/workflows/utils.sh
+++ .github/workflows/utils.sh
@@ -18,6 +18,7 @@
 ################################################################################
 
 set -e
+sudo apt install -y dpkg
 wget https://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 sudo dkpg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 
