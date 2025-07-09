3a980e0ab806491091ec314f2285185c02614df5
LsomeYeah <lleiyeah.cat@outlook.com>
2025-07-09 16:27:05 +0800

    fix dependency for orc-format and test it

--- paimon-iceberg/src/test/java/org/apache/paimon/iceberg/IcebergRestMetadataCommitterTest.java
+++ paimon-iceberg/src/test/java/org/apache/paimon/iceberg/IcebergRestMetadataCommitterTest.java
@@ -636,6 +636,7 @@ public class IcebergRestMetadataCommitterTest {
         ThreadLocalRandom random = ThreadLocalRandom.current();
         int i = random.nextInt(3);
         String[] formats = new String[] {"orc", "parquet", "avro"};
-        return formats[i];
+        //        return formats[i];
+        return "orc";
     }
 }
