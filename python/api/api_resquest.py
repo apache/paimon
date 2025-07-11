14453e807d87369bbb709b8d9dbb6975d6c2cb00
yantian <lining.jln@alibaba-inc.com>
2025-07-11 14:02:26 +0800

    support python

--- python/api/api_resquest.py
+++ python/api/api_resquest.py
@@ -15,6 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 from abc import ABC
 from dataclasses import dataclass
 from typing import Dict, List, Any, Optional
