1d5b2a1dba3c6962bcc0046ed9b70397bfec67ad
yantian <lining.jln@alibaba-inc.com>
2025-07-22 16:25:09 +0800

    update test

--- paimon-python/pypaimon/tests/pvfs_test.py
+++ paimon-python/pypaimon/tests/pvfs_test.py
@@ -34,12 +34,58 @@ class PVFSTestCase(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp(prefix="unittest_")
         self.temp_path = Path(self.temp_dir)
+        # Create config
+        config = ConfigResponse(defaults={"prefix": "mock-test"})
+
+        # Create mock auth provider
+        class MockAuthProvider:
+            def merge_auth_header(self, headers, auth_param):
+                return {AUTHORIZATION_HEADER_KEY: "Bearer test-token"}
+
+        # Create server
+        self.catalog = 'test_catalog'
+        self.data_path = self.temp_dir
+        self.catalog = 'test_warehouse'
+        self.server = RESTCatalogServer(
+            data_path=self.data_path,
+            auth_provider=MockAuthProvider(),
+            config=config,
+            warehouse=self.catalog)
+        self.server.start()
+        print(f"Server started at: {self.server.get_url()}")
         print(f"create: {self.temp_path}")
+        options = {
+            'uri': f"http://localhost:{self.server.port}",
+            'warehouse': 'test_warehouse',
+            'dlf.region': 'cn-hangzhou',
+            "token.provider": "xxxx",
+            'dlf.access-key-id': 'xxxx',
+            'dlf.access-key-secret': 'xxxx'
+        }
+        self.pvfs = PaimonVirtualFileSystem(options)
+        self.database = 'test_database'
+        self.table = 'test_table'
+        self.test_databases = {
+            self.database: self.server.mock_database(self.database, {"k1": "v1", "k2": "v2"}),
+        }
+        data_fields = [
+            DataField(0, "id", AtomicType('INT'), 'id'),
+            DataField(1, "name", AtomicType('STRING'), 'name')
+        ]
+        schema = TableSchema(len(data_fields), data_fields, len(data_fields), [], [], {}, "")
+        self.test_tables = {
+            f"{self.database}.{self.table}": TableMetadata(uuid=str(uuid.uuid4()), is_external=True, schema=schema),
+        }
+        self.server.database_store.update(self.test_databases)
+        self.server.table_metadata_store.update(self.test_tables)
 
     def tearDown(self):
         if self.temp_path.exists():
             shutil.rmtree(self.temp_path)
             print(f"clean: {self.temp_path}")
+        if self.server is not None:
+            self.server.shutdown()
+            print("Server stopped")
 
     @staticmethod
     def _create_parquet_file(path: str):
@@ -58,81 +104,33 @@ class PVFSTestCase(unittest.TestCase):
         # print(df_read.head())
 
     def test(self):
-        # Create config
-        config = ConfigResponse(defaults={"prefix": "mock-test"})
+        nested_dir = self.temp_path / self.database / self.table
+        nested_dir.mkdir(parents=True)
+        data_file_name = 'a.parquet'
+        self._create_parquet_file(f"{nested_dir}/{data_file_name}")
+        database_dirs = self.pvfs.ls(f"pvfs://{self.catalog}", detail=False)
+        expect_database_dirs = set(map(
+            lambda x: self.pvfs._convert_database_virtual_path(self.catalog, x),
+            list(self.test_databases.keys())
+        ))
+        self.assertSetEqual(set(database_dirs), expect_database_dirs)
+        table_dirs = self.pvfs.ls(f"pvfs://{self.catalog}/{self.database}", detail=False)
+        expect_table_dirs = set(map(
+            lambda x: self.pvfs._convert_table_virtual_path(self.catalog, self.database, x),
+            [self.table]
+        ))
+        self.assertSetEqual(set(table_dirs), expect_table_dirs)
+        user_dirs = self.pvfs.ls(f"pvfs://{self.catalog}/{self.database}/{self.table}", detail=False)
+        self.assertSetEqual(set(user_dirs), {f'pvfs://{self.catalog}/{self.database}/{self.table}/{data_file_name}'})
 
-        # Create mock auth provider
-        class MockAuthProvider:
-            def merge_auth_header(self, headers, auth_param):
-                return {AUTHORIZATION_HEADER_KEY: "Bearer test-token"}
+        data_file_name = 'data.txt'
+        data_file_path = self.temp_path / self.database / self.table / 'data.txt'
+        data_file_path.touch()
+        content = 'Hello World'
+        date_file_virtual_path = f'pvfs://{self.catalog}/{self.database}/{self.table}/{data_file_name}'
+        with self.pvfs.open(date_file_virtual_path, 'w') as w:
+            w.write(content)
 
-        # Create server
-        data_path = self.temp_dir
-        catalog = 'test_warehouse'
-        server = RESTCatalogServer(
-            data_path=data_path,
-            auth_provider=MockAuthProvider(),
-            config=config,
-            warehouse=catalog)
-        try:
-            # Start server
-            server.start()
-            print(f"Server started at: {server.get_url()}")
-            test_databases = {
-                "default": server.mock_database("default", {"env": "test"}),
-                "test_db1": server.mock_database("test_db1", {"env": "test"}),
-                "test_db2": server.mock_database("test_db2", {"env": "test"}),
-                "prod_db": server.mock_database("prod_db", {"env": "prod"})
-            }
-            data_fields = [
-                DataField(0, "id", AtomicType('INT'), 'id'),
-                DataField(1, "name", AtomicType('STRING'), 'name')
-            ]
-            schema = TableSchema(len(data_fields), data_fields, len(data_fields), [], [], {}, "")
-            test_tables = {
-                "default.user": TableMetadata(uuid=str(uuid.uuid4()), is_external=True, schema=schema),
-            }
-            nested_dir = self.temp_path / "default" / "user"
-            nested_dir.mkdir(parents=True)
-            data_file_name = 'a.parquet'
-            self._create_parquet_file(f"{nested_dir}/{data_file_name}")
-            server.table_metadata_store.update(test_tables)
-            server.database_store.update(test_databases)
-            options = {
-                'uri': f"http://localhost:{server.port}",
-                'warehouse': 'test_warehouse',
-                'dlf.region': 'cn-hangzhou',
-                "token.provider": "xxxx",
-                'dlf.access-key-id': 'xxxx',
-                'dlf.access-key-secret': 'xxxx'
-            }
-            pvfs = PaimonVirtualFileSystem(options)
-            database_dirs = pvfs.ls("pvfs://test_warehouse", detail=False)
-            expect_database_dirs = set(map(
-                lambda x: pvfs._convert_database_virtual_path(catalog, x),
-                list(test_databases.keys())
-            ))
-            self.assertSetEqual(set(database_dirs), expect_database_dirs)
-            table_dirs = pvfs.ls("pvfs://test_warehouse/default", detail=False)
-            expect_table_dirs = set(map(
-                lambda x: pvfs._convert_table_virtual_path(catalog, 'default', x),
-                ['user']
-            ))
-            self.assertSetEqual(set(table_dirs), expect_table_dirs)
-            user_dirs = pvfs.ls("pvfs://test_warehouse/default/user", detail=False)
-            self.assertSetEqual(set(user_dirs), {f'pvfs://test_warehouse/default/user/{data_file_name}'})
-
-            data_file_name = self.temp_path / "default" / "user" / 'data.txt'
-            data_file_name.touch()
-            content = 'Hello World'
-            with pvfs.open('pvfs://test_warehouse/default/user/data.txt', 'w') as w:
-                w.write(content)
-
-            with pvfs.open('pvfs://test_warehouse/default/user/data.txt', 'r', encoding='utf-8') as file:
-                lines = file.readlines()
-                self.assertListEqual([content], lines)
-
-        finally:
-            # Shutdown server
-            server.shutdown()
-            print("Server stopped")
+        with self.pvfs.open(date_file_virtual_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            self.assertListEqual([content], lines)
