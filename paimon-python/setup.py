1427f0aad257a0af49e9e94d171ce1658c1e1753
discivigour <q46809o@163.com>
2025-07-24 15:52:39 +0800

    fix

--- paimon-python/setup.py
+++ paimon-python/setup.py
@@ -17,7 +17,6 @@
 ##########################################################################
 from setuptools import setup, find_packages
 
-# hello world1
 VERSION = "0.3.dev"  # noqa
 
 PACKAGES = find_packages(include=["pypaimon*"])
