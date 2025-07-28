248cd586b2b2d0574122ce2256da8c8212521896
Tan-JiaLiang <tanjialiang1997@gmail.com>
2025-07-28 11:12:34 +0800

    [core] Support the range-bitmap file index. (#5940)

---
+++ paimon-common/src/main/java/org/apache/paimon/fileindex/rangebitmap/BitSliceIndexBitmap.java
@@ -0,0 +1,312 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.fileindex.rangebitmap;
+
+import org.apache.paimon.fs.SeekableInputStream;
+import org.apache.paimon.utils.RoaringBitmap32;
+
+import javax.annotation.Nullable;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Implementation of bit-slice index bitmap. */
+public class BitSliceIndexBitmap {
+
+    public static final byte VERSION_1 = 1;
+    public static final byte CURRENT_VERSION = VERSION_1;
+
+    private final byte version;
+    private final int ebmLength;
+    private final ByteBuffer indexes;
+    private final RoaringBitmap32[] slices;
+    private final SeekableInputStream in;
+    private final int bodyOffset;
+
+    private RoaringBitmap32 ebm;
+    private boolean initialized = false;
+
+    public BitSliceIndexBitmap(SeekableInputStream in, int offset) throws IOException {
+        in.seek(offset);
+        byte[] headerLengthInBytes = new byte[Integer.BYTES];
+        in.read(headerLengthInBytes);
+        int headerLength = ByteBuffer.wrap(headerLengthInBytes).getInt();
+
+        byte[] headerInBytes = new byte[headerLength];
+        in.read(headerInBytes);
+        ByteBuffer headers = ByteBuffer.wrap(headerInBytes);
+
+        version = headers.get();
+        if (version > CURRENT_VERSION) {
+            throw new RuntimeException(
+                    String.format(
+                            "deserialize bsi index fail, " + "current version is lower than %d",
+                            version));
+        }
+
+        // slices size
+        slices = new RoaringBitmap32[headers.get()];
+
+        // ebm length
+        ebmLength = headers.getInt();
+
+        // read indexes
+        int indexesLength = headers.getInt();
+        indexes = (ByteBuffer) headers.slice().limit(indexesLength);
+
+        this.in = in;
+        this.bodyOffset = offset + Integer.BYTES + headerLength;
+    }
+
+    @Nullable
+    public Integer get(int key) {
+        if (!isNotNull().contains(key)) {
+            return null;
+        }
+        int value = 0;
+        for (int i = 0; i < slices.length; i++) {
+            if (getSlice(i).contains(key)) {
+                value |= (1 << i);
+            }
+        }
+        return value;
+    }
+
+    public RoaringBitmap32 eq(int code) {
+        RoaringBitmap32 state = isNotNull();
+        if (state.isEmpty()) {
+            return new RoaringBitmap32();
+        }
+
+        loadSlices(0, slices.length);
+        for (int i = 0; i < slices.length; i++) {
+            int bit = (code >> i) & 1;
+            if (bit == 1) {
+                state.and(getSlice(i));
+            } else {
+                state.andNot(getSlice(i));
+            }
+        }
+        return state;
+    }
+
+    public RoaringBitmap32 gt(int code) {
+        if (code < 0) {
+            return isNotNull();
+        }
+
+        RoaringBitmap32 foundSet = isNotNull();
+        if (foundSet.isEmpty()) {
+            return new RoaringBitmap32();
+        }
+
+        // the state is always start from the empty bitmap
+        RoaringBitmap32 state = null;
+
+        // if there is a run of k set bits starting from 0, [0, k] operations can be eliminated.
+        int start = Long.numberOfTrailingZeros(~code);
+        loadSlices(start, slices.length);
+        for (int i = start; i < slices.length; i++) {
+            if (state == null) {
+                state = getSlice(i).clone();
+                continue;
+            }
+
+            long bit = (code >> i) & 1;
+            if (bit == 1) {
+                state.and(getSlice(i));
+            } else {
+                state.or(getSlice(i));
+            }
+        }
+
+        if (state == null) {
+            return new RoaringBitmap32();
+        }
+
+        state.and(foundSet);
+        return state;
+    }
+
+    public RoaringBitmap32 gte(int code) {
+        return gt(code - 1);
+    }
+
+    public RoaringBitmap32 isNotNull() {
+        if (ebm == null) {
+            try {
+                in.seek(bodyOffset);
+                byte[] bytes = new byte[ebmLength];
+                in.read(bytes);
+                RoaringBitmap32 bitmap = new RoaringBitmap32();
+                bitmap.deserialize(ByteBuffer.wrap(bytes));
+                ebm = bitmap;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return ebm.clone();
+    }
+
+    private void loadSlices(int begin, int end) {
+        if (initialized) {
+            return;
+        }
+
+        indexes.position(2 * Integer.BYTES * begin);
+        int offset = indexes.getInt();
+        int length = indexes.getInt();
+        int[] lengths = new int[end];
+        lengths[begin] = length;
+        for (int i = begin + 1; i < end; i++) {
+            indexes.getInt();
+            lengths[i] = indexes.getInt();
+            length += lengths[i];
+        }
+
+        try {
+            in.seek(bodyOffset + ebmLength + offset);
+            byte[] bytes = new byte[length];
+            in.read(bytes);
+            ByteBuffer buffer = ByteBuffer.wrap(bytes);
+
+            int position = 0;
+            for (int i = begin; i < end; i++) {
+                buffer.position(position);
+                RoaringBitmap32 slice = new RoaringBitmap32();
+                slice.deserialize((ByteBuffer) buffer.slice().limit(lengths[i]));
+                slices[i] = slice;
+                position += lengths[i];
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        initialized = true;
+    }
+
+    private RoaringBitmap32 getSlice(int index) {
+        if (slices[index] == null) {
+            indexes.position(2 * Integer.BYTES * index);
+            int offset = indexes.getInt();
+            int length = indexes.getInt();
+
+            try {
+                in.seek(bodyOffset + ebmLength + offset);
+                byte[] bytes = new byte[length];
+                in.read(bytes);
+                RoaringBitmap32 slice = new RoaringBitmap32();
+                slice.deserialize(ByteBuffer.wrap(bytes));
+                slices[index] = slice;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return slices[index];
+    }
+
+    /** A Builder for {@link BitSliceIndexBitmap}. */
+    public static final class Appender {
+
+        private final int min;
+        private final int max;
+        private final RoaringBitmap32 ebm;
+        private final RoaringBitmap32[] slices;
+
+        public Appender(int min, int max) {
+            this.min = min;
+            this.max = max;
+            this.ebm = new RoaringBitmap32();
+            this.slices = new RoaringBitmap32[Long.SIZE - Long.numberOfLeadingZeros(max)];
+            for (int i = 0; i < slices.length; i++) {
+                slices[i] = new RoaringBitmap32();
+            }
+        }
+
+        public void append(int key, int value) {
+            if (value < 0) {
+                throw new UnsupportedOperationException("value can not be negative");
+            }
+
+            if (value < min || value > max) {
+                throw new IllegalArgumentException("value not in range [" + min + "," + max + "]");
+            }
+
+            // only bit=1 need to set
+            long bits = value;
+            while (bits != 0) {
+                slices[Long.numberOfTrailingZeros(bits)].add(key);
+                bits &= (bits - 1);
+            }
+            ebm.add(key);
+        }
+
+        public ByteBuffer serialize() {
+            int indexesLength = 2 * Integer.BYTES * slices.length;
+
+            byte[] ebmSerializeInBytes = ebm.serialize();
+            int ebmLength = ebmSerializeInBytes.length;
+
+            int headerSize = 0;
+            headerSize += Byte.BYTES; // version
+            headerSize += Byte.BYTES; // slices size
+            headerSize += Integer.BYTES; // ebm length
+            headerSize += Integer.BYTES; // indexes length
+            headerSize += indexesLength; // indexes size in bytes
+
+            int bodySize = 0;
+            bodySize += ebmLength;
+
+            int offset = 0;
+            ByteBuffer indexes = ByteBuffer.allocate(indexesLength);
+            List<byte[]> slicesSerializeInBytes = new ArrayList<>();
+            for (RoaringBitmap32 slice : slices) {
+                byte[] sliceInBytes = slice.serialize();
+                slicesSerializeInBytes.add(sliceInBytes);
+
+                int length = sliceInBytes.length;
+                indexes.putInt(offset);
+                indexes.putInt(length);
+
+                offset += length;
+                bodySize += length;
+            }
+
+            ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES + headerSize + bodySize);
+            buffer.putInt(headerSize);
+
+            // write header
+            buffer.put(CURRENT_VERSION);
+            buffer.put((byte) slices.length);
+            buffer.putInt(ebmLength);
+            buffer.putInt(indexesLength);
+            buffer.put(indexes.array());
+
+            // write body
+            buffer.put(ebmSerializeInBytes);
+            for (byte[] slice : slicesSerializeInBytes) {
+                buffer.put(slice);
+            }
+
+            return buffer;
+        }
+    }
+}
---
+++ paimon-common/src/main/java/org/apache/paimon/fileindex/rangebitmap/RangeBitmapFileIndex.java
@@ -0,0 +1,151 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.paimon.fileindex.rangebitmap;
+
+import org.apache.paimon.fileindex.FileIndexReader;
+import org.apache.paimon.fileindex.FileIndexResult;
+import org.apache.paimon.fileindex.FileIndexWriter;
+import org.apache.paimon.fileindex.FileIndexer;
+import org.apache.paimon.fileindex.bitmap.BitmapIndexResult;
+import org.apache.paimon.fileindex.rangebitmap.dictionary.chunked.KeyFactory;
+import org.apache.paimon.fs.SeekableInputStream;
+import org.apache.paimon.options.MemorySize;
+import org.apache.paimon.options.Options;
+import org.apache.paimon.predicate.FieldRef;
+import org.apache.paimon.types.DataType;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/** Implementation of range-bitmap file index. */
+public class RangeBitmapFileIndex implements FileIndexer {
+
+    public static final int VERSION_1 = 1;
+    public static final int CURRENT_VERSION = VERSION_1;
+
+    private final DataType dataType;
+    private final Options options;
+    public static final String CHUNK_SIZE = "chunk-size";
+
+    public RangeBitmapFileIndex(DataType dataType, Options options) {
+        this.dataType = dataType;
+        this.options = options;
+    }
+
+    @Override
+    public FileIndexWriter createWriter() {
+        return new Writer(dataType, options);
+    }
+
+    @Override
+    public FileIndexReader createReader(SeekableInputStream in, int start, int length) {
+        return new Reader(dataType, in, start);
+    }
+
+    private static class Writer extends FileIndexWriter {
+
+        private final Function<Object, Object> converter;
+        private final RangeBitmap.Appender appender;
+
+        public Writer(DataType dataType, Options options) {
+            KeyFactory factory = KeyFactory.create(dataType);
+            String chunkSize = options.getString(CHUNK_SIZE, factory.defaultChunkSize());
+            this.converter = factory.createConverter();
+            this.appender =
+                    new RangeBitmap.Appender(factory, (int) MemorySize.parse(chunkSize).getBytes());
+        }
+
+        @Override
+        public void write(Object key) {
+            appender.append(converter.apply(key));
+        }
+
+        @Override
+        public byte[] serializedBytes() {
+            return appender.serialize();
+        }
+    }
+
+    private static class Reader extends FileIndexReader {
+
+        private final Function<Object, Object> converter;
+        private final RangeBitmap bitmap;
+
+        public Reader(DataType dataType, SeekableInputStream in, int start) {
+            KeyFactory factory = KeyFactory.create(dataType);
+            this.converter = factory.createConverter();
+            this.bitmap = new RangeBitmap(in, start, factory);
+        }
+
+        @Override
+        public FileIndexResult visitIsNull(FieldRef fieldRef) {
+            return new BitmapIndexResult(bitmap::isNull);
+        }
+
+        @Override
+        public FileIndexResult visitIsNotNull(FieldRef fieldRef) {
+            return new BitmapIndexResult(bitmap::isNotNull);
+        }
+
+        @Override
+        public FileIndexResult visitEqual(FieldRef fieldRef, Object literal) {
+            return new BitmapIndexResult(() -> bitmap.eq(converter.apply(literal)));
+        }
+
+        @Override
+        public FileIndexResult visitNotEqual(FieldRef fieldRef, Object literal) {
+            return new BitmapIndexResult(() -> bitmap.neq(converter.apply(literal)));
+        }
+
+        @Override
+        public FileIndexResult visitIn(FieldRef fieldRef, List<Object> literals) {
+            return new BitmapIndexResult(
+                    () -> bitmap.in(literals.stream().map(converter).collect(Collectors.toList())));
+        }
+
+        @Override
+        public FileIndexResult visitNotIn(FieldRef fieldRef, List<Object> literals) {
+            return new BitmapIndexResult(
+                    () ->
+                            bitmap.notIn(
+                                    literals.stream().map(converter).collect(Collectors.toList())));
+        }
+
+        @Override
+        public FileIndexResult visitLessThan(FieldRef fieldRef, Object literal) {
+            return new BitmapIndexResult(() -> bitmap.lt(converter.apply(literal)));
+        }
+
+        @Override
+        public FileIndexResult visitLessOrEqual(FieldRef fieldRef, Object literal) {
+            return new BitmapIndexResult(() -> bitmap.lte(converter.apply(literal)));
+        }
+
+        @Override
+        public FileIndexResult visitGreaterThan(FieldRef fieldRef, Object literal) {
+            return new BitmapIndexResult(() -> bitmap.gt(converter.apply(literal)));
+        }
+
+        @Override
+        public FileIndexResult visitGreaterOrEqual(FieldRef fieldRef, Object literal) {
+            return new BitmapIndexResult(() -> bitmap.gte(converter.apply(literal)));
+        }
+    }
+}
