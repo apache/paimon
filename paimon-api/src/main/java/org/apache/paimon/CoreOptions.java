eaa8f7aac7af49a473e25501d789625207f91c14
yuzelin <zelin.yzl@alibaba-inc.com>
2025-07-31 12:24:15 +0800

    [core] Add compaction.force-wait to support force waiting compaction finish when preparing commit

--- paimon-api/src/main/java/org/apache/paimon/CoreOptions.java
+++ paimon-api/src/main/java/org/apache/paimon/CoreOptions.java
@@ -682,6 +682,13 @@ public class CoreOptions implements Serializable {
                     .withDescription(
                             "If set to true, compaction strategy will always include all level 0 files in candidates.");
 
+    public static final ConfigOption<Boolean> COMPACTION_FORCE_WAIT =
+            key("compaction.force-wait")
+                    .booleanType()
+                    .defaultValue(false)
+                    .withDescription(
+                            "If set to true, prepare commit will wait current compaction finished.");
+
     public static final ConfigOption<Integer> COMPACTION_SIZE_RATIO =
             key("compaction.size-ratio")
                     .intType()
@@ -2875,11 +2882,7 @@ public class CoreOptions implements Serializable {
     }
 
     public boolean prepareCommitWaitCompaction() {
-        if (!needLookup()) {
-            return false;
-        }
-
-        return options.get(LOOKUP_WAIT);
+        return options.get(COMPACTION_FORCE_WAIT) || (needLookup() && options.get(LOOKUP_WAIT));
     }
 
     public LookupCompactMode lookupCompact() {
