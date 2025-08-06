b4cfdf5e2cc4653e895e300d1ff6574eb0a43d89
Tan-JiaLiang <tanjialiang1997@gmail.com>
2025-08-06 17:45:54 +0800

    [core] Support TopN pushdown for append-only table

--- paimon-core/src/main/java/org/apache/paimon/io/FileIndexEvaluator.java
+++ paimon-core/src/main/java/org/apache/paimon/io/FileIndexEvaluator.java
@@ -23,6 +23,7 @@ import org.apache.paimon.fileindex.FileIndexResult;
 import org.apache.paimon.fs.FileIO;
 import org.apache.paimon.predicate.Predicate;
 import org.apache.paimon.predicate.PredicateBuilder;
+import org.apache.paimon.predicate.TopN;
 import org.apache.paimon.schema.TableSchema;
 
 import java.io.IOException;
@@ -36,40 +37,62 @@ public class FileIndexEvaluator {
             FileIO fileIO,
             TableSchema dataSchema,
             List<Predicate> dataFilter,
+            TopN topN,
             DataFilePathFactory dataFilePathFactory,
             DataFileMeta file)
             throws IOException {
-        if (dataFilter != null && !dataFilter.isEmpty()) {
+        FileIndexResult result = FileIndexResult.REMAIN;
+        if ((dataFilter == null || dataFilter.isEmpty()) && topN == null) {
+            return result;
+        }
+
+        FileIndexPredicate predicate = null;
+        try {
             byte[] embeddedIndex = file.embeddedIndex();
             if (embeddedIndex != null) {
-                try (FileIndexPredicate predicate =
-                        new FileIndexPredicate(embeddedIndex, dataSchema.logicalRowType())) {
-                    return predicate.evaluate(
-                            PredicateBuilder.and(dataFilter.toArray(new Predicate[0])));
+                predicate = new FileIndexPredicate(embeddedIndex, dataSchema.logicalRowType());
+            } else {
+                List<String> indexFiles =
+                        file.extraFiles().stream()
+                                .filter(
+                                        name ->
+                                                name.endsWith(
+                                                        DataFilePathFactory.INDEX_PATH_SUFFIX))
+                                .collect(Collectors.toList());
+                if (indexFiles.isEmpty()) {
+                    return result;
                 }
-            }
-
-            List<String> indexFiles =
-                    file.extraFiles().stream()
-                            .filter(name -> name.endsWith(DataFilePathFactory.INDEX_PATH_SUFFIX))
-                            .collect(Collectors.toList());
-            if (!indexFiles.isEmpty()) {
                 if (indexFiles.size() > 1) {
                     throw new RuntimeException(
                             "Found more than one index file for one data file: "
                                     + String.join(" and ", indexFiles));
                 }
-                // go to file index check
-                try (FileIndexPredicate predicate =
+                predicate =
                         new FileIndexPredicate(
                                 dataFilePathFactory.toAlignedPath(indexFiles.get(0), file),
                                 fileIO,
-                                dataSchema.logicalRowType())) {
-                    return predicate.evaluate(
-                            PredicateBuilder.and(dataFilter.toArray(new Predicate[0])));
-                }
+                                dataSchema.logicalRowType());
+            }
+
+            // data filter
+            if (dataFilter != null && !dataFilter.isEmpty()) {
+                result =
+                        predicate.evaluate(
+                                PredicateBuilder.and(dataFilter.toArray(new Predicate[0])));
+            }
+
+            // todo: We need to do more if the index filter supports pushdown.
+            //  e.g. All pushdown index filters must exist and be evaluated.
+            // topN filter
+            if (dataFilter == null && topN != null) {
+                result = predicate.evaluate(topN, result);
+            }
+
+            return result;
+        } finally {
+            if (predicate != null) {
+                predicate.close();
             }
         }
-        return FileIndexResult.REMAIN;
     }
 }
--- paimon-core/src/main/java/org/apache/paimon/utils/FormatReaderMapping.java
+++ paimon-core/src/main/java/org/apache/paimon/utils/FormatReaderMapping.java
@@ -23,6 +23,7 @@ import org.apache.paimon.format.FileFormatDiscover;
 import org.apache.paimon.format.FormatReaderFactory;
 import org.apache.paimon.partition.PartitionUtils;
 import org.apache.paimon.predicate.Predicate;
+import org.apache.paimon.predicate.TopN;
 import org.apache.paimon.schema.IndexCastMapping;
 import org.apache.paimon.schema.SchemaEvolutionUtil;
 import org.apache.paimon.schema.TableSchema;
@@ -60,6 +61,7 @@ public class FormatReaderMapping {
     private final TableSchema dataSchema;
     private final List<Predicate> dataFilters;
     private final Map<String, Integer> systemFields;
+    @Nullable private final TopN topN;
 
     public FormatReaderMapping(
             @Nullable int[] indexMapping,
@@ -69,7 +71,8 @@ public class FormatReaderMapping {
             FormatReaderFactory readerFactory,
             TableSchema dataSchema,
             List<Predicate> dataFilters,
-            Map<String, Integer> systemFields) {
+            Map<String, Integer> systemFields,
+            @Nullable TopN topN) {
         this.indexMapping = combine(indexMapping, trimmedKeyMapping);
         this.castMapping = castMapping;
         this.readerFactory = readerFactory;
@@ -77,6 +80,7 @@ public class FormatReaderMapping {
         this.dataSchema = dataSchema;
         this.dataFilters = dataFilters;
         this.systemFields = systemFields;
+        this.topN = topN;
     }
 
     private int[] combine(@Nullable int[] indexMapping, @Nullable int[] trimmedKeyMapping) {
@@ -130,6 +134,11 @@ public class FormatReaderMapping {
         return dataFilters;
     }
 
+    @Nullable
+    public TopN getTopN() {
+        return topN;
+    }
+
     /** Builder for {@link FormatReaderMapping}. */
     public static class Builder {
 
@@ -137,6 +146,7 @@ public class FormatReaderMapping {
         private final List<DataField> readTableFields;
         private final Function<TableSchema, List<DataField>> fieldsExtractor;
         @Nullable private final List<Predicate> filters;
+        @Nullable private final TopN topN;
         private final boolean rowTrackingEnabled;
 
         public Builder(
@@ -144,11 +154,13 @@ public class FormatReaderMapping {
                 List<DataField> readTableFields,
                 Function<TableSchema, List<DataField>> fieldsExtractor,
                 @Nullable List<Predicate> filters,
+                @Nullable TopN topN,
                 boolean rowTrackingEnabled) {
             this.formatDiscover = formatDiscover;
             this.readTableFields = readTableFields;
             this.fieldsExtractor = fieldsExtractor;
             this.filters = filters;
+            this.topN = topN;
             this.rowTrackingEnabled = rowTrackingEnabled;
         }
 
@@ -212,7 +224,8 @@ public class FormatReaderMapping {
                             .createReaderFactory(readRowType, readFilters),
                     dataSchema,
                     readFilters,
-                    systemFields);
+                    systemFields,
+                    topN);
         }
 
         private Map<String, Integer> findSystemFields(List<DataField> readTableFields) {
