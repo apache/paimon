6ce9f03a60d17265b8c068c87adb028c1c5049ed
xiyu.zk <xiyu.zk@alibaba-inc.com>
2025-09-21 13:40:14 +0800

    [spark] Support ACCEPT_ANY_SCHEMA for spark v2 write

--- paimon-spark/paimon-spark-ut/src/test/scala/org/apache/paimon/spark/sql/V2WriteMergeSchemaTest.scala
+++ paimon-spark/paimon-spark-ut/src/test/scala/org/apache/paimon/spark/sql/V2WriteMergeSchemaTest.scala
@@ -27,6 +27,7 @@ class V2WriteMergeSchemaTest extends PaimonSparkTestBase {
 
   override protected def sparkConf: SparkConf = {
     super.sparkConf
+      .set("spark.sql.catalog.paimon.cache-enabled", "false")
       .set("spark.paimon.write.use-v2-write", "true")
       .set("spark.paimon.write.merge-schema", "true")
       .set("spark.paimon.write.merge-schema.explicit-cast", "true")
