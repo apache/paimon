aa11de1090f55daf10721b87545ab2991ca7b991
ChengHui Chen <27797326+chenghuichen@users.noreply.github.com>
2025-10-20 19:19:41 +0800

    [Python] optimize codes related to push_down_utils (#6430)

--- paimon-python/pypaimon/read/push_down_utils.py
+++ paimon-python/pypaimon/read/push_down_utils.py
@@ -22,9 +22,9 @@ from pypaimon.common.predicate import Predicate
 from pypaimon.common.predicate_builder import PredicateBuilder
 
 
-def filter_and_transform_predicate(input_predicate: Predicate, all_fields: List[str], fields: List[str]):
-    new_predicate = filter_predicate_by_fields(input_predicate, fields)
-    part_to_index = {element: idx for idx, element in enumerate(fields)}
+def trim_and_transform_predicate(input_predicate: Predicate, all_fields: List[str], trimmed_keys: List[str]):
+    new_predicate = trim_predicate_by_fields(input_predicate, trimmed_keys)
+    part_to_index = {element: idx for idx, element in enumerate(trimmed_keys)}
     mapping: Dict[int, int] = {
         i: part_to_index.get(all_fields[i], -1)
         for i in range(len(all_fields))
@@ -32,12 +32,12 @@ def filter_and_transform_predicate(input_predicate: Predicate, all_fields: List[
     return _change_index(new_predicate, mapping)
 
 
-def filter_predicate_by_fields(input_predicate: Predicate, fields: List[str]):
-    if not input_predicate or not fields:
+def trim_predicate_by_fields(input_predicate: Predicate, trimmed_keys: List[str]):
+    if not input_predicate or not trimmed_keys:
         return None
 
     predicates: list[Predicate] = _split_and(input_predicate)
-    predicates = [element for element in predicates if _get_all_fields(element).issubset(fields)]
+    predicates = [element for element in predicates if _get_all_fields(element).issubset(trimmed_keys)]
     return PredicateBuilder.and_predicates(predicates)
 
 
@@ -46,7 +46,7 @@ def _split_and(input_predicate: Predicate):
         return list()
 
     if input_predicate.method == 'and':
-        return list(input_predicate.literals)
+        return [p for element in (input_predicate.literals or []) for p in _split_and(element)]
 
     return [input_predicate]
 
