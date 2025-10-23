3efbd28a412fb6dda1a704334be2ccd9e12b9c32
李鹏程 <li.pc@topsports.com.cn>
2025-10-23 18:25:57 +0800

    [feature] Force auto-tag creation ignore delay option

--- paimon-spark/paimon-spark-ut/src/test/scala/org/apache/paimon/spark/procedure/TriggerTagAutomaticCreationProcedureTest.scala
+++ paimon-spark/paimon-spark-ut/src/test/scala/org/apache/paimon/spark/procedure/TriggerTagAutomaticCreationProcedureTest.scala
@@ -72,7 +72,7 @@ class TriggerTagAutomaticCreationProcedureTest extends PaimonSparkTestBase {
     spark.sql("""alter table T_FORCE_AUTO_TAG_IGNORE_DELAY set tblproperties(
                 |'tag.automatic-creation'='process-time',
                 |'tag.creation-period'='daily',
-                |'tag.creation-delay'='2 d',
+                |'tag.creation-delay'='1 d',
                 |'tag.num-retained-max'='90'
                 |)""".stripMargin)
 
