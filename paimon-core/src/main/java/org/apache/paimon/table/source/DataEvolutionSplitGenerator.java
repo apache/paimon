6c09c058203b20199661c108e97dc3fb96fbbe54
JingsongLi <jingsonglee0@gmail.com>
2025-10-29 19:13:33 +0800

    [core] Refactor DataEvolutionSplitGenerator.split to validate more

--- paimon-core/src/main/java/org/apache/paimon/table/source/DataEvolutionSplitGenerator.java
+++ paimon-core/src/main/java/org/apache/paimon/table/source/DataEvolutionSplitGenerator.java
@@ -21,10 +21,10 @@ package org.apache.paimon.table.source;
 import org.apache.paimon.io.DataFileMeta;
 import org.apache.paimon.manifest.ManifestEntry;
 import org.apache.paimon.utils.BinPacking;
+import org.apache.paimon.utils.RangeHelper;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
@@ -32,6 +32,7 @@ import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 
 import static org.apache.paimon.format.blob.BlobFileFormat.isBlobFile;
+import static org.apache.paimon.utils.Preconditions.checkArgument;
 
 /** Append data evolution table split generator, which implementation of {@link SplitGenerator}. */
 public class DataEvolutionSplitGenerator implements SplitGenerator {
@@ -99,90 +100,61 @@ public class DataEvolutionSplitGenerator implements SplitGenerator {
             List<T> files,
             Function<T, String> fileNameF,
             Function<T, Long> firstRowIdF,
-            Function<T, Long> rowCountF,
-            Function<T, Long> maxSequenceNumberF) {
-        List<List<T>> splitByRowId = new ArrayList<>();
-        // Sort files by firstRowId and then by maxSequenceNumber
-        files.sort(
-                Comparator.comparingLong(
-                                (ToLongFunction<T>)
-                                        value ->
-                                                firstRowIdF.apply(value) == null
-                                                        ? Long.MIN_VALUE
-                                                        : firstRowIdF.apply(value))
-                        .thenComparingInt(f -> isBlobFile(fileNameF.apply(f)) ? 1 : 0)
-                        .thenComparing(
-                                (f1, f2) -> {
-                                    // If firstRowId is the same, we should read the file with
-                                    // larger sequence number first. Because larger sequence number
-                                    // file is more fresh
-                                    return Long.compare(
-                                            maxSequenceNumberF.apply(f2),
-                                            maxSequenceNumberF.apply(f1));
-                                }));
+            ToLongFunction<T> rowCountF,
+            ToLongFunction<T> maxSeqF) {
+        // group by row id range
+        ToLongFunction<T> firstRowIdFunc =
+                t -> {
+                    Long firstRowId = firstRowIdF.apply(t);
+                    checkArgument(
+                            firstRowId != null, "File %s First row id should not be null.", t);
+                    return firstRowId;
+                };
+        ToLongFunction<T> endRowIdF =
+                t -> firstRowIdFunc.applyAsLong(t) + rowCountF.applyAsLong(t) - 1;
+        RangeHelper<T> rangeHelper = new RangeHelper<>(firstRowIdFunc, endRowIdF);
+        List<List<T>> result = rangeHelper.mergeOverlappingRanges(files);
 
-        files = filterBlob(files, fileNameF, firstRowIdF, rowCountF);
+        // skip group which only has blob files
+        result.removeIf(next -> next.stream().allMatch(t -> isBlobFile(fileNameF.apply(t))));
 
-        // Split files by firstRowId
-        long lastRowId = -1;
-        long checkRowIdStart = 0;
-        List<T> currentSplit = new ArrayList<>();
-        for (int i = 0; i < files.size(); i++) {
-            T file = files.get(i);
-            Long firstRowId = firstRowIdF.apply(file);
-            if (firstRowId == null) {
-                splitByRowId.add(Collections.singletonList(file));
-                continue;
-            }
-            if (!isBlobFile(fileNameF.apply(file)) && firstRowId != lastRowId) {
-                if (!currentSplit.isEmpty()) {
-                    splitByRowId.add(currentSplit);
-                }
-                if (firstRowId < checkRowIdStart) {
-                    throw new IllegalStateException(
-                            String.format(
-                                    "There are overlapping files in the split: \n %s, the wrong file is: \n %s",
-                                    files.subList(Math.max(0, i - 20), i).stream()
-                                            .map(Object::toString)
-                                            .collect(Collectors.joining(",")),
-                                    file));
-                }
-                currentSplit = new ArrayList<>();
-                lastRowId = firstRowId;
-                checkRowIdStart = firstRowId + rowCountF.apply(file);
-            }
-            currentSplit.add(file);
-        }
-        if (!currentSplit.isEmpty()) {
-            splitByRowId.add(currentSplit);
-        }
-
-        return splitByRowId;
-    }
-
-    private static <T> List<T> filterBlob(
-            List<T> files,
-            Function<T, String> fileNameF,
-            Function<T, Long> firstRowIdF,
-            Function<T, Long> rowCountF) {
-        List<T> result = new ArrayList<>();
-        long rowIdStart = -1;
-        long rowIdEnd = -1;
-        for (T file : files) {
-            if (firstRowIdF.apply(file) == null) {
-                result.add(file);
-                continue;
-            }
-            if (!isBlobFile(fileNameF.apply(file))) {
-                rowIdStart = firstRowIdF.apply(file);
-                rowIdEnd = firstRowIdF.apply(file) + rowCountF.apply(file);
-                result.add(file);
-            } else {
-                if (firstRowIdF.apply(file) >= rowIdStart && firstRowIdF.apply(file) < rowIdEnd) {
-                    result.add(file);
+        // in group, sort by blob file and max_seq
+        for (List<T> group : result) {
+            // split to data files and blob files
+            List<T> dataFiles = new ArrayList<>();
+            List<T> blobFiles = new ArrayList<>();
+            for (T t : group) {
+                if (isBlobFile(fileNameF.apply(t))) {
+                    blobFiles.add(t);
+                } else {
+                    dataFiles.add(t);
                 }
             }
+
+            // data files sort by reversed max sequence number
+            dataFiles.sort(Comparator.comparingLong(maxSeqF).reversed());
+            checkArgument(!dataFiles.isEmpty(), "Data files should not be empty.");
+            checkArgument(
+                    rangeHelper.areAllRangesSame(dataFiles),
+                    "Data files %s should be all row id ranges same.",
+                    dataFiles);
+
+            // blob files sort by first row id
+            blobFiles.sort(Comparator.comparingLong(firstRowIdFunc));
+            if (!blobFiles.isEmpty()) {
+                checkArgument(
+                        rangeHelper.isContiguousAndCovered(blobFiles, dataFiles.get(0)),
+                        "Blob files %s should be contiguous and covered by data files %s.",
+                        blobFiles,
+                        dataFiles);
+            }
+
+            // concat data files and blob files
+            group.clear();
+            group.addAll(dataFiles);
+            group.addAll(blobFiles);
         }
+
         return result;
     }
 }
