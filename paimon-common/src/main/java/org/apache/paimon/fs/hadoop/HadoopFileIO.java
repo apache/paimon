81658e0a4983fbfdc6447738836ab41f04243137
senmiaoliu <senmiaoliu@trip.com>
2025-11-10 11:40:31 +0800

    Add support for atomic rename in file overwrite operations

--- paimon-common/src/main/java/org/apache/paimon/fs/hadoop/HadoopFileIO.java
+++ paimon-common/src/main/java/org/apache/paimon/fs/hadoop/HadoopFileIO.java
@@ -28,6 +28,7 @@ import org.apache.paimon.fs.PositionOutputStream;
 import org.apache.paimon.fs.RemoteIterator;
 import org.apache.paimon.fs.SeekableInputStream;
 import org.apache.paimon.hadoop.SerializableConfiguration;
+import org.apache.paimon.options.CatalogOptions;
 import org.apache.paimon.utils.FileIOUtils;
 import org.apache.paimon.utils.FunctionWithException;
 import org.apache.paimon.utils.Pair;
@@ -59,6 +60,8 @@ public class HadoopFileIO implements FileIO, HadoopOptionsProvider {
 
     private org.apache.paimon.options.Options options;
 
+    private boolean atomicRenameEnabled = true;
+
     protected transient volatile Map<Pair<String, String>, FileSystem> fsMap;
 
     private final Path path;
@@ -82,6 +85,7 @@ public class HadoopFileIO implements FileIO, HadoopOptionsProvider {
     public void configure(CatalogContext context) {
         this.hadoopConf = new SerializableConfiguration(context.hadoopConf());
         this.options = context.options();
+        this.atomicRenameEnabled = options.get(CatalogOptions.FILE_IO_ATOMIC_RENAME_ENABLED);
     }
 
     public Configuration hadoopConf() {
@@ -178,8 +182,12 @@ public class HadoopFileIO implements FileIO, HadoopOptionsProvider {
 
     @Override
     public void overwriteFileUtf8(Path path, String content) throws IOException {
-        boolean success = tryAtomicOverwriteViaRename(path, content);
-        if (!success) {
+        if (atomicRenameEnabled) {
+            boolean success = tryAtomicOverwriteViaRename(path, content);
+            if (!success) {
+                FileIO.super.overwriteFileUtf8(path, content);
+            }
+        } else {
             FileIO.super.overwriteFileUtf8(path, content);
         }
     }
