5434595a148a339dac115c450bf2afde3526d376
Jingsong Lee <jingsonglee0@gmail.com>
2025-12-08 22:45:00 +0800

    [core] Make bucketed append table write initial lighter (#6741)

--- paimon-core/src/main/java/org/apache/paimon/table/AppendOnlyFileStoreTable.java
+++ paimon-core/src/main/java/org/apache/paimon/table/AppendOnlyFileStoreTable.java
@@ -88,8 +88,9 @@ public class AppendOnlyFileStoreTable extends AbstractFileStoreTable {
 
     @Override
     protected SplitGenerator splitGenerator() {
-        long targetSplitSize = store().options().splitTargetSize();
-        long openFileCost = store().options().splitOpenFileCost();
+        CoreOptions options = store().options();
+        long targetSplitSize = options.splitTargetSize();
+        long openFileCost = options.splitOpenFileCost();
         return coreOptions().dataEvolutionEnabled()
                 ? new DataEvolutionSplitGenerator(targetSplitSize, openFileCost)
                 : new AppendOnlySplitGenerator(targetSplitSize, openFileCost, bucketMode());
