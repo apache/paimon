808a476f20167914a25c693028b2fb618f0adbdc
sychen <sychen@ctrip.com>
2025-12-26 19:46:25 +0800

    Revert "test"
    This reverts commit 3ac086b1b33a80d392c57878424de8ff17320e5b.

--- paimon-spark/paimon-spark-common/src/main/scala/org/apache/paimon/spark/SparkTable.scala
+++ paimon-spark/paimon-spark-common/src/main/scala/org/apache/paimon/spark/SparkTable.scala
@@ -32,7 +32,7 @@ case class SparkTable(override val table: Table)
   override def newRowLevelOperationBuilder(
       info: RowLevelOperationInfo): RowLevelOperationBuilder = {
     table match {
-      case t: FileStoreTable =>
+      case t: FileStoreTable if useV2Write =>
         () => new PaimonSparkCopyOnWriteOperation(t, info)
       case _ =>
         throw new UnsupportedOperationException(
