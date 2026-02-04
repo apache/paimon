a1d0ae7836af9829d94ebb9a2a06cd97e14ef17c
仟弋 <yejunhao.yjh@alibaba-inc.com>
2026-02-04 13:46:18 +0800

    Fix minus

--- paimon-api/src/main/java/org/apache/paimon/CoreOptions.java
+++ paimon-api/src/main/java/org/apache/paimon/CoreOptions.java
@@ -1889,12 +1889,7 @@ public class CoreOptions implements Serializable {
                                     + "the reader will try to get this partition from this fallback branch.");
 
     public static final ConfigOption<Boolean> ASYNC_FILE_WRITE =
-            key("    public static final ConfigOption<Boolean> ASYNC_FILE_WRITE =\n"
-                            + "            key(\"async-file-write\")\n"
-                            + "                    .booleanType()\n"
-                            + "                    .defaultValue(true)\n"
-                            + "                    .withDescription(\n"
-                            + "                            \"Whether to enable asynchronous IO writing when writing files.\");")
+            key("async-file-write")
                     .booleanType()
                     .defaultValue(true)
                     .withDescription(
