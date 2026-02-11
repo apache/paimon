465fb26eed3d0ab3df17019d64c1c28e6e13eb92
xuyu <11161569@vivo.com>
2026-02-11 16:09:42 +0800

    Fix style

--- paimon-api/src/main/java/org/apache/paimon/utils/StringUtils.java
+++ paimon-api/src/main/java/org/apache/paimon/utils/StringUtils.java
@@ -18,7 +18,12 @@
 
 package org.apache.paimon.utils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.apache.paimon.utils.Preconditions.checkArgument;
