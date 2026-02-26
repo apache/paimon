4eabeefba20267fc932362bd123674fd518b9cf5
仟弋 <yejunhao.yjh@alibaba-inc.com>
2026-02-26 17:21:43 +0800

    Fix comment

--- paimon-python/pypaimon/table/row/blob.py
+++ paimon-python/pypaimon/table/row/blob.py
@@ -125,13 +125,7 @@ class BlobDescriptor:
             return False
 
         version = raw[0]
-        if version == 1:
-            try:
-                cls.deserialize(raw)
-                return True
-            except Exception:
-                return False
-        if version != cls.CURRENT_VERSION:
+        if version > cls.CURRENT_VERSION:
             return False
 
         try:
