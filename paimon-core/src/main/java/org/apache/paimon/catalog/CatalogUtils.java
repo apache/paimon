44e101a85fc231b56205df8813deaf782f07465a
JingsongLi <jingsonglee0@gmail.com>
2026-03-04 18:34:18 +0800

    fix

--- paimon-core/src/main/java/org/apache/paimon/catalog/CatalogUtils.java
+++ paimon-core/src/main/java/org/apache/paimon/catalog/CatalogUtils.java
@@ -54,10 +54,12 @@ import javax.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 
 import static org.apache.paimon.CoreOptions.AUTO_CREATE;
@@ -209,20 +211,24 @@ public class CatalogUtils {
 
         // partitions should be seen even all files are level-0 when enable dv, see
         // https://github.com/apache/paimon/pull/6531 for details
-        List<PartitionEntry> partitionEntries;
         if (scan instanceof InnerTableScan) {
-            InnerTableScan innerTableScan = (InnerTableScan) scan;
-            if (partitionSpecs != null && !partitionSpecs.isEmpty()) {
-                innerTableScan = innerTableScan.withPartitionsFilter(partitionSpecs);
+            ((InnerTableScan) scan).withLevelFilter(level -> true);
+            if (partitionSpecs != null) {
+                ((InnerTableScan) scan).withPartitionsFilter(partitionSpecs);
             }
-            partitionEntries = innerTableScan.withLevelFilter(level -> true).listPartitionEntries();
-        } else {
-            partitionEntries = scan.listPartitionEntries();
         }
 
+        List<PartitionEntry> partitionEntries = scan.listPartitionEntries();
+
         List<Partition> partitions = new ArrayList<>(partitionEntries.size());
+        Set<Map<String, String>> filterSet =
+                partitionSpecs == null ? null : new HashSet<>(partitionSpecs);
         for (PartitionEntry entry : partitionEntries) {
-            partitions.add(entry.toPartition(computer));
+            Partition partition = entry.toPartition(computer);
+            if (filterSet != null && !filterSet.contains(partition.spec())) {
+                continue;
+            }
+            partitions.add(partition);
         }
         return partitions;
     }
