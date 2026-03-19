cc05f2fd9950a151f3c08ab27af90779af01a443
XiaoHongbo <1346652787@qq.com>
2026-03-19 08:49:58 +0800

    [python] Support partition.default-name to fix read null/empty partition values error  (#7472)

--- paimon-python/pypaimon/utils/file_store_path_factory.py
+++ paimon-python/pypaimon/utils/file_store_path_factory.py
@@ -21,6 +21,13 @@ from pypaimon.common.external_path_provider import ExternalPathProvider
 from pypaimon.table.bucket_mode import BucketMode
 
 
+def _is_null_or_whitespace_only(value) -> bool:
+    if value is None:
+        return True
+    s = str(value)
+    return len(s) == 0 or s.isspace()
+
+
 class FileStorePathFactory:
     MANIFEST_PATH = "manifest"
     MANIFEST_PREFIX = "manifest-"
@@ -91,7 +98,12 @@ class FileStorePathFactory:
         if partition:
             partition_parts = []
             for i, field_name in enumerate(self.partition_keys):
-                partition_parts.append(f"{field_name}={partition[i]}")
+                val = partition[i]
+                if _is_null_or_whitespace_only(val):
+                    val = self.default_part_value
+                else:
+                    val = str(val)
+                partition_parts.append(f"{field_name}={val}")
             if partition_parts:
                 relative_parts = partition_parts + relative_parts
 
