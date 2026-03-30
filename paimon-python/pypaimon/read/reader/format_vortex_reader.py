619dd77084223d254d94ebf89220d024eab27026
JingsongLi <jingsonglee0@gmail.com>
2026-03-30 23:21:09 +0800

    fix

--- paimon-python/pypaimon/read/reader/format_vortex_reader.py
+++ paimon-python/pypaimon/read/reader/format_vortex_reader.py
@@ -19,7 +19,6 @@
 from typing import List, Optional, Any
 
 import pyarrow as pa
-import pyarrow.dataset as ds
 from pyarrow import RecordBatch
 
 from pypaimon.common.file_io import FileIO
@@ -49,57 +48,28 @@ class FormatVortexReader(RecordBatchReader):
 
         columns_for_vortex = read_fields if read_fields else None
 
+        # Try to convert Arrow predicate to Vortex expr for native push-down
+        vortex_expr = None
+        if push_down_predicate is not None:
+            try:
+                from vortex.arrow.expression import arrow_to_vortex
+                arrow_schema = vortex_file.dtype.to_arrow_schema()
+                vortex_expr = arrow_to_vortex(push_down_predicate, arrow_schema)
+            except Exception:
+                pass
+
+        indices = None
         if row_indices is not None:
-            # Use scan with indices for efficient row-level pushdown
-            vortex_indices = vortex.array(row_indices)
-            scan_iter = vortex_file.scan(
-                columns_for_vortex, indices=vortex_indices, batch_size=batch_size)
-            pa_table = scan_iter.read_all().to_arrow_table()
-            pa_table = self._cast_string_view_columns(pa_table)
-            if push_down_predicate is not None:
-                in_memory_dataset = ds.InMemoryDataset(pa_table)
-                scanner = in_memory_dataset.scanner(filter=push_down_predicate, batch_size=batch_size)
-                self.reader = scanner.to_reader()
-            else:
-                self.reader = iter(pa_table.to_batches(max_chunksize=batch_size))
-        else:
-            pa_table = vortex_file.to_arrow(columns_for_vortex).read_all()
-            # Vortex exports string_view which some PyArrow kernels don't support yet.
-            pa_table = self._cast_string_view_columns(pa_table)
+            indices = vortex.array(row_indices)
 
-            if push_down_predicate is not None:
-                in_memory_dataset = ds.InMemoryDataset(pa_table)
-                scanner = in_memory_dataset.scanner(filter=push_down_predicate, batch_size=batch_size)
-                self.reader = scanner.to_reader()
-            else:
-                self.reader = iter(pa_table.to_batches(max_chunksize=batch_size))
-
-    @staticmethod
-    def _cast_string_view_columns(table: pa.Table) -> pa.Table:
-        new_fields = []
-        needs_cast = False
-        for field in table.schema:
-            if field.type == pa.string_view():
-                new_fields.append(field.with_type(pa.utf8()))
-                needs_cast = True
-            elif field.type == pa.binary_view():
-                new_fields.append(field.with_type(pa.binary()))
-                needs_cast = True
-            else:
-                new_fields.append(field)
-        if not needs_cast:
-            return table
-        return table.cast(pa.schema(new_fields))
+        self.record_batch_reader = vortex_file.scan(
+            columns_for_vortex, expr=vortex_expr, indices=indices, batch_size=batch_size).to_arrow()
 
     def read_arrow_batch(self) -> Optional[RecordBatch]:
         try:
-            if hasattr(self.reader, 'read_next_batch'):
-                return self.reader.read_next_batch()
-            else:
-                return next(self.reader)
+            return next(self.record_batch_reader)
         except StopIteration:
             return None
 
     def close(self):
-        if self.reader is not None:
-            self.reader = None
+        self.scan_iter = None
