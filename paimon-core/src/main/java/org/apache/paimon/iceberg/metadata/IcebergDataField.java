85e334c3b0a1a4b81dcbfd49274a8c211f20cfef
Arnav Balyan <arnavbalyan1@gmail.com>
2026-04-29 16:08:59 +0530

    update

--- paimon-core/src/main/java/org/apache/paimon/iceberg/metadata/IcebergDataField.java
+++ paimon-core/src/main/java/org/apache/paimon/iceberg/metadata/IcebergDataField.java
@@ -33,6 +33,7 @@ import org.apache.paimon.types.IntType;
 import org.apache.paimon.types.LocalZonedTimestampType;
 import org.apache.paimon.types.MapType;
 import org.apache.paimon.types.RowType;
+import org.apache.paimon.types.TimeType;
 import org.apache.paimon.types.TimestampType;
 import org.apache.paimon.types.VarBinaryType;
 import org.apache.paimon.types.VarCharType;
@@ -163,6 +164,12 @@ public class IcebergDataField {
                 return "double";
             case DATE:
                 return "date";
+            case TIME_WITHOUT_TIME_ZONE:
+                int timePrecision = ((TimeType) dataType).getPrecision();
+                Preconditions.checkArgument(
+                        timePrecision >= 0 && timePrecision <= 3,
+                        "Paimon Iceberg compatibility only support time type with precision from 0 to 3.");
+                return "time";
             case CHAR:
             case VARCHAR:
                 return "string";
@@ -235,6 +242,8 @@ public class IcebergDataField {
                     return new DoubleType(!isRequired);
                 case "date":
                     return new DateType(!isRequired);
+                case "time":
+                    return new TimeType(!isRequired, 3);
                 case "string":
                     return new VarCharType(!isRequired, VarCharType.MAX_LENGTH);
                 case "binary":
