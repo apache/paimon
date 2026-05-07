5268dea5c305fe3946768c82a0e4ad0c91d1b37b
仟弋 <yejunhao.yjh@alibaba-inc.com>
2026-05-07 15:13:41 +0800

    Fix minus

--- paimon-core/src/main/java/org/apache/paimon/table/PrimaryKeyFileStoreTable.java
+++ paimon-core/src/main/java/org/apache/paimon/table/PrimaryKeyFileStoreTable.java
@@ -44,7 +44,6 @@ import javax.annotation.Nullable;
 
 import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.Supplier;
 
 import static org.apache.paimon.predicate.PredicateBuilder.and;
 import static org.apache.paimon.predicate.PredicateBuilder.pickTransformFieldMapping;
@@ -147,11 +146,8 @@ public class PrimaryKeyFileStoreTable extends AbstractFileStoreTable {
 
     @Override
     public InnerTableRead newRead() {
-        Supplier<org.apache.paimon.operation.MergeFileSplitRead> mergeReadSupplier =
-                () -> store().newRead();
-        Supplier<org.apache.paimon.operation.RawFileSplitRead> batchRawReadSupplier =
-                () -> store().newBatchRawFileRead();
-        return new KeyValueTableRead(mergeReadSupplier, batchRawReadSupplier, schema());
+        return new KeyValueTableRead(
+                () -> store().newRead(), () -> store().newBatchRawFileRead(), schema());
     }
 
     @Override
