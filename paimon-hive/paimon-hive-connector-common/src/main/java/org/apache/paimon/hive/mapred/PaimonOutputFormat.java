13980ce523b790c985535f790713451981dc5fa0
Arnav Balyan <arnavbalyan1@gmail.com>
2026-05-12 10:39:01 +0530

    update

--- paimon-hive/paimon-hive-connector-common/src/main/java/org/apache/paimon/hive/mapred/PaimonOutputFormat.java
+++ paimon-hive/paimon-hive-connector-common/src/main/java/org/apache/paimon/hive/mapred/PaimonOutputFormat.java
@@ -19,13 +19,19 @@
 package org.apache.paimon.hive.mapred;
 
 import org.apache.paimon.CoreOptions;
+import org.apache.paimon.data.GenericRow;
 import org.apache.paimon.hive.RowDataContainer;
+import org.apache.paimon.schema.TableSchema;
 import org.apache.paimon.table.FileStoreTable;
 import org.apache.paimon.table.sink.BatchTableWrite;
 import org.apache.paimon.table.sink.BatchWriteBuilder;
+import org.apache.paimon.types.DataField;
+import org.apache.paimon.utils.PartitionPathUtils;
+import org.apache.paimon.utils.TypeUtils;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants;
 import org.apache.hadoop.hive.ql.exec.FileSinkOperator;
 import org.apache.hadoop.hive.ql.io.HiveOutputFormat;
 import org.apache.hadoop.io.NullWritable;
@@ -37,7 +43,10 @@ import org.apache.hadoop.mapred.TaskAttemptID;
 import org.apache.hadoop.util.Progressable;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -69,20 +78,79 @@ public class PaimonOutputFormat
             Properties properties,
             Progressable progressable)
             throws IOException {
-        return writer(jobConf);
+        FileStoreTable table = forWriteOnly(createFileStoreTable(jobConf));
+        PaimonRecordWriter inner = writer(jobConf, table);
+
+        GenericRow staticPartitionRow = buildStaticPartitionRow(path, properties, table.schema());
+        return staticPartitionRow == null
+                ? inner
+                : new PartitionedRecordWriter(
+                        inner, staticPartitionRow, table.schema().fields().size());
     }
 
     private static PaimonRecordWriter writer(JobConf jobConf) {
-        TaskAttemptID taskAttemptID = TezUtil.taskAttemptWrapper(jobConf);
+        return writer(jobConf, forWriteOnly(createFileStoreTable(jobConf)));
+    }
 
-        FileStoreTable table = createFileStoreTable(jobConf);
-        // force write-only = true
+    private static PaimonRecordWriter writer(JobConf jobConf, FileStoreTable table) {
+        TaskAttemptID taskAttemptID = TezUtil.taskAttemptWrapper(jobConf);
+        BatchWriteBuilder batchWriteBuilder = table.newBatchWriteBuilder();
+        BatchTableWrite batchTableWrite = batchWriteBuilder.newWrite();
+        return new PaimonRecordWriter(batchTableWrite, taskAttemptID, table.name());
+    }
+
+    private static FileStoreTable forWriteOnly(FileStoreTable table) {
         Map<String, String> newOptions =
                 Collections.singletonMap(CoreOptions.WRITE_ONLY.key(), Boolean.TRUE.toString());
-        FileStoreTable copy = table.copy(newOptions);
-        BatchWriteBuilder batchWriteBuilder = copy.newBatchWriteBuilder();
-        BatchTableWrite batchTableWrite = batchWriteBuilder.newWrite();
+        return table.copy(newOptions);
+    }
 
-        return new PaimonRecordWriter(batchTableWrite, taskAttemptID, copy.name());
+    static GenericRow buildStaticPartitionRow(
+            Path path, Properties properties, TableSchema schema) {
+        String partitionProperty =
+                properties.getProperty(hive_metastoreConstants.META_TABLE_PARTITION_COLUMNS);
+        if (partitionProperty == null || partitionProperty.isEmpty()) {
+            return null;
+        }
+        List<String> partitionKeys =
+                Arrays.asList(partitionProperty.split(org.apache.paimon.fs.Path.SEPARATOR));
+
+        LinkedHashMap<String, String> spec =
+                PartitionPathUtils.extractPartitionSpecFromPath(
+                        new org.apache.paimon.fs.Path(path.toString()));
+        if (spec.isEmpty()) {
+            return null;
+        }
+
+        GenericRow row = new GenericRow(partitionKeys.size());
+        List<DataField> fields = schema.fields();
+        for (int i = 0; i < partitionKeys.size(); i++) {
+            String key = partitionKeys.get(i);
+            String raw = lookupCaseInsensitive(spec, key);
+            if (raw == null) {
+                return null;
+            }
+            DataField field = findField(fields, key);
+            row.setField(i, TypeUtils.castFromString(raw, field.type()));
+        }
+        return row;
+    }
+
+    private static String lookupCaseInsensitive(Map<String, String> spec, String key) {
+        for (Map.Entry<String, String> e : spec.entrySet()) {
+            if (e.getKey().equalsIgnoreCase(key)) {
+                return e.getValue();
+            }
+        }
+        return null;
+    }
+
+    private static DataField findField(List<DataField> fields, String name) {
+        for (DataField f : fields) {
+            if (f.name().equalsIgnoreCase(name)) {
+                return f;
+            }
+        }
+        throw new IllegalStateException("Partition column not found in schema: " + name);
     }
 }
