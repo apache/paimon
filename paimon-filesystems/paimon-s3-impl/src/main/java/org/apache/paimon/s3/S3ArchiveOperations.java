46bf7f71c540945553cfae96c8e86c80aeb55855
shekharrajak <shekharrajak@live.com>
2026-06-06 17:39:50 +0530

    Implement S3 archive operations

--- paimon-filesystems/paimon-s3-impl/src/main/java/org/apache/paimon/s3/S3ArchiveOperations.java
+++ paimon-filesystems/paimon-s3-impl/src/main/java/org/apache/paimon/s3/S3ArchiveOperations.java
@@ -20,8 +20,25 @@ package org.apache.paimon.s3;
 
 import org.apache.paimon.fs.StorageType;
 
-import software.amazon.awssdk.services.s3.model.StorageClass;
+import org.apache.hadoop.fs.s3a.S3AFileSystem;
 
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CopyObjectRequest;
+import software.amazon.awssdk.services.s3.model.GlacierJobParameters;
+import software.amazon.awssdk.services.s3.model.MetadataDirective;
+import software.amazon.awssdk.services.s3.model.ObjectAlreadyInActiveTierErrorException;
+import software.amazon.awssdk.services.s3.model.ObjectNotInActiveTierErrorException;
+import software.amazon.awssdk.services.s3.model.RestoreObjectRequest;
+import software.amazon.awssdk.services.s3.model.RestoreRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.StorageClass;
+import software.amazon.awssdk.services.s3.model.TaggingDirective;
+import software.amazon.awssdk.services.s3.model.Tier;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 import static org.apache.paimon.utils.Preconditions.checkArgument;
@@ -31,6 +48,8 @@ import static org.apache.paimon.utils.Preconditions.checkNotNull;
 class S3ArchiveOperations {
 
     private static final long SECONDS_PER_DAY = 24 * 60 * 60;
+
+    private static final String RESTORE_ALREADY_IN_PROGRESS = "RestoreAlreadyInProgress";
 
     private S3ArchiveOperations() {}
 
@@ -67,5 +86,90 @@ class S3ArchiveOperations {
 
         checkArgument(days <= Integer.MAX_VALUE, "Restore duration is too large: %s", duration);
         return (int) days;
+    }
+
+    static void changeStorageClass(
+            S3AFileSystem fileSystem,
+            org.apache.hadoop.fs.Path path,
+            StorageClass storageClass,
+            String operation)
+            throws IOException {
+        String key = fileSystem.pathToKey(path);
+        CopyObjectRequest request =
+                fileSystem
+                        .getRequestFactory()
+                        .newCopyObjectRequestBuilder(key, key, fileSystem.getObjectMetadata(path))
+                        .storageClass(storageClass)
+                        .metadataDirective(MetadataDirective.COPY)
+                        .taggingDirective(TaggingDirective.COPY)
+                        .build();
+
+        try {
+            s3Client(fileSystem).copyObject(request);
+        } catch (ObjectNotInActiveTierErrorException e) {
+            throw new IOException(
+                    "S3 object "
+                            + path
+                            + " is not in active tier. Restore it before unarchiving.",
+                    e);
+        } catch (S3Exception e) {
+            throw new IOException(
+                    "Failed to " + operation + " S3 object " + path + " to " + storageClass + ".",
+                    e);
+        } catch (SdkException e) {
+            throw new IOException(
+                    "Failed to " + operation + " S3 object " + path + " to " + storageClass + ".",
+                    e);
+        }
+    }
+
+    static void restoreArchive(S3AFileSystem fileSystem, org.apache.hadoop.fs.Path path, int days)
+            throws IOException {
+        RestoreObjectRequest request = restoreObjectRequest(fileSystem.getBucket(), fileSystem.pathToKey(path), days);
+
+        try {
+            s3Client(fileSystem).restoreObject(request);
+        } catch (ObjectAlreadyInActiveTierErrorException e) {
+            throw new IOException("S3 object " + path + " is already in active tier.", e);
+        } catch (S3Exception e) {
+            if (isRestoreAlreadyInProgress(e)) {
+                return;
+            }
+            throw new IOException("Failed to restore archived S3 object " + path + ".", e);
+        } catch (SdkException e) {
+            throw new IOException("Failed to restore archived S3 object " + path + ".", e);
+        }
+    }
+
+    static RestoreObjectRequest restoreObjectRequest(String bucket, String key, int days) {
+        return RestoreObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .restoreRequest(
+                        RestoreRequest.builder()
+                                .days(days)
+                                .glacierJobParameters(
+                                        GlacierJobParameters.builder().tier(Tier.STANDARD).build())
+                                .build())
+                .build();
+    }
+
+    static boolean isRestoreAlreadyInProgress(S3Exception exception) {
+        return exception.awsErrorDetails() != null
+                && RESTORE_ALREADY_IN_PROGRESS.equals(exception.awsErrorDetails().errorCode());
+    }
+
+    private static S3Client s3Client(S3AFileSystem fileSystem) throws IOException {
+        try {
+            Method method = S3AFileSystem.class.getDeclaredMethod("getS3Client");
+            method.setAccessible(true);
+            return (S3Client) method.invoke(fileSystem);
+        } catch (NoSuchMethodException e) {
+            throw new IOException("S3AFileSystem does not expose an S3 client.", e);
+        } catch (IllegalAccessException e) {
+            throw new IOException("Failed to access S3 client from S3AFileSystem.", e);
+        } catch (InvocationTargetException e) {
+            throw new IOException("Failed to get S3 client from S3AFileSystem.", e);
+        }
     }
 }
