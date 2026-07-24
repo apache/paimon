9dfe225b1c62168d285212c6c30f4289c23a20f0
xiaohongbo <xiaohongbo.xhb@alibaba-inc.com>
2026-07-23 19:56:03 -0700

    [flink] Fix row-count option formatting

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/dataevolution/DataEvolutionPartialWriteOperator.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/dataevolution/DataEvolutionPartialWriteOperator.java
@@ -96,8 +96,7 @@ public class DataEvolutionPartialWriteOperator
         // Data evolution requires a single output file: disable both size and row rolling.
         Map<String, String> options = new HashMap<>();
         options.put(CoreOptions.TARGET_FILE_SIZE.key(), "99999 G");
-        options.put(
-                CoreOptions.TARGET_FILE_ROW_NUM.key(), String.valueOf(Long.MAX_VALUE));
+        options.put(CoreOptions.TARGET_FILE_ROW_NUM.key(), String.valueOf(Long.MAX_VALUE));
         return options;
     }
 
