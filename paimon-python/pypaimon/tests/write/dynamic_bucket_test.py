895686e8d1db6ceae987fe7bcf4a2a9638b215bf
JingsongLi <jingsonglee0@gmail.com>
2026-07-30 11:33:07 +0800

    Fix Python snapshot commit test callback

--- paimon-python/pypaimon/tests/write/dynamic_bucket_test.py
+++ paimon-python/pypaimon/tests/write/dynamic_bucket_test.py
@@ -621,7 +621,8 @@ class DynamicBucketTest(unittest.TestCase):
             original_snapshot_commit = commit.file_store_commit.snapshot_commit
             calls = 0
 
-            def lose_first_compare_and_set(snapshot, statistics):
+            def lose_first_compare_and_set(
+                    base_snapshot_uuid, snapshot, statistics):
                 nonlocal calls
                 calls += 1
                 concurrent_writer, concurrent_commit, concurrent_messages = (
