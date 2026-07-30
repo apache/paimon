0c8054cf4a7b409f927a588f997d3db8c5f1f2aa
Juntao Zhang <juntzhang@foxmail.com>
2026-07-30 23:36:12 +0800

    [core] fix conflict

--- paimon-core/src/test/java/org/apache/paimon/utils/ChainTableUtilsTest.java
+++ paimon-core/src/test/java/org/apache/paimon/utils/ChainTableUtilsTest.java
@@ -44,7 +44,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
