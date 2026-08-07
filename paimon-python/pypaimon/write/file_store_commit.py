a52df278a759feaad523ee60ccf0461c76a339d0
JingsongLi <jingsonglee0@gmail.com>
2026-08-07 13:49:34 +0800

    [python] Inherit watermark in PyPaimon commits

--- paimon-python/pypaimon/write/file_store_commit.py
+++ paimon-python/pypaimon/write/file_store_commit.py
@@ -742,6 +742,8 @@ class FileStoreCommit:
                 commit_identifier=commit_identifier,
                 commit_kind=commit_kind,
                 time_millis=int(time.time() * 1000),
+                watermark=(
+                    latest_snapshot.watermark if latest_snapshot else None),
                 next_row_id=next_row_id,
                 index_manifest=index_manifest,
             )
