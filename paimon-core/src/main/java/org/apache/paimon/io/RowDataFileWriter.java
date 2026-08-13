87eb04451d51de4a86b643b3b9a6c82a2c97d0f2
jianguotian <841604084@qq.com>
2026-08-13 14:13:21 +0800

    [core][format] Support Arrow bundle writes (#9100)

--- paimon-core/src/main/java/org/apache/paimon/io/RowDataFileWriter.java
+++ paimon-core/src/main/java/org/apache/paimon/io/RowDataFileWriter.java
@@ -116,6 +116,15 @@ public class RowDataFileWriter extends StatsCollectingSingleFileWriter<InternalR
 
     @Override
     public void writeBundle(BundleRecords bundle) throws IOException {
+        if (auxiliaryFileWriters.isEmpty()
+                && sequenceNumberTracker.supportsRowCountUpdate()
+                && !requiresPerRecordStats()) {
+            long rowCount = bundle.rowCount();
+            super.writeBundle(bundle);
+            sequenceNumberTracker.updateByRowCount(rowCount);
+            return;
+        }
+
         for (InternalRow row : bundle) {
             write(row);
         }
