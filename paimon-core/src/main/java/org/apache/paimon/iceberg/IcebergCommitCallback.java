0e9dc517016b2bcde94804e6a264d854878df7a8
Oleksandr Nitavskyi <oleksandr.nitavskyi@datadoghq.com>
2026-08-26 10:21:23 +0200

    [core][iceberg] Include table name in commit/Iceberg-commit exception (#9391)

--- paimon-core/src/main/java/org/apache/paimon/iceberg/IcebergCommitCallback.java
+++ paimon-core/src/main/java/org/apache/paimon/iceberg/IcebergCommitCallback.java
@@ -294,6 +294,8 @@ public class IcebergCommitCallback implements CommitCallback, TagCallback {
                                                         + commitUser
                                                         + " and identifier "
                                                         + committable.identifier()
+                                                        + " for table "
+                                                        + table.name()
                                                         + ". This is unexpected."));
         long snapshotId = snapshot.id();
         createMetadata(
