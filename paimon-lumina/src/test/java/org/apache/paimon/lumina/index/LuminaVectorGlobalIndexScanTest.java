0633e6785598313b4064c54579c9bf4de70babbd
QuakeWang <wangfuzheng0814@foxmail.com>
2026-08-31 09:19:31 +0800

    [lumina] Set schema ID in global index scan test

--- paimon-lumina/src/test/java/org/apache/paimon/lumina/index/LuminaVectorGlobalIndexScanTest.java
+++ paimon-lumina/src/test/java/org/apache/paimon/lumina/index/LuminaVectorGlobalIndexScanTest.java
@@ -231,8 +231,10 @@ public class LuminaVectorGlobalIndexScanTest {
                             entry.fileName(),
                             fileSize,
                             entry.rowCount(),
+                            null,
+                            null,
                             globalMeta,
-                            (String) null));
+                            ipTable.schema().id()));
         }
 
         DataIncrement dataIncrement = DataIncrement.indexIncrement(metas);
@@ -322,8 +324,10 @@ public class LuminaVectorGlobalIndexScanTest {
                             entry.fileName(),
                             fileSize,
                             entry.rowCount(),
+                            null,
+                            null,
                             globalMeta,
-                            (String) null));
+                            table.schema().id()));
         }
         return metas;
     }
