5672ab53149925f7857d85b2af491cb758635978
weijie.tong <tongweijie178@gmail.com>
2026-09-10 13:51:39 +0800

    remove a empty line

--- paimon-core/src/main/java/org/apache/paimon/table/source/TableRead.java
+++ paimon-core/src/main/java/org/apache/paimon/table/source/TableRead.java
@@ -30,7 +30,6 @@ import org.apache.paimon.reader.ReaderSupplier;
 import org.apache.paimon.reader.RecordReader;
 
 import javax.annotation.Nullable;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
