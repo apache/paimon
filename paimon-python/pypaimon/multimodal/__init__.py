e71c49410f7e1bf35487825f243a98e2949c1e33
XiaoHongbo <xiaohongbo.xhb@alibaba-inc.com>
2026-09-11 17:55:26 +0800

    [python] Add as-of joins for multimodal scans (#9536)

--- paimon-python/pypaimon/multimodal/__init__.py
+++ paimon-python/pypaimon/multimodal/__init__.py
@@ -42,6 +42,10 @@ from pypaimon.multimodal.table import (
     text_route,
     vector_route,
 )
+from pypaimon.multimodal.temporal import (
+    AsOfJoin,
+    join_asof,
+)
 from pypaimon.multimodal.video import VideoFrameCollator
 from pypaimon.table.row.blob import Blob, BlobDescriptor, VideoFrameDescriptor
 from pypaimon.table.data_evolution_merge_into import (
@@ -51,6 +55,7 @@ from pypaimon.table.data_evolution_merge_into import (
 )
 
 __all__ = [
+    "AsOfJoin",
     "Blob",
     "BlobDescriptor",
     "BlobObject",
@@ -71,6 +76,7 @@ __all__ = [
     "VideoFrameCollator",
     "VideoFrameDescriptor",
     "connect",
+    "join_asof",
     "lit",
     "source_col",
     "target_col",
