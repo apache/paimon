6939b71cf81b55407f9418f8d34caf26e230be0f
yantian <lining.jln@alibaba-inc.com>
2026-09-16 16:48:10 +0800

    [core] Avoid redundant sorting when building row range indexes from bitmaps

--- paimon-core/src/main/java/org/apache/paimon/globalindex/DataEvolutionBatchScan.java
+++ paimon-core/src/main/java/org/apache/paimon/globalindex/DataEvolutionBatchScan.java
@@ -286,7 +286,7 @@ public class DataEvolutionBatchScan implements DataTableScan {
             }
             if (indexResult.isPresent()) {
                 GlobalIndexResult result = indexResult.get();
-                rowRangeIndex = RowRangeIndex.create(result.results().toRangeList());
+                rowRangeIndex = RowRangeIndex.fromBitmap(result.results());
                 if (result instanceof ScoredGlobalIndexResult) {
                     scoreGetter = ((ScoredGlobalIndexResult) result).scoreGetter();
                 }
