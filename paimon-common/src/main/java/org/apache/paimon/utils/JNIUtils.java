e41c77297ce11c40ffee9dbf6d01b35352cb7fac
YeJunHao <41894543+leaves12138@users.noreply.github.com>
2026-09-22 18:43:21 +0800

    [common] Initialize JNI libraries independently by name (#10100)

--- paimon-common/src/main/java/org/apache/paimon/utils/JNIUtils.java
+++ paimon-common/src/main/java/org/apache/paimon/utils/JNIUtils.java
@@ -24,7 +24,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
@@ -35,7 +37,7 @@ public class JNIUtils {
 
     private static final Logger LOG = Logger.getLogger(JNIUtils.class.getName());
 
-    private static boolean inited = false;
+    private static final Set<String> INITIALIZED_LIBRARIES = new HashSet<>();
 
     private static String osName() {
         String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT).replace(' ', '_');
@@ -84,7 +86,7 @@ public class JNIUtils {
     }
 
     public static synchronized void load(String jniName) {
-        if (!inited) {
+        if (!INITIALIZED_LIBRARIES.contains(jniName)) {
             String jniFileName = resourceName(jniName);
             InputStream jniFileInput = JNIUtils.class.getResourceAsStream(jniFileName);
             if (jniFileInput == null) {
@@ -111,7 +113,7 @@ public class JNIUtils {
                             }
 
                             loadLibraryFile(tempFile.getAbsolutePath());
-                            inited = true;
+                            INITIALIZED_LIBRARIES.add(jniName);
                             return;
                         }
 
