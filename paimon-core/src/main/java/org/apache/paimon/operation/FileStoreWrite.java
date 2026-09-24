0ac88b2403f1711c5f105367685bcda927e985ac
David Wang <dwang4@atlassian.com>
2026-09-25 09:45:34 +1000

    [core] Route overwrite writes by partition bucket layout

--- paimon-core/src/main/java/org/apache/paimon/operation/FileStoreWrite.java
+++ paimon-core/src/main/java/org/apache/paimon/operation/FileStoreWrite.java
@@ -30,6 +30,7 @@ import org.apache.paimon.memory.MemoryPoolFactory;
 import org.apache.paimon.mergetree.compact.CompactRewriterFactory;
 import org.apache.paimon.metrics.MetricRegistry;
 import org.apache.paimon.table.sink.CommitMessage;
+import org.apache.paimon.table.sink.PartitionBucketMapping;
 import org.apache.paimon.table.sink.SinkRecord;
 import org.apache.paimon.types.RowType;
 import org.apache.paimon.utils.CommitIncrement;
@@ -52,6 +53,12 @@ import java.util.concurrent.ExecutorService;
 public interface FileStoreWrite<T> extends Restorable<List<FileStoreWrite.State<T>>> {
 
     FileStoreWrite<T> withWriteRestore(WriteRestore writeRestore);
+
+    /** Provides the preloaded partition-to-bucket mapping for fixed-bucket writes. */
+    default FileStoreWrite<T> withPartitionBucketMapping(
+            PartitionBucketMapping partitionBucketMapping) {
+        return this;
+    }
 
     FileStoreWrite<T> withIOManager(IOManager ioManager);
 
--- paimon-core/src/main/java/org/apache/paimon/table/sink/TableWriteImpl.java
+++ paimon-core/src/main/java/org/apache/paimon/table/sink/TableWriteImpl.java
@@ -123,6 +123,12 @@ public class TableWriteImpl<T> implements InnerTableWrite, Restorable<List<State
         return this;
     }
 
+    public TableWriteImpl<T> withPartitionBucketMapping(
+            PartitionBucketMapping partitionBucketMapping) {
+        write.withPartitionBucketMapping(partitionBucketMapping);
+        return this;
+    }
+
     @Override
     public TableWriteImpl<T> withIOManager(IOManager ioManager) {
         write.withIOManager(ioManager);
